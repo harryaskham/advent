e292f996e072ce34bc3583b469b2ca8831580c5d
Harry Askham <askham@google.com>
2019-11-29 16:25:02 +0000

    Solves day 1_2

--- src/TwentyEighteen.hs
+++ src/TwentyEighteen.hs
@@ -1,11 +1,27 @@
 module TwentyEighteen where
 
-day1_1 :: IO Int
-day1_1 = do
+import Data.Set
+
+freqsToNums :: IO [Int]
+freqsToNums = do
   content <- readFile "input/2018/1_1.txt"
-  return $ sum (parseLine <$> lines content)
+  return $ parseLine <$> lines content
     where
       parseLine :: String -> Int
-      parseLine (sign:number) = case sign of
-                                  '+' -> read number
-                                  '-' -> -1 * read number
+      parseLine (sign:number) =
+        case sign of
+          '+' -> read number
+          '-' -> -1 * read number
+
+day1_1 :: IO Int
+day1_1 = do
+  nums <- freqsToNums
+  return $ sum nums
+
+day1_2 :: IO Int
+day1_2 = do
+  nums <- cycle <$> freqsToNums
+  return $ next empty nums 0
+    where
+      next :: Set Int -> [Int] -> Int -> Int
+      next seen (n:ns) frequency = if frequency `member` seen then frequency else next (insert frequency seen) ns (frequency + n)
