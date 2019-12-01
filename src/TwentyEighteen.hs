91513b77e231aa7a1847c5b0c11c2816d912e3ca
Harry Askham <harryaskham@gmail.com>
2019-12-01 14:25:38 +0000

    Solves day4 2018 finally

--- src/TwentyEighteen.hs
+++ src/TwentyEighteen.hs
@@ -13,6 +13,8 @@ import Text.Regex.TDFA
 import Text.Regex.TDFA.Text ()
 import Text.ParserCombinators.ReadP
 import Control.Applicative
+import Data.Time
+import Data.Ord
 
 -- Read signed ints from file.
 freqsToNums :: IO [Int]
@@ -114,14 +116,14 @@ day3_2 = do
   return $ _id . head $ filter (isRectUnique (coordCounts frs)) frs
 
 -- Month Day Hour Minute
-data TS = TS Int Int Int Int deriving (Eq, Ord, Show)
+data TimeStamp = TimeStamp Day TimeOfDay deriving (Eq, Ord, Show)
 type GuardId = Int
 data LogLine = GuardChange GuardId
              | Asleep
              | Awake deriving (Eq, Ord, Show)
 
 -- Parse out the raw logs into timestamps and message
-parseLogSleep :: ReadP (TS, LogLine)
+parseLogSleep :: ReadP (TimeStamp, LogLine)
 parseLogSleep = do
   string "[1518-"
   month <- count 2 digit
@@ -133,12 +135,12 @@ parseLogSleep = do
   minutes <- count 2 digit
   string "] "
   logLine <- parseGuard +++ parseAsleep +++ parseAwake
-  return (TS (read month) (read day) (read hours) (read minutes), logLine)
+  return (TimeStamp (fromGregorian 1518 (read month) (read day)) (TimeOfDay (read hours) (read minutes) 0), logLine)
     where
       digit = satisfy isDigit
       parseGuard = do
         string "Guard #"
-        guardId <- count 2 digit <|> count 3 digit
+        guardId <- manyTill digit (satisfy (==' '))
         return $ GuardChange (read guardId)
       parseAsleep = do
         string "falls asleep"
@@ -147,20 +149,72 @@ parseLogSleep = do
         string "wakes up"
         return Awake
 
--- Split the logs into chunks that start with guard changes.
-splitLogs :: [(TS, LogLine)] -> [[(TS, LogLine)]]
+-- Split the logs into chunks - either only guard changes or only awake/asleep.
+splitLogs :: [(TimeStamp, LogLine)] -> [[(TimeStamp, LogLine)]]
 splitLogs = drop 1 . split (whenElt isGuard)
   where
     isGuard (_, GuardChange _) = True
     isGuard _ = False
 
 -- Groups split logs into pairs
-groupSplitLogs :: [[(TS, LogLine)]] -> [(GuardId, [(TS, LogLine)])]
-groupSplitLogs = undefined
+groupSplitLogs :: [[(TimeStamp, LogLine)]] -> [(GuardId, [(TimeStamp, LogLine)])]
+groupSplitLogs ls = (\l -> (getGuard (l !! 0), l !! 1)) <$> chunksOf 2 ls
+  where
+    getGuard [(_, GuardChange guardId)] = guardId
+
+-- Gets the total minutes asleep in the given awake/asleep logs.
+minutesAsleep :: [(TimeStamp, LogLine)] -> Int
+minutesAsleep ls = sum (sleepMinutes <$> chunks)
+  where
+    chunks = chunksOf 2 (fst <$> ls)
+    sleepMinutes [(TimeStamp _ t1), (TimeStamp _ t2)] = length $ getMinutes t1 t2
+
+-- Gets the discrete minutes asleep in the given logs
+discreteMinutesAsleep :: [(TimeStamp, LogLine)] -> [Int]
+discreteMinutesAsleep ls = concat $ sleepMinutes <$> chunks
+  where
+    chunks = chunksOf 2 (fst <$> ls)
+    sleepMinutes [(TimeStamp _ t1), (TimeStamp _ t2)] = getMinutes t1 t2
+
+-- Gets pairs of (guard ID, minutes asleep) - guard ID could be repeated.
+guardMinutesAsleep :: [(GuardId, [(TimeStamp, LogLine)])] -> [(GuardId, [Int])]
+guardMinutesAsleep = (fmap . fmap) discreteMinutesAsleep
+
+-- Create a map from guard ID to list of minutes spent asleep.
+guardTotals :: [(GuardId, [Int])] -> M.Map GuardId [Int]
+guardTotals ls = foldl (\acc (guardId, minutes) -> M.insertWith (++) guardId minutes acc) M.empty ls
+
+-- Find the guard with the most total sleep.
+sleepiestGuard :: M.Map GuardId [Int] -> GuardId
+sleepiestGuard guards = fst $ maximumBy (comparing (length . snd)) $ M.toList guards
+
+-- Find a given guard's most common minute
+mostCommonGuardMinute :: M.Map GuardId [Int] -> GuardId -> Int
+mostCommonGuardMinute guards guardId = fromMaybe 0 $ (fst . mostCommon) <$> (M.lookup guardId guards)
+
+-- Get the most common thing along with its count.
+mostCommon :: (Ord a, Eq a) => [a] -> (a, Int)
+mostCommon xs = maximumBy (comparing snd) $ M.toList (itemCounts xs)
+
+-- Converts a timestamp pair to the list of minute-values it contains
+getMinutes :: TimeOfDay -> TimeOfDay -> [Int]
+getMinutes (TimeOfDay _ m1 _) (TimeOfDay _ m2 _) = [m1..m2-1]
 
 day4_1 :: IO Int
 day4_1 = do
   -- Parse out the timestamps and logline, taking first match and dropped rest of string.
   ls <- sort . fmap (fst . head . readP_to_S parseLogSleep) . lines <$> readFile "input/2018/4.txt"
-  print $ splitLogs ls
-  return 0
+  let guards = guardTotals . guardMinutesAsleep . groupSplitLogs . splitLogs $ ls
+      guard = sleepiestGuard guards
+      minute = mostCommonGuardMinute guards guard in
+      return $ guard * minute
+
+day4_2 :: IO Int
+day4_2 = do
+  ls <- sort . fmap (fst . head . readP_to_S parseLogSleep) . lines <$> readFile "input/2018/4.txt"
+  let guards = guardTotals . guardMinutesAsleep . groupSplitLogs . splitLogs $ ls
+      guardsToMostCommonMinute = (\(g, ms) -> (g, mostCommon ms)) <$> (filter ((/=0) . length . snd) $ M.toList guards)
+      (guard, (minute, count)) = maximumBy (comparing $ snd . snd) guardsToMostCommonMinute in do
+      print guards
+      print guardsToMostCommonMinute
+      return $ guard * minute
