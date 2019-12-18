0ca7213a652ac20c9822b2dfd599f1a47036bca6
Harry Askham <askham@google.com>
2019-12-18 15:50:40 +0000

    17 setup

---
+++ src/Day17.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE MultiWayIf #-}
+
+module Day15 where
+
+import qualified Data.Set as S
+import Data.List
+import Data.Char
+import Data.List.Split hiding (condense)
+import qualified Data.Map.Strict as M
+import Data.Maybe
+import Data.Function ((&))
+import Control.Applicative
+import qualified Data.Vector as V
+import qualified Data.Tree as T
+import qualified Data.List.Safe as LS
+import Control.Monad
+import Data.Ord
+import Control.Lens hiding (Empty)
+import qualified Data.Vector.Split as VS
+import Data.Ratio
+import Data.Foldable
+import Text.ParserCombinators.ReadP
+import Debug.Trace
+import qualified Data.Matrix as MX
+import System.IO
+import System.IO.HiddenChar
+import Control.Concurrent
+import System.Random
+import TwentyNineteen (Machine(..), readProgram, stepUntilNOutputs, inputs, outputs, clear)
+
+day17 :: IO ()
+day17 = do
+  program <- readProgram "input/2019/177txt"
+  let machine = (Machine 0 [] [] program 0)
+  return ()
