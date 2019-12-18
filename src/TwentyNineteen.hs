9a573fc4b6e73f1bfc644cc95ffdd7d4918f4d19
Harry Askham <askham@google.com>
2019-12-18 13:00:09 +0000

    day16 pt1 but pt2 far off

--- src/TwentyNineteen.hs
+++ src/TwentyNineteen.hs
@@ -1064,9 +1064,9 @@ day14 = do
   _ <- search rMap 1 10000000000
   return ()
 
-errorCode :: Int -> [Integer]
-errorCode 0 = tail $ cycle [0, 1, 0, -1]
-errorCode n = tail . cycle $ concat $ replicate (n+1) <$> [0, 1, 0, -1]
+errorCode :: Int -> Int -> [Integer]
+errorCode l 0 = take l $ tail $ cycle [0, 1, 0, -1]
+errorCode l n = take l $ tail . cycle $ concat $ replicate (n+1) <$> [0, 1, 0, -1]
 
 combineCodeWithNumber :: [Integer] -> [Integer] -> Integer
 combineCodeWithNumber code number = fromIntegral . digitToInt . last . show $ sum mulPairs
@@ -1077,19 +1077,40 @@ combineCodeWithNumber code number = fromIntegral . digitToInt . last . show $ su
 runPhase :: [Integer] -> [Integer]
 runPhase number = take (length number) $ (combineCodeWithNumber <$> fst <*> snd) <$> codeNumbers
   where
-    codes = errorCode <$> [0..]
+    codes = errorCode (length number) <$> [0..]
     codeNumbers = zip codes (repeat number)
 
 runPhases :: Int -> [Integer] -> [Integer]
 runPhases 0 number = number
 runPhases n number = runPhases (n-1) (runPhase number)
 
+-- Okay, need to be smarter here.
+-- We need to drop 5970807 digits off the 100-phased number
+-- But each digit depends on all others at each phase
+-- But, these digits run in a cycle, even though the error code doesn't
+-- We can find the error number for the interesting digits easily.
+-- So to create digit 5970808 we need errorCode 5970807 over the entire 10000-replication number
+-- We can do this once, but how to do this over the next phase?
+--
+-- Recognise that replication == cancelling out.
+-- Any -1 in the same spot as a +1 cancels out and 0 doesn't contribute
+-- But +1 must be in the same alignment as the -1, can;t just cancel 1 and -1
+
 offset :: [Integer] -> Int
 offset number = read $ intToDigit . fromInteger <$> take 7 number
+
+-- Get from a list in a cyclic fashion
+modGet :: Int -> [Integer] -> Integer
+modGet n xs = xs !! (n `mod` length xs)
+
+-- Get the last N thigns "efficiently"
+-- Better if we use a Vector or Array.
+efficientLastN :: Int -> [Integer] -> [Integer]
+efficientLastN n xs = modGet <$> [n-1..length xs - 1] <*> pure xs
 
 day16 :: IO ()
 day16 = do
   ls <- lines <$> readFile "input/2019/16.txt"
   let number = fromIntegral . digitToInt <$> head ls
+      off = offset number
   print $ take 8 $ runPhases 100 number
-  print $ take 8 . drop (offset number) $ runPhases 100 (concat . replicate 10000 $ number)
