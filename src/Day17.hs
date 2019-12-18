c71c29a04c49ca208f15df9f2357101ae8a1fcda
Harry Askham <askham@google.com>
2019-12-18 17:14:16 +0000

    Trying to do 17_2

--- src/Day17.hs
+++ src/Day17.hs
@@ -32,8 +32,84 @@ import Control.Concurrent
 import System.Random
 import TwentyNineteen (Machine(..), readProgram, stepUntilNOutputs, inputs, outputs, clear, runProgram)
 
-day17 :: IO ()
-day17 = do
+data Space = Empty | Scaffold | RUp | RDown | RLeft | RRight | RFallen deriving (Eq)
+
+spaceFromChar '.' = Empty
+spaceFromChar '#' = Scaffold
+spaceFromChar '^' = RUp
+spaceFromChar 'v' = RDown
+spaceFromChar '>' = RRight
+spaceFromChar '<' = RLeft
+spaceFromChar 'X' = RFallen
+spaceFromChar e = error $ show e
+
+instance Show Space where
+  show Empty = "."
+  show Scaffold = "#"
+  show RUp = "^"
+  show RDown = "v"
+  show RRight = ">"
+  show RLeft = "<"
+  show RFallen = "X"
+
+asciiToGrid :: [Integer] -> [[Space]]
+asciiToGrid outputs = (fmap . fmap) spaceFromChar (take (length rows - 1) rows)
+  where
+    ascii = toEnum . fromIntegral <$> outputs
+    rows = lines ascii
+
+intersectionCoords :: [[Space]] -> [(Int, Int)]
+intersectionCoords grid = filter isIntersection coords
+  where
+    coords = [(x, y) | x <- [1 .. length (head grid) - 2], y <- [1 .. length grid - 2]]
+    isIntersection (x, y) = ((grid !! y !! x) == Scaffold)
+                            && ((grid !! (y+1) !! x) == Scaffold)
+                            && ((grid !! (y-1) !! x) == Scaffold)
+                            && ((grid !! y !! (x+1)) == Scaffold)
+                            && ((grid !! y !! (x-1)) == Scaffold)
+    
+day17_1 :: IO ()
+day17_1 = do
   program <- readProgram "input/2019/17.txt"
   machine <- runProgram (Machine 0 [] [] program 0)
-  putStrLn $ toEnum . fromIntegral <$> machine ^. outputs
+  let grid = asciiToGrid $ machine ^. outputs
+      intersections = intersectionCoords grid
+      alignmentParams = ((*) <$> fst <*> snd) <$> intersections
+  sequenceA_ $ print <$> grid
+  print $ sum alignmentParams
+
+inputMovements :: String -> String -> String -> String -> Bool -> Machine -> Machine
+inputMovements routine funcA funcB funcC cameraOn machine =
+  machine & inputs .~ concat [asciiRoutine, asciiFuncA, asciiFuncB, asciiFuncC, asciiCamera]
+  where
+    asciiRoutine = fromIntegral . fromEnum <$> routine ++ "\n"
+    asciiFuncA = fromIntegral . fromEnum <$> funcA ++ "\n"
+    asciiFuncB = fromIntegral . fromEnum <$> funcB ++ "\n"
+    asciiFuncC = fromIntegral . fromEnum <$> funcC ++ "\n"
+    asciiCamera = [if cameraOn then (fromIntegral . fromEnum) 'y' else (fromIntegral . fromEnum) 'n', (fromIntegral . fromEnum) '\n']
+
+day17_2 :: IO ()
+day17_2 = do
+  program <- readProgram "input/2019/17.txt"
+  let machine = Machine 0 [] [] (M.insert 0 2 program) 0
+      moved = inputMovements
+                "A,B,C,B,A,C"
+                "R,8,R,8"
+                "R,4,R,4,R,8"
+                "L,6,L,2"
+                True
+                machine
+    {-
+                "A,B,C"
+                "L,4,L,4,L,10,R,4,R,4"
+                "L,4,L,4"
+                "R,8,R,10"
+                True
+                machine
+                -}
+  print $ moved ^. inputs
+  runMoved <- runProgram moved
+  let splitOutputs = chunksOf (31 * 39 + 1) $ runMoved ^. outputs
+      printChunk :: [Integer] -> IO ()
+      printChunk chunk = if length chunk > 10 then sequenceA_ $ print <$> asciiToGrid chunk else print chunk
+  sequenceA_ $ printChunk <$> splitOutputs
