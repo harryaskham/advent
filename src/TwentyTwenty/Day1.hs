9824dd38a74da73504442af43b2ddb2e34a762ab
Harry Askham <harryaskham@gmail.com>
2020-12-01 10:00:10 +0000

    Improves day1 solutions

--- src/TwentyTwenty/Day1.hs
+++ src/TwentyTwenty/Day1.hs
@@ -1,6 +1,5 @@
 module TwentyTwenty.Day1 where
 
-import Data.List (nub)
 import qualified Data.Map.Strict as M
 import Data.Maybe (catMaybes)
 
@@ -9,17 +8,13 @@ inputPath = "input/2020/1.txt"
 
 part1 :: IO ()
 part1 = do
-  ls <- lines <$> readFile inputPath
-  let xs = read <$> ls
+  xs <- fmap read . lines <$> readFile inputPath
   print $ [a * b | a <- xs, b <- xs, a + b == 2020]
 
 -- O(n^2) solution using precomputed all-pairs distance-to-2020
+part2 :: IO ()
 part2 = do
-  ls <- lines <$> readFile inputPath
-  let xs = read <$> ls
-      differenceLookup = M.fromList $ (\a b -> (2020 - a - b, (a, b))) <$> xs <*> xs
-      get a =
-        case M.lookup a differenceLookup of
-          Just (b, c) -> Just (a * b * c)
-          Nothing -> Nothing
+  xs <- fmap read . lines <$> readFile inputPath
+  let lookup = M.fromList $ (\a b -> (2020 - a - b, (a, b))) <$> xs <*> xs
+      get a = (a *) . (uncurry (*)) <$> M.lookup a lookup
   print $ catMaybes $ get <$> xs
