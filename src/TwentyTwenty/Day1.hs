d01a5c5360dbacaca8032724c3f42beda0e178dc
Harry Askham <harryaskham@gmail.com>
2020-12-01 09:45:40 +0000

    Improves day1 solutions

--- src/TwentyTwenty/Day1.hs
+++ src/TwentyTwenty/Day1.hs
@@ -1,44 +1,25 @@
 module TwentyTwenty.Day1 where
 
 import Data.List (nub)
+import qualified Data.Map.Strict as M
 import Data.Maybe (catMaybes)
 
 inputPath :: String
 inputPath = "input/2020/1.txt"
 
--- Take an indexed pair and return the product if valid.
-solvePair :: (Int, Int) -> (Int, Int) -> Maybe Int
-solvePair (i1, e1) (i2, e2) =
-  if i1 /= i2 && e1 + e2 == 2020
-    then Just (e1 * e2)
-    else Nothing
-
 part1 :: IO ()
 part1 = do
   ls <- lines <$> readFile inputPath
-  let entries = zip [0 ..] $ read <$> ls
-  -- Solve all pairs of indexed entries, and remove duplication
-  print $ head $ nub $ catMaybes $ solvePair <$> entries <*> entries
+  let xs = read <$> ls
+  print $ [a * b | a <- xs, b <- xs, a + b == 2020]
 
--- Combine two Maybes, dropping second one if both are Just.
-combineMaybe :: Maybe a -> Maybe a -> Maybe a
-combineMaybe (Just a) (Just _) = Just a
-combineMaybe (Just a) Nothing = Just a
-combineMaybe Nothing (Just a) = Just a
-combineMaybe Nothing Nothing = Nothing
-
--- Find the product of the subset of length n that sums to 2020
-findProduct :: [Int] -> [Int] -> Int -> Int -> Maybe Int
-findProduct [] _ _ _ = Nothing
-findProduct (x : xs) used n target
-  | length used == n && sum used == target = Just $ product used
-  | length used == n = Nothing
-  | otherwise =
-    combineMaybe
-      (findProduct xs used n target)
-      (findProduct xs (x : used) n target)
-
+-- O(n^2) solution using precomputed all-pairs distance-to-2020
 part2 = do
   ls <- lines <$> readFile inputPath
-  let entries = read <$> ls
-  print $ findProduct entries [] 3 2020
+  let xs = read <$> ls
+      differenceLookup = M.fromList $ (\a b -> (2020 - a - b, (a, b))) <$> xs <*> xs
+      get a =
+        case M.lookup a differenceLookup of
+          Just (b, c) -> Just (a * b * c)
+          Nothing -> Nothing
+  print $ catMaybes $ get <$> xs
