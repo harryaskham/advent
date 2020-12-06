566d4929be130d37b38e0ab9e96a373e9b121353
Harry Askham <harryaskham@gmail.com>
2020-12-06 07:45:13 +0000

    Solves Day6, preps Day7

--- src/TwentyTwenty/Day6.hs
+++ src/TwentyTwenty/Day6.hs
@@ -7,15 +7,12 @@ import qualified Data.Set as S
 inputPath :: String
 inputPath = "input/2020/6.txt"
 
-groups :: [String] -> [[String]]
-groups lines = splitOn [""] lines
-
 part1 :: IO Int
 part1 = do
-  gs <- groups . lines <$> readFile inputPath
-  return . sum $ length . nub . concat <$> gs
+  groups <- splitOn [""] . lines <$> readFile inputPath
+  return . sum $ length . nub . concat <$> groups
 
 part2 :: IO Int
 part2 = do
-  gs <- groups . lines <$> readFile inputPath
-  return . sum $ S.size . foldl1 S.intersection . fmap S.fromList <$> gs
+  groups <- splitOn [""] . lines <$> readFile inputPath
+  return . sum $ S.size . foldl1 S.intersection . fmap S.fromList <$> groups
