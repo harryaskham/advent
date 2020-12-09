67212381d93975b50cb73c6ccb51e4b37ccec9f7
Harry Askham <harryaskham@gmail.com>
2020-12-09 08:57:01 +0000

    Splits out Util module

--- src/TwentyTwenty/Day6.hs
+++ src/TwentyTwenty/Day6.hs
@@ -3,16 +3,16 @@ module TwentyTwenty.Day6 where
 import Data.List (nub)
 import Data.List.Split (splitOn)
 import qualified Data.Set as S
+import Util ((<$$>))
 
 inputPath :: String
 inputPath = "input/2020/6.txt"
 
+groups :: IO [[String]]
+groups = splitOn [""] . lines <$> readFile inputPath
+
 part1 :: IO Int
-part1 = do
-  groups <- splitOn [""] . lines <$> readFile inputPath
-  return . sum $ length . nub . concat <$> groups
+part1 = sum <$> length . nub . concat <$$> groups
 
 part2 :: IO Int
-part2 = do
-  groups <- splitOn [""] . lines <$> readFile inputPath
-  return . sum $ S.size . foldl1 S.intersection . fmap S.fromList <$> groups
+part2 = sum <$> S.size . foldl1 S.intersection . fmap S.fromList <$$> groups
---
+++ src/Util.hs
@@ -0,0 +1,6 @@
+module Util where
+
+infixl 5 <$$>
+
+(<$$>) :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
+(<$$>) = fmap . fmap
