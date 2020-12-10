2b7b5a5e6915c99db6eb845cc038ce65da91f983
Harry Askham <harryaskham@gmail.com>
2020-12-10 09:03:06 +0000

    Switches from IORef to MonadMemo

--- src/TwentyTwenty/Day10.hs
+++ src/TwentyTwenty/Day10.hs
@@ -18,9 +18,9 @@ part1 = differences 0 0 0 . sort . fmap read . lines <$> readFile inputPath
 
 arrangements :: (Int, [Int]) -> Memo (Int, [Int]) Int Int
 arrangements (_, [_]) = return 1
-arrangements (last, (j1 : j2 : js)) = do
-  nWithoutSkip <- memo arrangements (j1, (j2 : js))
-  nWithSkip <- memo arrangements (last, (j2 : js))
+arrangements (last, j1 : j2 : js) = do
+  nWithoutSkip <- memo arrangements (j1, j2 : js)
+  nWithSkip <- memo arrangements (last, j2 : js)
   if j2 - last > 3
     then return nWithoutSkip
     else return $ nWithoutSkip + nWithSkip
