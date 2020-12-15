4c3cad9b17b851d149e4b6e6cc6231ef5f4ecd37
Harry Askham <harryaskham@gmail.com>
2020-12-15 08:41:36 +0000

    Solves Day 15

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,6 @@
 module Main where
 
-import qualified TwentyTwenty.Day13 as D13
+import qualified TwentyTwenty.Day15 as D15
 
 main :: IO ()
-main = do
-  x <- D13.part2
-  print x
+main = print D15.part2
--- src/TwentyTwenty/Day15.hs
+++ src/TwentyTwenty/Day15.hs
@@ -1,9 +1,40 @@
 module TwentyTwenty.Day15 where
 
-inputPath :: String
-inputPath = "input/2020/15.txt"
+import Data.List.Split (splitOn)
+import qualified Data.Map.Strict as M
 
-part1 :: IO Int
-part1 = do
-  ls <- lines <$> readFile inputPath
-  return 0
+input :: [Int]
+input = read <$> splitOn "," "20,0,1,11,6,3"
+
+data GameState = GameState Int Int (M.Map Int [Int]) deriving (Show)
+
+mkGameState :: [Int] -> GameState
+mkGameState xs =
+  GameState
+    (length xs + 1)
+    (last xs)
+    (M.fromList (zip xs (pure <$> [1 ..])))
+
+stepGame :: GameState -> GameState
+stepGame (GameState step lastSaid ages) =
+  let nextSaid =
+        case M.lookup lastSaid ages of
+          Just [_] -> 0
+          Just (age1 : age2 : _) -> age1 - age2
+      nextAges =
+        case M.lookup nextSaid ages of
+          Nothing -> M.insert nextSaid [step] ages
+          Just as -> M.insert nextSaid (step : as) ages
+   in GameState (step + 1) nextSaid nextAges
+
+stepUntil :: Int -> GameState -> GameState
+stepUntil target state@(GameState step _ _) =
+  if step > target
+    then state
+    else stepUntil target (stepGame state)
+
+part1 :: Int
+part1 = let (GameState _ x _) = stepUntil 2020 (mkGameState input) in x
+
+part2 :: Int
+part2 = let (GameState _ x _) = stepUntil 30000000 (mkGameState input) in x
