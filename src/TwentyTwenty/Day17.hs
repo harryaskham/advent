a55041f7321a52c97eccae42b4f0d3513434c684
Harry Askham <harryaskham@gmail.com>
2020-12-17 09:28:21 +0000

    Solves day 17

--- src/TwentyTwenty/Day17.hs
+++ src/TwentyTwenty/Day17.hs
@@ -2,7 +2,6 @@
 
 module TwentyTwenty.Day17 where
 
-import Data.List
 import qualified Data.Map.Strict as M
 import Data.Maybe (catMaybes)
 import Text.RawString.QQ (r)
@@ -14,16 +13,19 @@ fromChar :: Char -> Cell
 fromChar '.' = Inactive
 fromChar '#' = Active
 
-type Coord = (Int, Int, Int)
+type Coord = (Int, Int, Int, Int)
 
 type Bound = (Int, Int)
 
-type Bounds = (Bound, Bound, Bound)
+type Bounds = (Bound, Bound, Bound, Bound)
 
 data Grid = Grid
   { unGrid :: M.Map Coord Cell,
-    unBounds :: Bounds
+    unBounds :: Bounds,
+    unDim :: Dim
   }
+
+data Dim = Dim3 | Dim4
 
 input :: [[Cell]]
 input =
@@ -38,80 +40,100 @@ input =
 .....###
 .####..#|]
 
-dI :: [[Cell]]
-dI =
-  fromChar
-    <$$> lines
-      [r|.#.
-..#
-###|]
-
-mkGrid :: [[Cell]] -> Grid
-mkGrid ls = Grid grid bounds
+mkGrid :: [[Cell]] -> Dim -> Grid
+mkGrid ls dim = Grid grid bounds dim
   where
-    grid = M.fromList [((x, y, 0), c) | (y, row) <- zip [0 ..] ls, (x, c) <- zip [0 ..] row]
+    grid =
+      M.fromList
+        [ ((x, y, 0, 0), c)
+          | (y, row) <- zip [0 ..] ls,
+            (x, c) <- zip [0 ..] row
+        ]
     h = length ls
     w = length (head ls)
-    bounds = ((-1, w), (-1, h), (-1, 1))
+    bounds =
+      ( (-1, w),
+        (-1, h),
+        (-1, 1),
+        case dim of
+          Dim3 -> (0, 0)
+          Dim4 -> (-1, 1)
+      )
 
-growBounds :: Bounds -> Bounds
-growBounds ((x1, x2), (y1, y2), (z1, z2)) =
-  ((x1 - 1, x2 + 1), (y1 - 1, y2 + 1), (z1 - 1, z2 + 1))
+growBounds :: Grid -> Bounds
+growBounds
+  (Grid _ ((x1, x2), (y1, y2), (z1, z2), (w1, w2)) dim) =
+    ( (x1 - 1, x2 + 1),
+      (y1 - 1, y2 + 1),
+      (z1 - 1, z2 + 1),
+      case dim of
+        Dim3 -> (w1, w2)
+        Dim4 -> (w1 - 1, w2 + 1)
+    )
 
-boundsToCoords :: Bounds -> [Coord]
-boundsToCoords ((x1, x2), (y1, y2), (z1, z2)) =
-  [(x, y, z) | x <- [x1 .. x2], y <- [y1 .. y2], z <- [z1 .. z2]]
+getCoords :: Grid -> [Coord]
+getCoords (Grid _ ((x1, x2), (y1, y2), (z1, z2), (w1, w2)) dim) =
+  [ (x, y, z, w)
+    | x <- [x1 .. x2],
+      y <- [y1 .. y2],
+      z <- [z1 .. z2],
+      w <- case dim of
+        Dim3 -> [0]
+        Dim4 -> [w1 .. w2]
+  ]
 
 neighbours :: Grid -> Coord -> [Cell]
-neighbours (Grid grid _) (x, y, z) =
+neighbours (Grid grid _ dim) (x, y, z, w) =
   catMaybes $ M.lookup <$> neighbourCoords <*> pure grid
   where
     neighbourCoords =
-      [ (x + xi, y + yi, z + zi)
+      [ (x + xi, y + yi, z + zi, w + wi)
         | xi <- [-1 .. 1],
           yi <- [-1 .. 1],
           zi <- [-1 .. 1],
-          (xi /= 0 || yi /= 0 || zi /= 0)
+          wi <- case dim of
+            Dim3 -> [0]
+            Dim4 -> [-1 .. 1],
+          xi /= 0 || yi /= 0 || zi /= 0 || wi /= 0
       ]
 
 stepCoord :: Grid -> Coord -> Cell
-stepCoord g@(Grid grid _) coord =
+stepCoord g@(Grid grid _ _) coord =
   case M.lookup coord grid of
-    Just Active -> if activeNs == 2 || activeNs == 3 then Active else Inactive
-    _ -> if activeNs == 3 then Active else Inactive
+    Just Active ->
+      if activeNs == 2 || activeNs == 3
+        then Active
+        else Inactive
+    _ ->
+      if activeNs == 3
+        then Active
+        else Inactive
   where
-    activeNs = length [n | n <- neighbours g coord, n == Active]
+    activeNs = length . filter (== Active) $ neighbours g coord
 
 step :: Grid -> Grid
-step g@(Grid grid bounds) = Grid grid' bounds'
+step g@(Grid _ _ dim) = Grid grid' bounds' dim
   where
-    grid' = M.fromList [(coord, stepCoord g coord) | coord <- boundsToCoords bounds]
-    bounds' = growBounds bounds
+    grid' =
+      M.fromList
+        [ (coord, stepCoord g coord)
+          | coord <- getCoords g
+        ]
+    bounds' = growBounds g
 
 stepN :: Int -> Grid -> Grid
 stepN 0 grid = grid
 stepN n grid = stepN (n -1) (step grid)
 
+solveForDim :: Dim -> Int
+solveForDim dim =
+  length
+    . filter (== Active)
+    . fmap snd
+    $ M.toList (unGrid $ stepN 6 (mkGrid input dim))
+
 part1 :: Int
-part1 = length [c | c <- snd <$> M.toList (unGrid $ stepN 6 (mkGrid input)), c == Active]
+part1 = solveForDim Dim3
 
-toChar Active = '#'
-toChar Inactive = '.'
-
-fromJ (Just c) = c
-fromJ Nothing = Inactive
-
-pretty (Grid grid ((x1, x2), (y1, y2), (z1, z2))) =
-  [intercalate "\n" $ [[toChar (fromJ (M.lookup (x, y, z) grid)) | x <- [x1 .. x2]] | y <- [y1 .. y2]] | z <- [z1 .. z2]]
-
-stepNIO :: Int -> Grid -> IO Grid
-stepNIO 0 grid = return grid
-stepNIO n grid = do
-  traverse putStrLn (pretty grid)
-  stepNIO (n -1) (step grid)
-
-debug :: IO ()
-debug = do
-  let grid = mkGrid dI
-  stepNIO 6 grid
-  return ()
+part2 :: Int
+part2 = solveForDim Dim4
