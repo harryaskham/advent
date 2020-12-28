219cf9d9c35b9136261b15d4ad37bf725a094141
Harry Askham <harryaskham@gmail.com>
2020-12-28 10:16:46 +0000

    Solve 2017-15

--- src/TwentySeventeen/Day15.hs
+++ src/TwentySeventeen/Day15.hs
@@ -1,10 +1,33 @@
 module TwentySeventeen.Day15 where
 
-inputPath :: String
-inputPath = "input/2017/15.txt"
+import Data.Bits (Bits ((.&.)))
+import Data.List (foldl')
 
-part1 :: IO ()
-part1 = undefined
+input :: (Int, Int)
+input = (703, 516)
 
-part2 :: IO ()
-part2 = undefined
+least16 :: Int -> Int
+least16 x = x .&. (2 ^ 16 - 1)
+
+part1 :: Int
+part1 =
+  snd $
+    foldl'
+      ( \((a, b), n) _ ->
+          if least16 a == least16 b
+            then (next (a, b), n + 1)
+            else (next (a, b), n)
+      )
+      (input, 0)
+      [0 .. 40000000]
+  where
+    next (a, b) = ((a * 16807) `rem` 2147483647, (b * 48271) `rem` 2147483647)
+
+part2 :: Int
+part2 = length stream
+  where
+    genA' a = let a' = a * 16807 `rem` 2147483647 in a' : genA' a'
+    genB' b = let b' = b * 48271 `rem` 2147483647 in b' : genB' b'
+    genA = filter ((== 0) . (`mod` 4)) (genA' (fst input))
+    genB = filter ((== 0) . (`mod` 8)) (genB' (snd input))
+    stream = filter (\(a, b) -> least16 a == least16 b) $ take 5000000 $ zip genA genB
