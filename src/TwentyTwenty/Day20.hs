6626f796dcf15dd2e1de7aeb3bef5d8e1e4a1f20
Harry Askham <harryaskham@gmail.com>
2020-12-29 10:01:28 +0000

    Uses Grid library for D20

--- src/TwentyTwenty/Day20.hs
+++ src/TwentyTwenty/Day20.hs
@@ -1,9 +1,10 @@
 module TwentyTwenty.Day20 where
 
-import Data.List (intersect, nub)
+import Data.List (intersect)
 import qualified Data.Map.Strict as M
 import Data.Maybe (catMaybes, fromMaybe)
 import qualified Data.Set as S
+import Grid (Grid, joinGrids, maxXY, toGrid, variants)
 import Text.ParserCombinators.Parsec
   ( GenParser,
     char,
@@ -21,7 +22,7 @@ import Util (readWithParser)
 inputPath :: String
 inputPath = "input/2020/20.txt"
 
-data Tile = Tile Int (M.Map (Int, Int) Char) deriving (Eq)
+data Tile = Tile Int (Grid Char) deriving (Eq, Show)
 
 tile :: GenParser Char st Tile
 tile = do
@@ -35,12 +36,7 @@ tile = do
   return $
     Tile
       (read tileId)
-      ( M.fromList
-          [ ((x, y), c)
-            | (y, row) <- zip [0 ..] rows,
-              (x, c) <- zip [0 ..] row
-          ]
-      )
+      (toGrid id rows)
 
 tiles :: GenParser Char st [Tile]
 tiles = do
@@ -48,33 +44,10 @@ tiles = do
   eof
   return tiles
 
-maxXY :: M.Map (Int, Int) a -> (Int, Int)
-maxXY m = (maximum $ fst <$> M.keys m, maximum $ snd <$> M.keys m)
-
-modifyCoords :: ((Int, Int) -> (Int, Int)) -> Tile -> Tile
-modifyCoords f (Tile tId tiles) =
-  Tile tId (M.mapKeys (fromOrigin . f . toOrigin) tiles)
-  where
-    (maxX, maxY) = maxXY tiles
-    xO = (maxX + 1) `div` 2
-    yO = (maxY + 1) `div` 2
-    toOrigin (x, y) = (x - xO, y - yO)
-    fromOrigin (x, y) = (x + xO, y + yO)
-
-variants :: Tile -> [Tile]
-variants t = nub $ modifyCoords <$> mods <*> pure t
-  where
-    flipH (x, y) = (negate x - 1, y)
-    flipV (x, y) = (x, negate y - 1)
-    rot90 (x, y) = (y, negate x - 1)
-    rot180 = rot90 . rot90
-    rot270 = rot90 . rot90 . rot90
-    mods = (.) <$> [id, flipH, flipV] <*> [id, rot90, rot180, rot270]
-
 mkVariantMap :: [Tile] -> M.Map Int [Tile]
 mkVariantMap ts =
   M.fromList $
-    (\t@(Tile tId _) -> (tId, variants t)) <$> ts
+    (\(Tile tId grid) -> (tId, Tile tId <$> variants grid)) <$> ts
 
 edgeString :: [(Int, Int)] -> Tile -> String
 edgeString coords (Tile _ tiles) = (tiles M.!) <$> coords
@@ -95,7 +68,7 @@ mkEdgeMap :: (Tile -> String) -> [Tile] -> M.Map String [Tile]
 mkEdgeMap getEdge ts =
   M.fromListWith (++) (zip (getEdge <$> ts) (pure <$> ts))
 
-type Jigsaw = M.Map (Int, Int) Tile
+type Jigsaw = Grid Tile
 
 solvedJigsaws ::
   Int ->
@@ -148,7 +121,7 @@ solveJigsaw :: IO (Jigsaw, Int)
 solveJigsaw = do
   ts <- readWithParser tiles <$> readFile inputPath
   let width = round $ sqrt . fromIntegral $ length ts
-      allVariants = concatMap variants ts
+      allVariants = concatMap (\(Tile tId grid) -> Tile tId <$> variants grid) ts
       variantMap = mkVariantMap ts
       leftEdgeMap = mkEdgeMap leftEdge allVariants
       topEdgeMap = mkEdgeMap topEdge allVariants
@@ -166,7 +139,7 @@ solveJigsaw = do
 getId :: Tile -> Int
 getId (Tile tId _) = tId
 
-getTiles :: Tile -> M.Map (Int, Int) Char
+getTiles :: Tile -> Grid Char
 getTiles (Tile _ tiles) = tiles
 
 part1 :: IO Int
@@ -181,24 +154,10 @@ part1 = do
               ]
   return $ product $ getId <$> corners
 
-getJT :: Jigsaw -> Int -> Int -> Int -> Int -> Char
-getJT jigsaw jx jy tx ty =
-  getTiles (jigsaw M.! (jx, jy)) M.! (tx, ty)
-
-concatJigsaw :: Jigsaw -> Int -> Tile
-concatJigsaw jigsaw width =
-  Tile
-    9999
-    ( M.fromList
-        [ ( (jx * 8 + (tx - 1), jy * 8 + (ty - 1)),
-            getJT jigsaw jx jy tx ty
-          )
-          | jx <- [0 .. width - 1],
-            jy <- [0 .. width - 1],
-            tx <- [1 .. 8],
-            ty <- [1 .. 8]
-        ]
-    )
+chopBorders :: Grid Char -> Grid Char
+chopBorders =
+  M.mapKeys (\(x, y) -> (x - 1, y - 1))
+    . M.filterWithKey (\(x, y) _ -> x > 0 && x < 9 && y > 0 && y < 9)
 
 monsterCoords :: [(Int, Int)]
 monsterCoords =
@@ -247,9 +206,9 @@ nonMonsterCoords tiles =
 
 part2 :: IO Int
 part2 = do
-  (jigsaw, width) <- solveJigsaw
-  let tile = concatJigsaw jigsaw width
-      images = getTiles <$> variants tile
+  (jigsaw, _) <- solveJigsaw
+  let grid = joinGrids (chopBorders <$> (getTiles <$> jigsaw))
+      images = variants grid
   return
     . length
     . head
