ab7b7b505329f43b17b78201c69b7da9dc9c5cb3
Harry Askham <harryaskham@gmail.com>
2020-12-30 14:14:01 +0000

    Solve 2016-6

--- src/TwentySixteen/Day6.hs
+++ src/TwentySixteen/Day6.hs
@@ -1,9 +1,25 @@
 module TwentySixteen.Day6 where
 
+import Data.List (sortOn, transpose)
+import qualified Data.Map.Strict as M
+import Util (countMap)
+
 inputPath :: String
 inputPath = "input/2016/6.txt"
 
-part1 :: IO Int
-part1 = do
-  ls <- lines <$> readFile inputPath
-  return 0
+part12 :: IO [String]
+part12 =
+  traverse
+    ( \firstLast ->
+        fmap
+          ( fst
+              . firstLast
+              . sortOn snd
+              . M.toList
+              . countMap
+          )
+          . transpose
+          . lines
+          <$> readFile inputPath
+    )
+    [last, head]
