7555d8dd7a1836b661f71d72c42ddd07c55c7dd0
Harry Askham <harryaskham@gmail.com>
2020-12-30 11:35:21 +0000

    Solve 2016-5

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import TwentySeventeen.Day17
+import TwentySixteen.Day5
 
 main :: IO ()
 main = print part2
--- src/TwentySixteen/Day5.hs
+++ src/TwentySixteen/Day5.hs
@@ -1,9 +1,37 @@
 module TwentySixteen.Day5 where
 
-inputPath :: String
-inputPath = "input/2016/5.txt"
+import qualified Crypto.Hash.MD5 as MD5
+import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Char8 as BC
+import Data.Char (digitToInt)
+import qualified Data.Map.Strict as M
+import Data.Tuple.Extra (first)
+import Util (toTuple2)
 
-part1 :: IO Int
-part1 = do
-  ls <- lines <$> readFile inputPath
-  return 0
+input :: String
+input = "ugkcyxxp"
+
+hashes :: [String]
+hashes =
+  BC.unpack
+    . B16.encode
+    . MD5.hash
+    . BC.pack
+    . (input ++)
+    . show
+    <$> [0 ..]
+
+part1 :: String
+part1 = take 8 [hash !! 5 | hash <- hashes, take 5 hash == "00000"]
+
+part2 :: String
+part2 =
+  let m =
+        M.fromListWith
+          (flip const)
+          [ first digitToInt . toTuple2 . take 2 . drop 5 $ hash
+            | hash <- take 50000000 hashes,
+              take 5 hash == "00000",
+              digitToInt (hash !! 5) < 8
+          ]
+   in (m M.!) <$> [0 .. 7]
