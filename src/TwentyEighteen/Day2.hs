e08513029334fa1f3a51cb737a4249d930a733c3
Harry Askham <harryaskham@gmail.com>
2021-01-10 19:00:20 +0000

    Split out 2018-3

--- src/TwentyEighteen/Day2.hs
+++ src/TwentyEighteen/Day2.hs
@@ -1,23 +1,8 @@
 module TwentyEighteen.Day2 where
 
-import Control.Applicative
-import Control.Monad
-import Control.Monad.Fix
-import Data.Char
-import Data.Function ((&))
-import Data.List
-import Data.List.Split
 import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Ord
-import qualified Data.Sequence as SQ
-import qualified Data.Set as S
-import Data.Time
-import qualified Data.Vector as V
-import Text.ParserCombinators.ReadP
-import Text.Regex.TDFA
-import Text.Regex.TDFA.Text ()
-import Util
+import Data.Maybe (catMaybes)
+import Util (countMap)
 
 -- Does the list contain the same thing exactly n times?
 exactlyN :: Ord a => Int -> [a] -> Bool
@@ -38,8 +23,7 @@ editDistance xs ys = length $ filter (== False) $ fmap (uncurry (==)) (zip xs ys
 -- Gets only the items that are the same in both cases.
 common :: Eq a => [a] -> [a] -> [a]
 common xs ys =
-  catMaybes $
-    filter (/= Nothing) $ fmap (\(x, y) -> if x == y then Just x else Nothing) (zip xs ys)
+  catMaybes $ filter (/= Nothing) $ fmap (\(x, y) -> if x == y then Just x else Nothing) (zip xs ys)
 
 part2 :: IO String
 part2 = do
--- src/TwentyEighteen/Solutions.hs
+++ src/TwentyEighteen/Solutions.hs
@@ -24,54 +24,6 @@ import Text.Regex.TDFA
 import Text.Regex.TDFA.Text ()
 import Util
 
--- The details of a fabric rectangle
-data FabRect = FabRect
-  { _id :: String,
-    _xC :: Int,
-    _yC :: Int,
-    _width :: Int,
-    _height :: Int
-  }
-  deriving (Show)
-
--- Read a rectangle from #id @ x,y: wxh form.
-parseFabricRect :: String -> FabRect
-parseFabricRect s = FabRect idStr (read xStr) (read yStr) (read widthStr) (read heightStr)
-  where
-    [_ : idStr, _, coordStr, dimStr] = words s
-    [xStr, yStr] = splitOn "," $ delete ':' coordStr
-    [widthStr, heightStr] = splitOn "x" dimStr
-
--- Keep track of which coords have been covered how many times.
--- Pass in the current counts for each location and the current coord to update it with.
-trackOverlap :: M.Map (Int, Int) Int -> (Int, Int) -> M.Map (Int, Int) Int
-trackOverlap counts coord = M.insertWith (+) coord 1 counts
-
-coordsForRect :: FabRect -> [(Int, Int)]
-coordsForRect fr = [(x, y) | x <- [_xC fr .. _xC fr + _width fr - 1], y <- [_yC fr .. _yC fr + _height fr - 1]]
-
-coordCounts :: [FabRect] -> M.Map (Int, Int) Int
-coordCounts frs = countMap coords
-  where
-    coords = concat $ coordsForRect <$> frs
-
-day3_1 :: IO Int
-day3_1 = do
-  frs <- fmap parseFabricRect . lines <$> readFile "input/2018/3.txt"
-  return $ snd <$> M.toList (coordCounts frs) & filter (> 1) & length
-
--- Is the given rectangle non-overlapping?
-isRectUnique :: M.Map (Int, Int) Int -> FabRect -> Bool
-isRectUnique coordCounts rect = all (== 1) counts
-  where
-    coords = coordsForRect rect
-    counts = catMaybes $ (`M.lookup` coordCounts) <$> coords
-
-day3_2 :: IO String
-day3_2 = do
-  frs <- fmap parseFabricRect . lines <$> readFile "input/2018/3.txt"
-  return $ _id . head $ filter (isRectUnique (coordCounts frs)) frs
-
 -- Month Day Hour Minute
 data TimeStamp = TimeStamp Day TimeOfDay deriving (Eq, Ord, Show)
 
