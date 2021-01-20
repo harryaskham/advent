3035e57a9a7301184bd6c2a401e8df638d121ff4
Harry Askham <harryaskham@gmail.com>
2021-01-20 10:48:34 +0000

    before trying minheap

--- src/TwentyEighteen/Day17.hs
+++ src/TwentyEighteen/Day17.hs
@@ -60,87 +60,85 @@ closedIn :: Grid Cell -> Coord2 -> Bool
 closedIn grid (x, y) = closedToRight (x, y) && closedToLeft (x, y)
   where
     closedToRight (x, y)
-      | M.lookup (x, y + 1) grid == Just Sand = False
+      | M.lookup (x, y + 1) grid `elem` [Nothing, Just Sand] = False
       | M.lookup (x + 1, y) grid == Nothing = False
       | M.lookup (x + 1, y) grid == Just Clay = True
       | otherwise = closedToRight (x + 1, y)
     closedToLeft (x, y)
-      | M.lookup (x, y + 1) grid == Just Sand = False
+      | M.lookup (x, y + 1) grid `elem` [Nothing, Just Sand] = False
       | M.lookup (x - 1, y) grid == Nothing = False
       | M.lookup (x - 1, y) grid == Just Clay = True
       | otherwise = closedToLeft (x - 1, y)
 
-drip :: Int -> Grid Cell -> Coord2 -> (Maybe (Grid Cell), Set Coord2)
-drip maxY grid (x, y) = go (SQ.singleton ((x, y), S.empty)) S.empty
+drip :: Int -> Grid Cell -> Coord2 -> (Grid Cell, Set Coord2)
+drip maxY grid' (x, y) = go grid' (SQ.singleton ((x, y), S.empty)) S.empty
   where
-    go SQ.Empty allSeen = (Nothing, S.insert (x, y) allSeen)
-    go (((x, y), seen) SQ.:<| queue) allSeen
-      | y > maxY = go queue (S.union nextAllSeen seen)
+    go grid SQ.Empty allSeen = (grid, S.insert (x, y) allSeen)
+    go grid (((x, y), seen) SQ.:<| queue) allSeen
+      | y == maxY = go placedWater queue (S.union nextAllSeen seen)
+      | (x, y) `S.member` seen = go grid queue allSeen
       | null nextStates && closedIn grid (x, y) =
         --traceStrLn ("grid:\n" ++ prettyWithWater (M.insert (x, y) Water grid) (S.insert (x, y) nextAllSeen)) $
-        (Just allPlacedWater, S.insert (x, y) nextAllSeen)
-      | otherwise = go (queue SQ.>< SQ.fromList nextStates) (S.union nextAllSeen seen)
+        (placedWater, S.insert (x, y) nextAllSeen)
+      | otherwise = go grid (queue SQ.>< SQ.fromList nextStates) (S.union nextAllSeen seen)
       where
-        downState = if not ((x, y + 1) `S.member` seen) && (not ((x, y + 1) `M.member` grid) || M.lookup (x, y + 1) grid == Just Sand) then Just ((x, y + 1), (S.insert (x, y) seen)) else Nothing
-        leftState = if not ((x -1, y) `S.member` seen) && M.lookup (x -1, y) grid == Just Sand then Just ((x -1, y), (S.insert (x, y) seen)) else Nothing
-        rightState = if not ((x + 1, y) `S.member` seen) && M.lookup (x + 1, y) grid == Just Sand then Just ((x + 1, y), (S.insert (x, y) seen)) else Nothing
+        nextSeen = S.insert (x, y) seen
+        mkState pos =
+          if (not (pos `S.member` (seen `S.union` allSeen))) && M.lookup pos grid == Just Sand
+            then Just (pos, nextSeen)
+            else Nothing
+        downState = mkState (x, y + 1)
+        leftState = if (x -1, y + 1) `M.member` grid then mkState (x -1, y) else Nothing
+        rightState = if (x + 1, y + 1) `M.member` grid then mkState (x + 1, y) else Nothing
         nextStates = case downState of
           Just ds -> [ds]
           Nothing -> catMaybes [leftState, rightState]
         nextAllSeen = foldl' (flip S.insert) allSeen (fst <$> nextStates)
-        placedWater = M.insert (x, y) Water grid
-        closedSeen = filter (closedIn grid) (S.toList seen)
-        allPlacedWater = foldl' (\m p -> M.insert p Water m) placedWater closedSeen
-
--- always fill from the lowest unseen with no water?
+        closedSeen = filter (closedIn grid) ((x, y) : S.toList seen)
+        placedWater = foldl' (\m p -> M.insert p Water m) grid closedSeen
 
 fill :: Int -> Grid Cell -> Coord2 -> (Grid Cell, Set Coord2)
-fill maxY grid startPos = go grid (PQ.singleton (snd startPos) startPos) S.empty
+fill maxY grid startPos = go grid (PQ.singleton (snd startPos) startPos) S.empty S.empty
   where
-    go grid queue seen
+    go grid queue seen filledFrom
       | PQ.null queue = (grid, seen)
+      | snd source > maxY = go grid rest seen nextFilledFrom
+      | source `S.member` filledFrom = go grid rest seen nextFilledFrom
+      | M.lookup source grid `elem` [Nothing, Just Water] = go grid rest seen filledFrom
       | otherwise =
-        traceShow (S.size seen) $
-          case gridM of
-            Nothing -> (grid, nextSeen)
-            Just grid' ->
-              let wetClay = SQ.fromList . S.toList . S.filter ((/= Water) . (grid' M.!)) $ seen'
-               in --traceStrLn (prettyWithWater grid' nextSeen) $
-                  go grid' (foldl' (\q (x, y) -> PQ.insert y (x, y) q) rest wetClay) nextSeen
+        traceShow (S.size seen, source, M.lookup source grid, S.size seen', length wetClay) $
+          go grid' (foldl' (\q (x, y) -> PQ.insert y (x, y) q) rest wetClay) nextSeen nextFilledFrom
       where
         ((_, source), rest) = PQ.deleteFindMax queue
-        (gridM, seen') = drip maxY grid source
+        (grid', seen') = drip maxY grid source
+        wetClay = SQ.fromList . S.toList . S.filter (not . (`elem` [Nothing, Just Water]) . (`M.lookup` grid')) $ seen'
         nextSeen = S.union seen seen'
+        nextFilledFrom = S.insert source filledFrom
 
 readGrid :: IO (Grid Cell, Coord2)
 readGrid = do
   veins <- readWithParser veins <$> input 2018 17
+  --veins <- readWithParser veins <$> exampleInput 2018 17
   let clayCoords = S.fromList $ concat veins
       minX = fst $ minimumOn fst (concat veins)
       maxX = fst $ maximumOn fst (concat veins)
       maxY = snd $ maximumOn snd (concat veins)
       startPos = (500 - minX + 1, 1)
       grid =
-        M.insert startPos Water $
-          M.fromList
-            [ ((x, y), if (x + minX - 1, y) `S.member` clayCoords then Clay else Sand)
-              | x <- [0 .. (maxX - minX) + 2],
-                y <- [0 .. maxY]
-            ]
+        M.fromList
+          [ ((x, y), if (x + minX - 1, y) `S.member` clayCoords then Clay else Sand)
+            | x <- [0 .. (maxX - minX) + 2],
+              y <- [0 .. maxY]
+          ]
   return (grid, startPos)
 
 prettyWithWater :: Grid Cell -> Set Coord2 -> String
 prettyWithWater grid seen =
-  --pretty $ M.filterWithKey (\(_, y) _ -> y < 40) (foldl' (\g p -> if g M.! p /= Water then M.insert p SeenWater g else g) grid (S.toList seen'))
   pretty $ (foldl' (\g p -> if g M.! p /= Water then M.insert p SeenWater g else g) grid (S.toList seen'))
   where
     (minX, minY) = minXY grid
     (maxX, maxY) = maxXY grid
     seen' = S.filter (\(x, y) -> x >= minX && x <= maxX && y >= minY && y <= maxY) seen
-
--- TODO: abstract into concepts of bucket, and which side it will spill over, and distance to next bucket.
--- spill on one side == distance to bottom, on both it sthat times two
--- buckets in buckets present a problem or maybe they dont? we can fill all at aonce anyway
 
 part1 :: IO Int
 part1 = do
