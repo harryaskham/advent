ef2741f0be56d0745eac2c1dcf0f7f0f6b83c331
Harry Askham <harryaskham@gmail.com>
2021-01-27 21:55:18 +0000

    Prep D20

---
+++ src/TwentyEighteen/Day20.hs
@@ -0,0 +1,28 @@
+module TwentyEighteen.Day20 where
+
+import Coord
+import Data.Bits
+import Data.Char
+import qualified Data.Foldable as F
+import Data.Function
+import Data.List
+import Data.List.Extra
+import Data.Map (Map)
+import qualified Data.Map.Strict as M
+import Data.Maybe
+import Data.Sequence (Seq)
+import qualified Data.Sequence as SQ
+import Data.Set (Set)
+import qualified Data.Set as S
+import Data.Tuple.Extra
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import Debug.Trace
+import Grid
+import Text.ParserCombinators.Parsec
+import Util
+
+part1 :: IO Int
+part1 = do
+  rs <- lines <$> input 2018 20
+  return 0
