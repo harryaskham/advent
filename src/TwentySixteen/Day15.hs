c8af0df30a41abb94ae594f335063eebb6a70b43
Harry Askham <harryaskham@gmail.com>
2021-02-02 21:35:38 +0000

    Solves 2016-15

--- src/TwentySixteen/Day15.hs
+++ src/TwentySixteen/Day15.hs
@@ -1,32 +1,17 @@
 module TwentySixteen.Day15 where
 
-import Control.Monad
-import Control.Monad.Memo
-import Coord
-import Data.Bits
-import Data.Char
-import qualified Data.Foldable as F
-import Data.Function
-import Data.List
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Sequence (Seq)
-import qualified Data.Sequence as SQ
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Tuple.Extra
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Debug.Trace
-import Grid
+import Data.Char (digitToInt)
+import Data.List (nub)
 import Text.ParserCombinators.Parsec
-import Util
+  ( GenParser,
+    digit,
+    eof,
+    many1,
+    string,
+  )
+import Util (eol, input, readWithParser)
 
-data Disc = Disc Int Int Int deriving (Show)
+data Disc = Disc Int Int Int
 
 discs :: GenParser Char () [Disc]
 discs = do
