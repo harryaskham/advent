b06ed39316f6220c421079a112fe0f564515c923
Harry Askham <harryaskham@gmail.com>
2021-02-03 12:52:36 +0000

    Solves 2016-20

--- src/TwentySixteen/Day20.hs
+++ src/TwentySixteen/Day20.hs
@@ -1,32 +1,28 @@
 module TwentySixteen.Day20 where
 
-import Control.Monad
-import Control.Monad.Memo
-import Coord
-import Data.Bits
-import Data.Char
-import qualified Data.Foldable as F
-import Data.Function
-import Data.List
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Sequence (Seq)
-import qualified Data.Sequence as SQ
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Tuple.Extra
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Debug.Trace
-import Grid
-import Text.ParserCombinators.Parsec
-import Util
+import Data.List (sortOn)
+import Data.List.Extra (splitOn)
+import Util (input, toTuple2, (<$$>))
 
-part1 :: IO Int
-part1 = do
-  ls <- lines <$> input 2016 20
-  return 0
+lowestUnblocked :: [(Int, Int)] -> Int
+lowestUnblocked blacklist = go (sortOn fst blacklist)
+  where
+    go [(_, u)] = u + 1
+    go ((l1, u1) : (l2, u2) : rest)
+      | l2 > u1 + 1 = u1 + 1
+      | u2 < u1 = go ((l1, u1) : rest)
+      | otherwise = go ((l2, u2) : rest)
+
+numAllowed :: [(Int, Int)] -> Int
+numAllowed blacklist = go (sortOn fst blacklist)
+  where
+    go [(_, u)] = 2 ^ 32 - 1 - u
+    go ((l1, u1) : (l2, u2) : rest)
+      | l2 > u1 = l2 - u1 - 1 + go ((l2, u2) : rest)
+      | u2 < u1 = go ((l1, u1) : rest)
+      | otherwise = go ((l2, u2) : rest)
+
+part12 :: IO (Int, Int)
+part12 = do
+  is <- (toTuple2 <$>) . (read <$$>) . (splitOn "-" <$>) . lines <$> input 2016 20
+  return (lowestUnblocked is, numAllowed is)
