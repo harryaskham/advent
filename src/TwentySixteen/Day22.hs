97f0c62b9498839c6877cb0d1231807f5863bea5
Harry Askham <harryaskham@gmail.com>
2021-02-03 23:33:04 +0000

    Solves 2016-22 by inspection

--- src/TwentySixteen/Day22.hs
+++ src/TwentySixteen/Day22.hs
@@ -15,6 +15,7 @@ import qualified Data.Map.Strict as M
 import Data.Maybe
 import Data.Monoid
 import Data.Ord
+import qualified Data.PQueue.Prio.Min as PQ
 import Data.Sequence (Seq)
 import qualified Data.Sequence as SQ
 import Data.Set (Set)
@@ -33,7 +34,7 @@ data Node = Node
     used :: Int,
     avail :: Int
   }
-  deriving (Show, Eq, Ord)
+  deriving (Eq, Ord)
 
 nodes :: GenParser Char () [Node]
 nodes = do
@@ -74,29 +75,31 @@ part1 = do
   ns <- readWithParser nodes <$> input 2016 22
   return . length . viablePairs $ ns
 
-viableConnectedPairs :: [Node] -> Map Coord2 [Coord2]
-viableConnectedPairs ns =
-  M.fromListWith
-    (++)
-    [ (pos a, [pos b])
-      | (a, b) <- viablePairs ns,
-        manhattan (pos a) (pos b) == 1
-    ]
-
--- TODO: A* with a heuristic that favours both distance to G,
--- and estimates of the cost of opening up the path to G.
--- Simple manhattan to G is not tight enough, but cost of opening path up is quite suble
-fewestSteps :: Map Coord2 Node -> Coord2 -> Int
-fewestSteps nodeMap gPos = go (SQ.singleton (gPos, nodeMap, 0, S.empty, viableConnectedPairs (M.elems nodeMap)))
+astar :: Map Coord2 Node -> Coord2 -> Int
+astar nodeMap gPos =
+  go (PQ.singleton (h gPos (emptyPos nodeMap) 0) (gPos, nodeMap, 0)) S.empty
   where
-    go SQ.Empty = 0
-    go ((gPos, nodeMap, steps, seen, viable) SQ.:<| queue)
+    h gPos@(x, y) (ex, ey) steps = (manhattan (0, 0) gPos, abs (y - ey), abs (x - ex), steps)
+    emptyPos nodeMap = head . M.keys $ M.filter ((== 0) . used) nodeMap
+    go queue seen
+      | null queue = 0
+      | nodeMap `S.member` seen = go rest seen
       | gPos == (0, 0) = steps
-      -- | (gPos, nodeMap) `S.member` seen = go queue seen
       | otherwise =
-        traceShow (steps, gPos, length queue, length nextStates, M.size viable) $
-        go (queue SQ.>< SQ.fromList nextStates)
+        tracePause "" $
+          traceStrLn (pretty nodeMap) $
+            traceShow (steps, gPos, length queue, length nextStates) $
+              go
+                ( foldl'
+                    ( \q st@(gPos, nextNodeMap, _) ->
+                        PQ.insert (h gPos (emptyPos nextNodeMap) (steps + 1)) st q
+                    )
+                    rest
+                    nextStates
+                )
+                (S.insert nodeMap seen)
       where
+        ((_, (gPos, nodeMap, steps)), rest) = PQ.deleteFindMin queue
         moveData (a, b) =
           ( a {used = 0, avail = (avail a + used a)},
             b {used = (used b + used a), avail = (avail b - used a)}
@@ -105,32 +108,42 @@ fewestSteps nodeMap gPos = go (SQ.singleton (gPos, nodeMap, 0, S.empty, viableCo
         nextStates =
           [ ( if pos a == gPos then pos b else gPos,
               nextNodeMap,
-              steps + 1,
-              nextSeen,
-              nextViable
+              steps + 1
             )
-            | aPos <- M.keys viable,
-              bPos <- viable M.! aPos,
-              not ((aPos, bPos) `S.member` seen),
-              let a' = nodeMap M.! aPos
-                  b' = nodeMap M.! bPos,
+            | a' <- M.elems nodeMap,
+              b' <- nodeNeighbors nodeMap a',
               canMove a' b',
               let (a, b) = moveData (a', b')
                   nextNodeMap = M.insert (pos a) a . M.insert (pos b) b $ nodeMap
-                  nextSeen = S.insert (pos a, pos b) seen
-                  nextViable =
-                    --viableConnectedPairs (M.elems nextNodeMap)
-                    viable
-                      & M.delete aPos
-                      & M.delete bPos
-                      & M.unionWith (union) (viableConnectedPairs (a : (nodeNeighbors nextNodeMap a)))
-                      & M.unionWith (intersect) (viableConnectedPairs (b : (nodeNeighbors nextNodeMap b)))
           ]
 
+instance Show Node where
+  show (Node pos used avail)
+    | pos == (30, 0) = "G"
+    | pos == (0, 0) = "S"
+    | used == 0 = "_"
+    | fromIntegral used / fromIntegral (used + avail) < 0.5 = " "
+    | fromIntegral used / fromIntegral (used + avail) < 0.7 = "."
+    | fromIntegral used / fromIntegral (used + avail) < 0.8 = "o"
+    | fromIntegral used / fromIntegral (used + avail) < 0.9 = "O"
+    | otherwise = "#"
+
+-- Solves with A* search - overshoots by 2, needs a better heuristic
+part2_unfinished :: IO Int
+part2_unfinished = do
+  ns <- readWithParser nodes <$> input 2016 22
+  let nodeMap = M.fromList [(pos n, n) | n <- ns]
+      maxX = maximum (fst . pos <$> ns)
+  return $ astar nodeMap (maxX, 0)
+
+-- Solves by inspection. We can only swap with empties.
 part2 :: IO Int
 part2 = do
   ns <- readWithParser nodes <$> input 2016 22
-  --ns <- readWithParser nodes <$> exampleInput 2016 22
   let nodeMap = M.fromList [(pos n, n) | n <- ns]
-      maxX = maximum (fst . pos <$> ns)
-  return $ fewestSteps nodeMap (maxX, 0)
+  return $
+    manhattan (emptyPos nodeMap) (4, 15) -- Distance to gap in "fence"
+      + manhattan (4, 15) (30, 0) -- Distance to target
+      + 5 * 29 -- Empty-looping required to move target to goal.
+  where
+    emptyPos nodeMap = head . M.keys $ M.filter ((== 0) . used) nodeMap
