4e8d276d52fcaedde8e859b68f195fc78f98c205
Harry Askham <harryaskham@gmail.com>
2021-02-04 22:58:58 +0000

    Solves 2015-6

--- src/TwentyFifteen/Day6.hs
+++ src/TwentyFifteen/Day6.hs
@@ -1,32 +1,99 @@
 module TwentyFifteen.Day6 where
 
-import Control.Monad
-import Control.Monad.Memo
-import Coord
-import Data.Bits
-import Data.Char
-import qualified Data.Foldable as F
-import Data.Function
-import Data.List
-import Data.List.Extra
-import Data.Map (Map)
+import Coord (Coord2)
+import Data.List (foldl')
 import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Sequence (Seq)
-import qualified Data.Sequence as SQ
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Tuple.Extra
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Debug.Trace
-import Grid
+import Grid (Grid)
 import Text.ParserCombinators.Parsec
-import Util
+  ( GenParser,
+    char,
+    eof,
+    many,
+    sepBy,
+    string,
+    try,
+    (<|>),
+  )
+import Util (eol, input, number, readWithParser, toTuple2)
+
+data Light = On | Off | Brightness Integer deriving (Eq, Show)
+
+instance Num Light where
+  (Brightness a) + (Brightness b) = Brightness (a + b)
+  negate (Brightness a) = Brightness (negate a)
+  fromInteger a = Brightness a
+
+data Operation
+  = TurnOn (Coord2, Coord2)
+  | TurnOff (Coord2, Coord2)
+  | Toggle (Coord2, Coord2)
+
+operations :: GenParser Char () [Operation]
+operations = many operation <* eof
+  where
+    operation = (on <|> off <|> toggle) <* eol
+    coord2 = toTuple2 <$> number `sepBy` char ','
+    coords = do
+      c1 <- coord2
+      string " through "
+      c2 <- coord2
+      return (c1, c2)
+    on = try $ TurnOn <$> (string "turn on " *> coords)
+    off = try $ TurnOff <$> (string "turn off " *> coords)
+    toggle = try $ Toggle <$> (string "toggle " *> coords)
+
+runOp :: Grid Light -> Operation -> Grid Light
+runOp grid (TurnOn ((x1, y1), (x2, y2))) =
+  foldl'
+    (\g c -> M.insert c On g)
+    grid
+    [(x, y) | x <- [x1 .. x2], y <- [y1 .. y2]]
+runOp grid (TurnOff ((x1, y1), (x2, y2))) =
+  foldl'
+    (\g c -> M.insert c Off g)
+    grid
+    [(x, y) | x <- [x1 .. x2], y <- [y1 .. y2]]
+runOp grid (Toggle ((x1, y1), (x2, y2))) =
+  let toggle On = Off
+      toggle Off = On
+   in foldl'
+        (\g c -> M.adjust toggle c g)
+        grid
+        [(x, y) | x <- [x1 .. x2], y <- [y1 .. y2]]
 
 part1 :: IO Int
-part1 = do
-  ls <- lines <$> input 2015 6
-  return 0
+part1 =
+  M.size
+    . M.filter (== On)
+    . foldl'
+      runOp
+      (M.fromList [((x, y), Off) | x <- [0 .. 999], y <- [0 .. 999]])
+    . readWithParser operations
+    <$> input 2015 6
+
+runOp2 :: Grid Light -> Operation -> Grid Light
+runOp2 grid (TurnOn ((x1, y1), (x2, y2))) =
+  foldl'
+    (\g c -> M.adjust (+ Brightness 1) c g)
+    grid
+    [(x, y) | x <- [x1 .. x2], y <- [y1 .. y2]]
+runOp2 grid (TurnOff ((x1, y1), (x2, y2))) =
+  foldl'
+    (\g c -> M.adjust (\(Brightness a) -> if a < 1 then Brightness a else Brightness a - 1) c g)
+    grid
+    [(x, y) | x <- [x1 .. x2], y <- [y1 .. y2]]
+runOp2 grid (Toggle ((x1, y1), (x2, y2))) =
+  foldl'
+    (\g c -> M.adjust (+ Brightness 2) c g)
+    grid
+    [(x, y) | x <- [x1 .. x2], y <- [y1 .. y2]]
+
+part2 :: IO Light
+part2 =
+  sum
+    . M.elems
+    . foldl'
+      runOp2
+      (M.fromList [((x, y), Brightness 0) | x <- [0 .. 999], y <- [0 .. 999]])
+    . readWithParser operations
+    <$> input 2015 6
