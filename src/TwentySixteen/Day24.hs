e939a77ccd6d36d1e617d56faea59d4f81d321ab
Harry Askham <harryaskham@gmail.com>
2021-02-04 13:37:19 +0000

    Solves 2016-24

--- src/TwentySixteen/Day24.hs
+++ src/TwentySixteen/Day24.hs
@@ -1,41 +1,20 @@
 module TwentySixteen.Day24 where
 
-import Control.Monad
-import Control.Monad.Memo
-import Coord
-import Data.Bits
-import Data.Char
-import qualified Data.Foldable as F
-import Data.Function
-import Data.List
-import Data.List.Extra
+import Coord (Coord2, neighborsNoDiags)
+import Data.Char (digitToInt)
 import Data.Map (Map)
 import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Sequence (Seq)
 import qualified Data.Sequence as SQ
-import Data.Set (Set)
 import qualified Data.Set as S
-import Data.Tuple.Extra
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Debug.Trace
-import Grid
-import Text.ParserCombinators.Parsec
-import Util
+import Grid (Grid, toGrid)
+import Util (input, swapMap, unjust)
 
-data Cell = Wall | Empty (Maybe Int) deriving (Eq, Ord)
+data Cell = Wall | Empty (Maybe Int) deriving (Ord, Eq)
 
 fromChar :: Char -> Cell
 fromChar '#' = Wall
 fromChar '.' = Empty Nothing
 fromChar x = Empty (Just $ digitToInt x)
-
--- All pairs shortest paths between points of interest
--- including the starting point, even though we never need to go back there
--- Then it's dijkstra
 
 onlyNumbers :: Grid Cell -> Grid Cell
 onlyNumbers = M.filter f
@@ -47,49 +26,57 @@ getNumber :: Cell -> Maybe Int
 getNumber (Empty (Just x)) = Just x
 getNumber _ = Nothing
 
-shortestDistancesFrom :: Grid Cell -> Coord2 -> Map Int Int
-shortestDistancesFrom grid start =
-  go (SQ.singleton start) S.empty M.empty
+shortestDistanceBetween :: Grid Cell -> Coord2 -> Coord2 -> Int
+shortestDistanceBetween grid start end =
+  go (SQ.singleton (start, 0)) S.empty
   where
-    numNumbers = M.size (onlyNumbers grid)
-    go SQ.Empty _ distances = distances
-    go (pos SQ.:<| queue) seen distances
-      | M.size distances == numNumbers = distances
-      | pos `S.member` seen = go queue seen distances
+    go ((pos, steps) SQ.:<| queue) seen
+      | pos == end = steps
+      | pos `S.member` seen = go queue seen
       | otherwise =
-        case getNumber (grid M.! pos) of
-          Just x ->
-            if x `M.member` distances
-              then go nextQueue nextSeen distances
-              else go nextQueue nextSeen (M.insert x (S.size seen) distances)
-          Nothing -> go nextQueue nextSeen distances
+        go nextQueue (S.insert pos seen)
       where
-        nextSeen = S.insert pos seen
         nextStates =
-          [ n
+          [ (n, steps + 1)
             | n <- neighborsNoDiags pos,
               M.lookup n grid /= Just Wall
           ]
         nextQueue = queue SQ.>< SQ.fromList nextStates
 
-shortestVisitationFrom :: Map Int (Map Int Int) -> Int -> Int
-shortestVisitationFrom graph start =
+shortestVisitationFrom :: Map Int (Map Int Int) -> Int -> Maybe Int -> Int
+shortestVisitationFrom graph start finalDestination =
   go start (S.singleton start) 0
   where
     go current visited steps
-      | null remaining = traceShow visited $ steps
-      | otherwise = traceShow visited $ minimum nextSteps
+      | null remaining = case finalDestination of
+        Nothing -> steps
+        Just d -> steps + distances M.! d
+      | otherwise = minimum nextSteps
       where
         distances = graph M.! current
         remaining = M.filterWithKey (\k _ -> not (k `S.member` visited)) distances
         nextSteps = (\(i, d) -> go i (S.insert i visited) (steps + d)) <$> M.toList remaining
 
-part1 :: IO Int
-part1 = do
+part12 :: IO (Int, Int)
+part12 = do
   grid <- toGrid fromChar . lines <$> input 2016 24
-  let numCellToPositions = swapMap (onlyNumbers grid)
-      numCellToDistances = shortestDistancesFrom grid <$> numCellToPositions
-      numToDistances = M.mapKeys (unjust . getNumber) numCellToDistances
-  return $ shortestVisitationFrom numToDistances 0
-
--- 5717 too high
+  let numToPosition = M.mapKeys (unjust . getNumber) $ swapMap (onlyNumbers grid)
+      numToDistances =
+        M.fromList
+          [ ( a,
+              M.fromList
+                [ ( b,
+                    shortestDistanceBetween
+                      grid
+                      (numToPosition M.! a)
+                      (numToPosition M.! b)
+                  )
+                  | b <- M.keys numToPosition
+                ]
+            )
+            | a <- M.keys numToPosition
+          ]
+  return $
+    ( shortestVisitationFrom numToDistances 0 Nothing,
+      shortestVisitationFrom numToDistances 0 (Just 0)
+    )
