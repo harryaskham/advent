85e0c199796e904306304a376aea6e1a99f27b0c
Harry Askham <harryaskham@gmail.com>
2021-02-05 11:29:27 +0000

    Solves 2015-7

--- src/TwentyFifteen/Day7.hs
+++ src/TwentyFifteen/Day7.hs
@@ -1,32 +1,77 @@
 module TwentyFifteen.Day7 where
 
-import Control.Monad
-import Control.Monad.Memo
-import Coord
-import Data.Bits
-import Data.Char
-import qualified Data.Foldable as F
-import Data.Function
-import Data.List
-import Data.List.Extra
+import Control.Monad.Memo (Memo, MonadMemo (memo), startEvalMemo)
+import Data.Bits (Bits (complement, shiftL, shiftR, (.&.), (.|.)))
 import Data.Map (Map)
 import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Sequence (Seq)
-import qualified Data.Sequence as SQ
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Tuple.Extra
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Debug.Trace
-import Grid
 import Text.ParserCombinators.Parsec
-import Util
+  ( GenParser,
+    choice,
+    eof,
+    letter,
+    many,
+    many1,
+    string,
+    try,
+    (<|>),
+  )
+import Util (eol, input, number, readWithParser)
 
-part1 :: IO Int
-part1 = do
-  ls <- lines <$> input 2015 7
-  return 0
+data Connection
+  = Assign Value Value
+  | And Value Value Value
+  | Or Value Value Value
+  | Not Value Value
+  | LShift Value Int Value
+  | RShift Value Int Value
+
+data Value = Wire String | Literal Int deriving (Eq, Ord)
+
+connections :: GenParser Char () [Connection]
+connections = many instruction <* eof
+  where
+    instruction = choice (try <$> [assign, and, or, not, lshift, rshift]) <* eol
+    wire = Wire <$> many1 letter
+    literal = Literal <$> number
+    value = wire <|> literal
+    assign = Assign <$> value <* string " -> " <*> wire
+    and = And <$> value <* string " AND " <*> value <* string " -> " <*> wire
+    or = Or <$> value <* string " OR " <*> value <* string " -> " <*> wire
+    not = Not <$> (string "NOT " >> value) <* string " -> " <*> wire
+    lshift = LShift <$> value <* string " LSHIFT " <*> number <* string " -> " <*> wire
+    rshift = RShift <$> value <* string " RSHIFT " <*> number <* string " -> " <*> wire
+
+outputWire :: Connection -> Value
+outputWire (Assign _ v) = v
+outputWire (And _ _ v) = v
+outputWire (Or _ _ v) = v
+outputWire (Not _ v) = v
+outputWire (LShift _ _ v) = v
+outputWire (RShift _ _ v) = v
+
+wireMap :: [Connection] -> Map Value Connection
+wireMap cs = M.fromList [(outputWire c, c) | c <- cs]
+
+resolve :: Map Value Connection -> Value -> Int
+resolve wm wire = startEvalMemo $ go wire
+  where
+    go :: Value -> Memo Value Int Int
+    go (Literal x) = return x
+    go wire =
+      case wm M.! wire of
+        Assign v _ -> memo go v
+        And a b _ -> (.&.) <$> memo go a <*> memo go b
+        Or a b _ -> (.|.) <$> memo go a <*> memo go b
+        Not a _ -> complement <$> memo go a
+        LShift a n _ -> shiftL <$> memo go a <*> pure n
+        RShift a n _ -> shiftR <$> memo go a <*> pure n
+
+part12 :: IO (Int, Int)
+part12 = do
+  wm <- wireMap . readWithParser connections <$> input 2015 7
+  let part1 = resolve wm (Wire "a")
+      part2 =
+        resolve
+          (M.insert (Wire "b") (Assign (Literal part1) (Wire "b")) wm)
+          (Wire "a")
+  return (part1, part2)
