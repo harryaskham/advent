4985b78ce6f5329a46950e7fe6d781dc4b9c0815
Harry Askham <harryaskham@gmail.com>
2021-02-21 16:54:14 +0000

    Solves 2015-11

--- src/TwentyFifteen/Day11.hs
+++ src/TwentyFifteen/Day11.hs
@@ -1,32 +1,38 @@
 module TwentyFifteen.Day11 where
 
-import Control.Monad
-import Control.Monad.Memo
-import Coord
-import Data.Bits
-import Data.Char
-import qualified Data.Foldable as F
-import Data.Function
-import Data.List
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Sequence (Seq)
-import qualified Data.Sequence as SQ
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Tuple.Extra
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Debug.Trace
-import Grid
-import Text.ParserCombinators.Parsec
-import Util
+import Data.Char (chr, ord)
+import Data.List (group)
+import Numeric (readInt, showIntAtBase)
 
-part1 :: IO Int
-part1 = do
-  ls <- lines <$> input 2015 11
-  return 0
+password :: String
+password = "vzbxkghb"
+
+toBase26 :: String -> Integer
+toBase26 = fst . head . readInt 26 (`elem` "abcdefghijklmnopqrstuvwxyz") (\c -> ord c - 97)
+
+fromBase26 :: Integer -> String
+fromBase26 = ($ "") . showIntAtBase 26 (\c -> chr $ c + 97)
+
+increment :: String -> String
+increment = fromBase26 . (+ 1) . toBase26
+
+valid :: String -> Bool
+valid = all (== True) . ([hasThreeStraight, noIOL, hasTwoPairs] <*>) . pure
+
+hasThreeStraight :: String -> Bool
+hasThreeStraight [_, _] = False
+hasThreeStraight (a : b : c : xs)
+  | ord b == ord a + 1 && ord c == ord b + 1 = True
+  | otherwise = hasThreeStraight (b : c : xs)
+
+noIOL :: String -> Bool
+noIOL = not . any (== True) . (elem <$> "iol" <*>) . pure
+
+hasTwoPairs :: String -> Bool
+hasTwoPairs = (>= 2) . length . filter ((>= 2) . length) . group
+
+part1 :: String
+part1 = head . filter valid $ iterate increment password
+
+part2 :: String
+part2 = head . drop 1 . filter valid $ iterate increment password
