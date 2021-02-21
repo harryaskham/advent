70f2f78aeb757225bda5380a0786626205fe267d
Harry Askham <harryaskham@gmail.com>
2021-02-21 18:39:21 +0000

    Solves 2015-12

--- src/TwentyFifteen/Day12.hs
+++ src/TwentyFifteen/Day12.hs
@@ -1,32 +1,58 @@
 module TwentyFifteen.Day12 where
 
-import Control.Monad
-import Control.Monad.Memo
-import Coord
-import Data.Bits
-import Data.Char
-import qualified Data.Foldable as F
-import Data.Function
-import Data.List
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Sequence (Seq)
-import qualified Data.Sequence as SQ
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Tuple.Extra
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Debug.Trace
-import Grid
 import Text.ParserCombinators.Parsec
-import Util
+  ( GenParser,
+    between,
+    char,
+    eof,
+    letter,
+    many,
+    many1,
+    oneOf,
+    sepBy,
+    (<|>),
+  )
+import Util (eol, input, readWithParser)
+
+data Json
+  = Obj [(Json, Json)]
+  | Lst [Json]
+  | StrLit String
+  | NumLit Int
+  deriving (Eq, Ord, Show)
+
+json :: GenParser Char () Json
+json = obj <|> lst <|> strLit <|> numLit
+  where
+    numLit = NumLit . read <$> many (oneOf "-0123456789")
+    strLit = StrLit <$> between (char '"') (char '"') (many1 letter)
+    lst = Lst <$> between (char '[') (char ']') (json `sepBy` char ',')
+    obj = Obj <$> between (char '{') (char '}') (keyVal `sepBy` char ',')
+    keyVal = do
+      k <- strLit
+      char ':'
+      v <- json
+      return (k, v)
+
+sumJson :: Json -> Int
+sumJson (StrLit _) = 0
+sumJson (NumLit x) = x
+sumJson (Lst xs) = sum $ sumJson <$> xs
+sumJson (Obj kvs) = sum $ sumJson . snd <$> kvs
+
+solve :: (Json -> Int) -> IO Int
+solve f = f . readWithParser (json <* eol <* eof) <$> input 2015 12
 
 part1 :: IO Int
-part1 = do
-  ls <- lines <$> input 2015 12
-  return 0
+part1 = solve sumJson
+
+noRedSum :: Json -> Int
+noRedSum (StrLit _) = 0
+noRedSum (NumLit x) = x
+noRedSum (Lst xs) = sum $ noRedSum <$> xs
+noRedSum (Obj kvs)
+  | (StrLit "red") `elem` (snd <$> kvs) = 0
+  | otherwise = sum $ noRedSum . snd <$> kvs
+
+part2 :: IO Int
+part2 = solve noRedSum
