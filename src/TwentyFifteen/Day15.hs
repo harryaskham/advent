edc9b990998321d4ecf92b19b97219ea5ef88d44
Harry Askham <harryaskham@gmail.com>
2021-02-22 11:52:23 +0000

    Solves 2015-15

--- src/TwentyFifteen/Day15.hs
+++ src/TwentyFifteen/Day15.hs
@@ -1,32 +1,72 @@
 module TwentyFifteen.Day15 where
 
-import Control.Monad
-import Control.Monad.Memo
-import Coord
-import Data.Bits
-import Data.Char
-import qualified Data.Foldable as F
-import Data.Function
-import Data.List
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Sequence (Seq)
-import qualified Data.Sequence as SQ
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Tuple.Extra
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Debug.Trace
-import Grid
+import Data.List (transpose)
 import Text.ParserCombinators.Parsec
-import Util
+  ( GenParser,
+    eof,
+    letter,
+    many,
+    many1,
+    oneOf,
+    sepBy,
+    string,
+  )
+import Util (eol, input, number, readWithParser)
+
+type Ingredient = (String, Int, Int, Int, Int, Int)
+
+ingredients :: GenParser Char () [Ingredient]
+ingredients = many line <* eof
+  where
+    line = do
+      name <- many1 letter
+      string ": capacity "
+      [cap, dur, fla, tex, cal] <-
+        number `sepBy` (many (oneOf "abcdefghijklmnopqrstuvwxyz, "))
+      eol
+      return (name, cap, dur, fla, tex, cal)
+
+score :: [Ingredient] -> [Int] -> Int
+score is spoons =
+  product
+    . fmap (\x -> if x < 0 then 0 else x)
+    . fmap sum
+    . transpose
+    $ [ [n * cap, n * dur, n * fla, n * tex]
+        | (n, (_, cap, dur, fla, tex, _)) <- zip spoons is
+      ]
+
+solve :: ([Ingredient] -> [Int] -> Int) -> IO Int
+solve f = do
+  is <- readWithParser ingredients <$> input 2015 15
+  return $
+    maximum
+      [ f is [a, b, c, d]
+        | a <- [0 .. 100],
+          b <- [0 .. 100 - a],
+          c <- [0 .. 100 - a - b],
+          let d = 100 - a - b - c
+      ]
 
 part1 :: IO Int
-part1 = do
-  ls <- lines <$> input 2015 15
-  return 0
+part1 = solve score
+
+score2 :: [Ingredient] -> [Int] -> Int
+score2 is spoons
+  | cals == 500 = score
+  | otherwise = 0
+  where
+    propsCals =
+      [ ([n * cap, n * dur, n * fla, n * tex], n * cal)
+        | (n, (_, cap, dur, fla, tex, cal)) <- zip spoons is
+      ]
+    score =
+      product
+        . fmap (\x -> if x < 0 then 0 else x)
+        . fmap sum
+        . transpose
+        $ fst <$> propsCals
+    cals = sum (snd <$> propsCals)
+
+part2 :: IO Int
+part2 = solve score2
