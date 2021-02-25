1f7116472b23b075720c306d342ee6a07e7cab0a
Harry Askham <harryaskham@gmail.com>
2021-02-25 11:25:10 +0000

    Preps rest of 2019

---
+++ src/TwentyNineteen/Day22.hs
@@ -0,0 +1,32 @@
+module TwentyNineteen.Day22 where
+
+import Control.Monad
+import Control.Monad.Memo
+import Coord
+import Data.Bits
+import Data.Char
+import qualified Data.Foldable as F
+import Data.Function
+import Data.List
+import Data.List.Extra
+import Data.Map (Map)
+import qualified Data.Map.Strict as M
+import Data.Maybe
+import Data.Monoid
+import Data.Ord
+import Data.Sequence (Seq)
+import qualified Data.Sequence as SQ
+import Data.Set (Set)
+import qualified Data.Set as S
+import Data.Tuple.Extra
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import Debug.Trace
+import Grid
+import Text.ParserCombinators.Parsec
+import Util
+
+part1 :: IO Int
+part1 = do
+  ls <- lines <$> input 2019 22
+  return 0
