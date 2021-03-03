5f20cb758325b02c4bb36f8115590c3de3c298d7
Harry Askham <harryaskham@gmail.com>
2021-03-03 17:16:08 +0000

    Solves 2019-23

--- src/TwentyNineteen/Day23.hs
+++ src/TwentyNineteen/Day23.hs
@@ -1,32 +1,107 @@
 module TwentyNineteen.Day23 where
 
-import Control.Monad
-import Control.Monad.Memo
-import Coord
-import Data.Bits
-import Data.Char
-import qualified Data.Foldable as F
-import Data.Function
-import Data.List
-import Data.List.Extra
+import Control.Lens (over, set, view)
+import Data.List (foldl')
 import Data.Map (Map)
 import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Monoid
-import Data.Ord
-import Data.Sequence (Seq)
-import qualified Data.Sequence as SQ
 import Data.Set (Set)
 import qualified Data.Set as S
-import Data.Tuple.Extra
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Debug.Trace
-import Grid
-import Text.ParserCombinators.Parsec
-import Util
+import Data.Tuple.Extra (fst3)
+import System.IO.Unsafe (unsafePerformIO)
+import TwentyNineteen.Intcode
+  ( Machine (Machine),
+    inputs,
+    outputs,
+    readProgram,
+    stepProgram,
+  )
+import Util (toTuple3)
 
-part1 :: IO Int
-part1 = do
-  ls <- lines <$> input 2019 23
-  return 0
+type Network = Map Integer Machine
+
+stepNetwork :: Network -> Integer
+stepNetwork network =
+  case [y | (i, _, y) <- packets, i == 255] of
+    [] -> stepNetwork $ unsafePerformIO . stepProgram <$> network'
+    y : _ -> y
+  where
+    packets =
+      [ toTuple3 os
+        | m <- M.elems network,
+          let os = view outputs m,
+          length os == 3
+      ]
+    replaceEmpty m
+      | null (view inputs m) = set inputs [-1] m
+      | otherwise = m
+    replaceThrees m
+      | length (view outputs m) == 3 = set outputs [] m
+      | otherwise = m
+    receivePacket (x, y) m = over inputs (++ [x, y]) m
+    network' =
+      replaceThrees
+        . replaceEmpty
+        <$> foldl'
+          (\acc (i, x, y) -> M.adjust (receivePacket (x, y)) i acc)
+          network
+          packets
+
+solve :: (Network -> Integer) -> IO Integer
+solve f = do
+  program <- readProgram "input/2019/23.txt"
+  let network = M.fromList [(i, Machine 0 [i] [] program 0) | i <- [0 .. 49]]
+  return $ f network
+
+part1 :: IO Integer
+part1 = solve stepNetwork
+
+stepNetworkNAT :: Int -> Set Integer -> Maybe (Integer, Integer) -> Network -> Integer
+stepNetworkNAT timeSinceLastPacket seen nat network
+  | timeSinceLastPacket > 1000 =
+    let Just (x, y) = nat'
+     in if y `S.member` seen
+          then y
+          else
+            stepNetworkNAT
+              0
+              (S.insert y seen)
+              nat'
+              (M.adjust (receivePacket (x, y)) 0 network')
+  | otherwise =
+    stepNetworkNAT
+      timeSinceLastPacket'
+      seen
+      nat'
+      (unsafePerformIO . stepProgram <$> network')
+  where
+    packets =
+      [ toTuple3 os
+        | m <- M.elems network,
+          let os = view outputs m,
+          length os == 3
+      ]
+    timeSinceLastPacket' = case packets of
+      [] -> timeSinceLastPacket + 1
+      _ -> 0
+    machinePackets = filter ((/= 255) . fst3) packets
+    natPackets = filter ((== 255) . fst3) packets
+    nat' = case reverse natPackets of
+      [] -> nat
+      (_, x, y) : _ -> Just (x, y)
+    replaceEmpty m
+      | null (view inputs m) = set inputs [-1] m
+      | otherwise = m
+    replaceThrees m
+      | length (view outputs m) == 3 = set outputs [] m
+      | otherwise = m
+    receivePacket (x, y) m = over inputs (++ [x, y]) m
+    network' =
+      replaceThrees
+        . replaceEmpty
+        <$> foldl'
+          (\acc (i, x, y) -> M.adjust (receivePacket (x, y)) i acc)
+          network
+          machinePackets
+
+part2 :: IO Integer
+part2 = solve (stepNetworkNAT 0 S.empty Nothing)
