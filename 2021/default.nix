3442b8e257a88be3dc0ca80e1db8c21b16730ec7
Harry Askham <harryaskham@gmail.com>
2021-11-17 14:19:03 +0000

    Adds libs ready for 2021

--- 2021/default.nix
+++ 2021/default.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, containers, lib }:
+{ mkDerivation, array, base, containers, lib, parsec, pqueue, text
+, vector
+}:
 mkDerivation {
   pname = "x2021";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base containers ];
+  executableHaskellDepends = [
+    array base containers parsec pqueue text vector
+  ];
   license = "unknown";
   hydraPlatforms = lib.platforms.none;
 }
