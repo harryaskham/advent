559d805b55b032515d5aaabc5c4e8bfaba7c68be
Harry Askham <harryaskham@gmail.com>
2021-11-18 13:54:30 +0000

    template update

--- 2021/app/Day25.hs
+++ 2021/app/Day25.hs
@@ -1,10 +1,12 @@
 module Day25 where
 
+import qualified Data.Array as A
 import qualified Data.Map.Strict as M
 import qualified Data.PQueue.Prio.Min as PQ
 import qualified Data.Sequence as SQ
 import qualified Data.Set as S
 import Data.Text.Read
+import qualified Data.Vector as V
 import Helper.Coord
 import Helper.Grid
 import Helper.Tracers
