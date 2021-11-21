963fb3863b5bc4322473b371c765a8b6c1c093f1
Harry Askham <harryaskham@gmail.com>
2021-11-21 12:02:31 +0000

    Fix test imports

--- 2021/test/TestSuite.hs
+++ 2021/test/TestSuite.hs
@@ -29,77 +29,77 @@ import Test.HUnit (assertEqual)
 
 main :: IO ()
 main = do
-  assertEqual "Day1 (Part 1)" "Part 1" =<< Day1.part1
-  assertEqual "Day1 (Part 2)" "Part 2" =<< Day1.part2
+  assertEqual "Day 1 (Part 1)" "Part 1" =<< Day1.part1
+  assertEqual "Day 1 (Part 2)" "Part 2" =<< Day1.part2
 
-  assertEqual "Day2 (Part 1)" "Part 1" =<< Day2.part1
-  assertEqual "Day2 (Part 2)" "Part 2" =<< Day2.part2
+  assertEqual "Day 2 (Part 1)" "Part 1" =<< Day2.part1
+  assertEqual "Day 2 (Part 2)" "Part 2" =<< Day2.part2
 
-  assertEqual "Day3 (Part 1)" "Part 1" =<< Day3.part1
-  assertEqual "Day3 (Part 2)" "Part 2" =<< Day3.part2
+  assertEqual "Day 3 (Part 1)" "Part 1" =<< Day3.part1
+  assertEqual "Day 3 (Part 2)" "Part 2" =<< Day3.part2
 
-  assertEqual "Day4 (Part 1)" "Part 1" =<< Day4.part1
-  assertEqual "Day4 (Part 2)" "Part 2" =<< Day4.part2
+  assertEqual "Day 4 (Part 1)" "Part 1" =<< Day4.part1
+  assertEqual "Day 4 (Part 2)" "Part 2" =<< Day4.part2
 
-  assertEqual "Day5 (Part 1)" "Part 1" =<< Day5.part1
-  assertEqual "Day5 (Part 2)" "Part 2" =<< Day5.part2
+  assertEqual "Day 5 (Part 1)" "Part 1" =<< Day5.part1
+  assertEqual "Day 5 (Part 2)" "Part 2" =<< Day5.part2
 
-  assertEqual "Day6 (Part 1)" "Part 1" =<< Day6.part1
-  assertEqual "Day6 (Part 2)" "Part 2" =<< Day6.part2
+  assertEqual "Day 6 (Part 1)" "Part 1" =<< Day6.part1
+  assertEqual "Day 6 (Part 2)" "Part 2" =<< Day6.part2
 
-  assertEqual "Day7 (Part 1)" "Part 1" =<< Day7.part1
-  assertEqual "Day7 (Part 2)" "Part 2" =<< Day7.part2
+  assertEqual "Day 7 (Part 1)" "Part 1" =<< Day7.part1
+  assertEqual "Day 7 (Part 2)" "Part 2" =<< Day7.part2
 
-  assertEqual "Day8 (Part 1)" "Part 1" =<< Day8.part1
-  assertEqual "Day8 (Part 2)" "Part 2" =<< Day8.part2
+  assertEqual "Day 8 (Part 1)" "Part 1" =<< Day8.part1
+  assertEqual "Day 8 (Part 2)" "Part 2" =<< Day8.part2
 
-  assertEqual "Day9 (Part 1)" "Part 1" =<< Day9.part1
-  assertEqual "Day9 (Part 2)" "Part 2" =<< Day9.part2
+  assertEqual "Day 9 (Part 1)" "Part 1" =<< Day9.part1
+  assertEqual "Day 9 (Part 2)" "Part 2" =<< Day9.part2
 
-  assertEqual "Day10 (Part 1)" "Part 1" =<< Day10.part1
-  assertEqual "Day10 (Part 2)" "Part 2" =<< Day10.part2
+  assertEqual "Day 10 (Part 1)" "Part 1" =<< Day10.part1
+  assertEqual "Day 10 (Part 2)" "Part 2" =<< Day10.part2
 
-  assertEqual "Day11 (Part 1)" "Part 1" =<< Day11.part1
-  assertEqual "Day11 (Part 2)" "Part 2" =<< Day11.part2
+  assertEqual "Day 11 (Part 1)" "Part 1" =<< Day11.part1
+  assertEqual "Day 11 (Part 2)" "Part 2" =<< Day11.part2
 
-  assertEqual "Day12 (Part 1)" "Part 1" =<< Day12.part1
-  assertEqual "Day12 (Part 2)" "Part 2" =<< Day12.part2
+  assertEqual "Day 12 (Part 1)" "Part 1" =<< Day12.part1
+  assertEqual "Day 12 (Part 2)" "Part 2" =<< Day12.part2
 
-  assertEqual "Day13 (Part 1)" "Part 1" =<< Day13.part1
-  assertEqual "Day13 (Part 2)" "Part 2" =<< Day13.part2
+  assertEqual "Day 13 (Part 1)" "Part 1" =<< Day13.part1
+  assertEqual "Day 13 (Part 2)" "Part 2" =<< Day13.part2
 
-  assertEqual "Day14 (Part 1)" "Part 1" =<< Day14.part1
-  assertEqual "Day14 (Part 2)" "Part 2" =<< Day14.part2
+  assertEqual "Day 14 (Part 1)" "Part 1" =<< Day14.part1
+  assertEqual "Day 14 (Part 2)" "Part 2" =<< Day14.part2
 
-  assertEqual "Day15 (Part 1)" "Part 1" =<< Day15.part1
-  assertEqual "Day15 (Part 2)" "Part 2" =<< Day15.part2
+  assertEqual "Day 15 (Part 1)" "Part 1" =<< Day15.part1
+  assertEqual "Day 15 (Part 2)" "Part 2" =<< Day15.part2
 
-  assertEqual "Day16 (Part 1)" "Part 1" =<< Day16.part1
-  assertEqual "Day16 (Part 2)" "Part 2" =<< Day16.part2
+  assertEqual "Day 16 (Part 1)" "Part 1" =<< Day16.part1
+  assertEqual "Day 16 (Part 2)" "Part 2" =<< Day16.part2
 
-  assertEqual "Day17 (Part 1)" "Part 1" =<< Day17.part1
-  assertEqual "Day17 (Part 2)" "Part 2" =<< Day17.part2
+  assertEqual "Day 17 (Part 1)" "Part 1" =<< Day17.part1
+  assertEqual "Day 17 (Part 2)" "Part 2" =<< Day17.part2
 
-  assertEqual "Day18 (Part 1)" "Part 1" =<< Day18.part1
-  assertEqual "Day18 (Part 2)" "Part 2" =<< Day18.part2
+  assertEqual "Day 18 (Part 1)" "Part 1" =<< Day18.part1
+  assertEqual "Day 18 (Part 2)" "Part 2" =<< Day18.part2
 
-  assertEqual "Day19 (Part 1)" "Part 1" =<< Day19.part1
-  assertEqual "Day19 (Part 2)" "Part 2" =<< Day19.part2
+  assertEqual "Day 19 (Part 1)" "Part 1" =<< Day19.part1
+  assertEqual "Day 19 (Part 2)" "Part 2" =<< Day19.part2
 
-  assertEqual "Day20 (Part 1)" "Part 1" =<< Day20.part1
-  assertEqual "Day20 (Part 2)" "Part 2" =<< Day20.part2
+  assertEqual "Day 20 (Part 1)" "Part 1" =<< Day20.part1
+  assertEqual "Day 20 (Part 2)" "Part 2" =<< Day20.part2
 
-  assertEqual "Day21 (Part 1)" "Part 1" =<< Day21.part1
-  assertEqual "Day21 (Part 2)" "Part 2" =<< Day21.part2
+  assertEqual "Day 21 (Part 1)" "Part 1" =<< Day21.part1
+  assertEqual "Day 21 (Part 2)" "Part 2" =<< Day21.part2
 
-  assertEqual "Day22 (Part 1)" "Part 1" =<< Day22.part1
-  assertEqual "Day22 (Part 2)" "Part 2" =<< Day22.part2
+  assertEqual "Day 22 (Part 1)" "Part 1" =<< Day22.part1
+  assertEqual "Day 22 (Part 2)" "Part 2" =<< Day22.part2
 
-  assertEqual "Day23 (Part 1)" "Part 1" =<< Day23.part1
-  assertEqual "Day23 (Part 2)" "Part 2" =<< Day23.part2
+  assertEqual "Day 23 (Part 1)" "Part 1" =<< Day23.part1
+  assertEqual "Day 23 (Part 2)" "Part 2" =<< Day23.part2
 
-  assertEqual "Day24 (Part 1)" "Part 1" =<< Day24.part1
-  assertEqual "Day24 (Part 2)" "Part 2" =<< Day24.part2
+  assertEqual "Day 24 (Part 1)" "Part 1" =<< Day24.part1
+  assertEqual "Day 24 (Part 2)" "Part 2" =<< Day24.part2
 
-  assertEqual "Day25 (Part 1)" "Part 1" =<< Day25.part1
-  assertEqual "Day25 (Part 2)" "Part 2" =<< Day25.part2
+  assertEqual "Day 25 (Part 1)" "Part 1" =<< Day25.part1
+  assertEqual "Day 25 (Part 2)" "Part 2" =<< Day25.part2
