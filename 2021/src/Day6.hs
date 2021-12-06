3daf41496ba7b1fdb71e1a9fcb68e52a2cb38099
Harry Askham <harryaskham@gmail.com>
2021-12-06 15:51:03 +0000

    Day6 Int -> Integer

--- 2021/src/Day6.hs
+++ 2021/src/Day6.hs
@@ -5,18 +5,18 @@ import Helper.TH (input)
 import Helper.Util (eol, number, parseWith)
 import Text.ParserCombinators.Parsec (char, eof, sepBy)
 
-fish :: [Int]
+fish :: [Integer]
 fish = parseWith (number `sepBy` char ',' <* (eol >> eof)) $(input 6)
 
-progeny :: Int -> Int
+progeny :: Integer -> Integer
 progeny d' = startEvalMemo (sum <$> mapM (memo go . (d',)) fish)
   where
     go (0, _) = return 1
     go (d, 0) = (+) <$> memo go (d - 1, 6) <*> memo go (d - 1, 8)
     go (d, f) = memo go (d - 1, f - 1)
 
-part1 :: Int
+part1 :: Integer
 part1 = progeny 80
 
-part2 :: Int
+part2 :: Integer
 part2 = progeny 256
