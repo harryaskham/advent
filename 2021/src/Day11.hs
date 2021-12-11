0ddee38eb3307d209ca16a3dde4423659e87d59b
Harry Askham <harryaskham@gmail.com>
2021-12-11 13:07:21 +0000

    Day 11 cleanup

--- 2021/src/Day11.hs
+++ 2021/src/Day11.hs
@@ -18,12 +18,10 @@ step (g, flashes) = second ((+ flashes) . S.size) (go ((+ 1) <$> g) S.empty)
       | otherwise = uncurry go (foldl' flash (g, flashed) toFlash)
       where
         toFlash = [p | p <- flippedLookupM minBound g, not (p `S.member` flashed)]
-        flash (g, flashed) p
-          | p `S.member` flashed = (g, flashed)
-          | otherwise =
-            ( foldl' (flip (M.adjust flashIncrement)) g (neighbors p),
-              S.insert p flashed
-            )
+        flash (g, flashed) p =
+          ( foldl' (flip (M.adjust flashIncrement)) g (neighbors p),
+            S.insert p flashed
+          )
 
 steps :: [(Grid DigitCell, Int)]
 steps = iterate step (readGrid $(input 11), 0)
@@ -33,6 +31,6 @@ part1 = snd (steps !! 100)
 
 part2 :: Int
 part2 =
-  let fs = snd <$> steps
-      diffs = uncurry (-) <$> zip (drop 1 fs) fs
+  let flashes = snd <$> steps
+      diffs = uncurry (-) <$> zip (drop 1 flashes) flashes
    in fst . (!! 0) . dropWhile ((/= 100) . snd) $ zip [1 ..] diffs
