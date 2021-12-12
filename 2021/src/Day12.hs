f79e12222952946dc395d4fa06230e80039a9047
Harry Askham <harryaskham@gmail.com>
2021-12-12 09:32:24 +0000

    Solves Day12

--- 2021/src/Day12.hs
+++ 2021/src/Day12.hs
@@ -1,50 +1,63 @@
 module Day12 (part1, part2) where
 
-import Data.Array qualified as A
-import Data.Bimap (Bimap)
-import Data.Bimap qualified as BM
+import Data.Char (isLower, isUpper)
 import Data.Map.Strict qualified as M
-import Data.Mod
-import Data.PQueue.Prio.Min qualified as PQ
 import Data.Sequence qualified as SQ
 import Data.Set qualified as S
-import Data.Text qualified as T
-import Data.Text.Read
-import Data.Vector qualified as V
-import Helper.Coord
-import Helper.Grid
-import Helper.TH
-import Helper.Tracers
-import Helper.Util
-import Text.ParserCombinators.Parsec
+import Helper.TH (input)
+import Helper.Util (adjustWithDefault, count, parseLinesWith, toTuple2)
+import Text.ParserCombinators.Parsec (GenParser, char, letter, many1, sepBy)
 
--- parser :: GenParser Char () [Int]
--- parser = many1 (number <* eol) <* eof
+data Node
+  = Start
+  | End
+  | Big String
+  | Small String
+  deriving (Eq, Ord, Show)
 
--- line :: GenParser Char () Int
--- line = number
+toNode :: String -> Maybe Node
+toNode "start" = Just Start
+toNode "end" = Just End
+toNode n
+  | all isUpper n = Just (Big n)
+  | all isLower n = Just (Small n)
+  | otherwise = Nothing
 
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
+isSmall :: Node -> Bool
+isSmall (Small _) = True
+isSmall _ = False
 
--- instance GridCell Cell where
---   charMap =
---     BM.fromList
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
+line :: GenParser Char () (Node, Node)
+line = toTuple2 <$> (mapMaybe toNode <$> many1 letter `sepBy` char '-')
 
-part1 :: Text
-part1 =
+allPaths :: (Map Node Int -> Bool) -> Map Node [Node] -> [[Node]]
+allPaths validVisitCounts g = go (SQ.singleton (Start, [], M.empty)) S.empty []
+  where
+    go SQ.Empty _ paths = reverse <$> paths
+    go (st@(current, path, visitCounts) SQ.:<| queue) seen paths
+      | current == End = go queue seen' (path' : paths)
+      | st `S.member` seen = go queue seen paths
+      | current == Start && not (null path) = go queue seen paths
+      | validVisitCounts visitCounts' = go queue' seen' paths
+      | otherwise = go queue seen paths
+      where
+        path' = current : path
+        seen' = S.insert st seen
+        visitCounts' = adjustWithDefault 0 (+ 1) current visitCounts
+        next = case M.lookup current g of
+          Nothing -> SQ.Empty
+          Just ns -> SQ.fromList (ns <&> (,path',visitCounts'))
+        queue' = queue SQ.>< next
+
+graph :: Map Node [Node]
+graph =
   $(input 12)
-    -- & readAs (signed decimal)
-    -- & parseWith parser
-    -- & parseLinesWith line
-    -- & lines
-    -- & readGrid
-    & (<> "Part 1")
+    & parseLinesWith line
+    & concatMap (\(a, b) -> [(a, [b]), (b, [a])])
+    & M.fromListWith (++)
 
-part2 :: Text
-part2 = "Part 2"
+part1 :: Int
+part1 = graph & allPaths ((== 0) . M.size . (M.filterWithKey (\k v -> isSmall k && v > 1))) & length
+
+part2 :: Int
+part2 = graph & allPaths ((== (True, True)) . (((<= 1) . Helper.Util.count (== 2)) &&& (all (== 1) . filter (/= 2))) . M.elems . M.filterWithKey (\k v -> isSmall k)) & length
