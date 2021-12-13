44e78cead452e6404244535576a93504c9880d54
Harry Askham <harryaskham@gmail.com>
2021-12-13 12:08:45 +0000

    Everything done in the parser

--- 2021/src/Day13.hs
+++ 2021/src/Day13.hs
@@ -1,23 +1,25 @@
 module Day13 (part1, part2) where
 
 import Control.Arrow (app)
+import Data.Foldable (foldl1)
 import Data.List ((!!))
+import Data.List.NonEmpty qualified as NE
 import Data.Set qualified as S
 import Helper.Grid (SimpleWall (Wall), fromCoords, pretty)
 import Helper.TH (input)
 import Helper.Util (eol, number, parseWith, toTuple2)
 import Text.ParserCombinators.Parsec (GenParser, char, eof, many1, sepBy1, string)
 
-parser :: GenParser Char () ([Set (Int, Int) -> Set (Int, Int)], Set (Int, Int))
-parser =
+parser :: GenParser Char () (NonEmpty (Set (Int, Int)))
+parser = do
   let f' e m c = S.map (\p -> if e p > c then m ((+ c * 2) . negate) p else p)
       f ch e m = string (ch : "=") *> (f' e m <$> number)
-   in flip (,)
-        <$> (S.fromList <$> (many1 (toTuple2 <$> number `sepBy1` char ',' <* eol) <* eol))
-        <*> (many1 ((string "fold along " >> (f 'x' fst first <|> f 'y' snd second)) <* eol) <* eof)
+  dots <- S.fromList <$> (many1 (toTuple2 <$> number `sepBy1` char ',' <* eol) <* eol)
+  folds <- many1 ((string "fold along " >> (f 'x' fst first <|> f 'y' snd second)) <* eol) <* eof
+  return $ NE.fromList (scanl' (&) dots folds)
 
 part1 :: Int
-part1 = $(input 13) & parseWith parser & first (!! 0) & app & S.size
+part1 = $(input 13) & parseWith parser & (NE.!! 1) & S.size
 
 part2 :: Text
-part2 = $(input 13) & parseWith parser & uncurry (flip (foldl' (&))) & fromCoords Wall & pretty
+part2 = $(input 13) & parseWith parser & last & fromCoords Wall & pretty
