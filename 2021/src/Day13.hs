513480edf5afee1293274cfa14fc7ba080b9b15c
Harry Askham <harryaskham@gmail.com>
2021-12-13 07:41:33 +0000

    Day13 cleanup

--- 2021/src/Day13.hs
+++ 2021/src/Day13.hs
@@ -1,52 +1,21 @@
 module Day13 (part1, part2) where
 
-import Data.Array qualified as A
-import Data.Bimap (Bimap)
-import Data.Bimap qualified as BM
 import Data.List ((!!))
-import Data.Map.Strict qualified as M
-import Data.Mod
-import Data.PQueue.Prio.Min qualified as PQ
-import Data.Sequence qualified as SQ
 import Data.Set qualified as S
-import Data.Text qualified as T
-import Data.Text.Read
-import Data.Vector qualified as V
-import Helper.Coord
-import Helper.Grid
-import Helper.TH
-import Helper.Tracers
-import Helper.Util
-import Text.ParserCombinators.Parsec
-import Prelude hiding ((<|>))
+import Helper.Grid (SimpleWall (Wall), fromCoords, pretty)
+import Helper.TH (input)
+import Helper.Util (eol, number, parseWith, toTuple2)
+import Text.ParserCombinators.Parsec (GenParser, char, eof, many1, sepBy1, string)
 
 data Fold = FoldX Int | FoldY Int deriving (Eq, Ord, Show)
 
 parser :: GenParser Char () (Set (Int, Int), [Fold])
 parser = do
-  dots <- many1 (number `sepBy1` char ',' <* eol) <* eol
-  folds <- many1 do
-    string "fold along "
-    f <- (string "x=" *> (FoldX <$> number)) <|> (string "y=" *> (FoldY <$> number))
-    eol
-    return f
-  eof
-  return (S.fromList (toTuple2 <$> dots), folds)
-
--- line :: GenParser Char () Int
--- line = number
-
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
-
--- instance GridCell Cell where
---   charMap =
---     BM.fromList
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
+  let fX = string "x=" *> (FoldX <$> number)
+      fY = string "y=" *> (FoldY <$> number)
+  dots <- many1 (toTuple2 <$> number `sepBy1` char ',' <* eol) <* eol
+  folds <- many1 ((string "fold along " >> (fX <|> fY)) <* eol) <* eof
+  return (S.fromList dots, folds)
 
 doFold :: Set (Int, Int) -> Fold -> Set (Int, Int)
 doFold ps (FoldX c) = S.map (\(x, y) -> if x > c then (c * 2 - x, y) else (x, y)) ps
