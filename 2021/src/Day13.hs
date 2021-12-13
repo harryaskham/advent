3cc9c2cffeafb2daa7ea87ca5efe6393aa4bff96
Harry Askham <harryaskham@gmail.com>
2021-12-13 12:51:55 +0000

    Everything done in the parser

--- 2021/src/Day13.hs
+++ 2021/src/Day13.hs
@@ -8,12 +8,11 @@ import Helper.Util (appWhen, eol, number, parseWith, toTuple2)
 import Text.ParserCombinators.Parsec (GenParser, char, eof, many1, sepBy1, string)
 
 parser :: GenParser Char () (NonEmpty (Set (Int, Int)))
-parser = do
-  let f' e m c = S.map (appWhen ((> c) . e) (m ((+ c * 2) . negate)))
-      f ch e m = string (ch : "=") *> (f' e m <$> number)
-  dots <- S.fromList <$> (many1 (toTuple2 <$> number `sepBy1` char ',' <* eol) <* eol)
-  folds <- many1 ((string "fold along " >> (f 'x' fst first <|> f 'y' snd second)) <* eol) <* eof
-  return $ NE.fromList (scanl' (&) dots folds)
+parser =
+  let f ch e m = string (ch : "=") >> ((\c -> S.map (appWhen ((> c) . e) (m ((+ c * 2) . negate)))) <$> number)
+   in fmap NE.fromList . scanl' (&)
+        <$> (S.fromList <$> (many1 (toTuple2 <$> number `sepBy1` char ',' <* eol) <* eol))
+        <*> (many1 ((string "fold along " >> (f 'x' fst first <|> f 'y' snd second)) <* eol) <* eof)
 
 part1 :: Int
 part1 = $(input 13) & parseWith parser & (NE.!! 1) & S.size
