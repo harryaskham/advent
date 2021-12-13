941b21adbf34510eed3f889eece3a7f70e5c8791
Harry Askham <harryaskham@gmail.com>
2021-12-13 07:58:15 +0000

    Day13 cleanup

--- 2021/src/Day13.hs
+++ 2021/src/Day13.hs
@@ -1,5 +1,6 @@
 module Day13 (part1, part2) where
 
+import Control.Arrow (app)
 import Data.List ((!!))
 import Data.Set qualified as S
 import Helper.Grid (SimpleWall (Wall), fromCoords, pretty)
@@ -7,22 +8,16 @@ import Helper.TH (input)
 import Helper.Util (eol, number, parseWith, toTuple2)
 import Text.ParserCombinators.Parsec (GenParser, char, eof, many1, sepBy1, string)
 
-data Fold = FoldX Int | FoldY Int deriving (Eq, Ord, Show)
-
-parser :: GenParser Char () (Set (Int, Int), [Fold])
+parser :: GenParser Char () ([Set (Int, Int) -> Set (Int, Int)], Set (Int, Int))
 parser = do
-  let fX = string "x=" *> (FoldX <$> number)
-      fY = string "y=" *> (FoldY <$> number)
+  let fX = string "x=" *> ((\c -> S.map (\(x, y) -> if x > c then (c * 2 - x, y) else (x, y))) <$> number)
+      fY = string "y=" *> ((\c -> S.map (\(x, y) -> if y > c then (x, c * 2 - y) else (x, y))) <$> number)
   dots <- many1 (toTuple2 <$> number `sepBy1` char ',' <* eol) <* eol
   folds <- many1 ((string "fold along " >> (fX <|> fY)) <* eol) <* eof
-  return (S.fromList dots, folds)
-
-doFold :: Set (Int, Int) -> Fold -> Set (Int, Int)
-doFold ps (FoldX c) = S.map (\(x, y) -> if x > c then (c * 2 - x, y) else (x, y)) ps
-doFold ps (FoldY c) = S.map (\(x, y) -> if y > c then (x, c * 2 - y) else (x, y)) ps
+  return (folds, S.fromList dots)
 
 part1 :: Int
-part1 = $(input 13) & parseWith parser & second (!! 0) & uncurry doFold & S.size
+part1 = $(input 13) & parseWith parser & first (!! 0) & app & S.size
 
 part2 :: Text
-part2 = $(input 13) & parseWith parser & uncurry (foldl' doFold) & fromCoords Wall & pretty
+part2 = $(input 13) & parseWith parser & uncurry (flip (foldl' (&))) & fromCoords Wall & pretty
