aefa0b146c44bb033da3f485ac637eb4877b11d9
Harry Askham <harryaskham@gmail.com>
2021-12-13 11:38:56 +0000

    Shorter parser

--- 2021/src/Day13.hs
+++ 2021/src/Day13.hs
@@ -9,13 +9,12 @@ import Helper.Util (eol, number, parseWith, toTuple2)
 import Text.ParserCombinators.Parsec (GenParser, char, eof, many1, sepBy1, string)
 
 parser :: GenParser Char () ([Set (Int, Int) -> Set (Int, Int)], Set (Int, Int))
-parser = do
-  let f e m c = S.map (\p -> if e p > c then m ((+ c * 2) . negate) p else p)
-      fX = string "x=" *> (f fst first <$> number)
-      fY = string "y=" *> (f snd second <$> number)
-  dots <- many1 (toTuple2 <$> number `sepBy1` char ',' <* eol) <* eol
-  folds <- many1 ((string "fold along " >> (fX <|> fY)) <* eol) <* eof
-  return (folds, S.fromList dots)
+parser =
+  let f' e m c = S.map (\p -> if e p > c then m ((+ c * 2) . negate) p else p)
+      f ch e m = string (ch : "=") *> (f' e m <$> number)
+   in flip (,)
+        <$> (S.fromList <$> (many1 (toTuple2 <$> number `sepBy1` char ',' <* eol) <* eol))
+        <*> (many1 ((string "fold along " >> (f 'x' fst first <|> f 'y' snd second)) <* eol) <* eof)
 
 part1 :: Int
 part1 = $(input 13) & parseWith parser & first (!! 0) & app & S.size
