ceff975cb27f66fe42c3a444dcfa936be6ce9fd8
Harry Askham <harryaskham@gmail.com>
2021-12-15 17:35:31 +0000

    Move end into grid extension

--- 2021/src/Day15.hs
+++ 2021/src/Day15.hs
@@ -8,10 +8,10 @@ import Helper.Grid (DigitCell, Grid, cellToInt, incrementMod9, maxXY, readGrid)
 import Helper.TH (input)
 import Helper.Util (both)
 
-lowestRisk :: Grid DigitCell -> Int -> (Int, Int) -> Maybe Int
-lowestRisk g n end = go (PQ.singleton 0 (0, 0)) M.empty
+lowestRisk :: Grid DigitCell -> Int -> Maybe Int
+lowestRisk g n = go (PQ.singleton 0 (0, 0)) M.empty
   where
-    (member, lookup) = extendGrid g n
+    (member, lookup, end) = extendGrid g n
     go queue lowestRiskAt
       | null queue = Nothing
       | pos == end = Just risk'
@@ -30,22 +30,22 @@ lowestRisk g n end = go (PQ.singleton 0 (0, 0)) M.empty
         queue' = foldl' (flip (PQ.insert risk')) rest next
         lowestRiskAt' = M.insertWith min pos risk lowestRiskAt
 
-extendGrid :: Grid DigitCell -> Int -> ((Int, Int) -> Bool, (Int, Int) -> Int)
-extendGrid g n = (member, lookup)
+extendGrid :: Grid DigitCell -> Int -> ((Int, Int) -> Bool, (Int, Int) -> Int, (Int, Int))
+extendGrid g n = (member, lookup, (w * n - 1, h * n - 1))
   where
     (w, h) = both (+ 1) (maxXY g)
     lookup (x, y) =
-      let (xMod, x') = x `divMod` w
-          (yMod, y') = y `divMod` h
-       in fromInteger $ cellToInt (incrementMod9 (xMod + yMod) (g M.! (x', y')))
+      let (xi, x') = x `divMod` w
+          (yi, y') = y `divMod` h
+       in fromInteger $ cellToInt (incrementMod9 (xi + yi) (g M.! (x', y')))
     member (x, y) = x >= 0 && y >= 0 && x < w * n && y < h * n
 
 part1 :: Maybe Int
 part1 =
   let g = readGrid $(input 15)
-   in lowestRisk g 1 (99, 99)
+   in lowestRisk g 1
 
 part2 :: Maybe Int
 part2 =
   let g = readGrid $(input 15)
-   in lowestRisk g 5 (499, 499)
+   in lowestRisk g 5
