ea7882e1a760d056aeb11a23633bd792a58d1fbb
Harry Askham <harryaskham@gmail.com>
2021-12-15 17:31:07 +0000

    Logically extends rather than generating grid, saves anotehr half second

--- 2021/src/Day15.hs
+++ 2021/src/Day15.hs
@@ -1,20 +1,21 @@
 module Day15 (part1, part2) where
 
-import Data.List ((!!))
 import Data.Map.Strict qualified as M
 import Data.PQueue.Prio.Min qualified as PQ
 import Data.Set qualified as S
-import Helper.Coord (manhattan, neighborsNoDiags)
-import Helper.Grid (DigitCell, Grid, cellToInt, incrementMod9, intToCell, joinGrids, maxXY, readGrid)
+import Helper.Coord (neighborsNoDiags)
+import Helper.Grid (DigitCell, Grid, cellToInt, incrementMod9, maxXY, readGrid)
 import Helper.TH (input)
+import Helper.Util (both)
 
-lowestRisk :: Grid DigitCell -> Maybe Int
-lowestRisk g = go (PQ.singleton 0 (0, 0)) M.empty
+lowestRisk :: Grid DigitCell -> Int -> (Int, Int) -> Maybe Int
+lowestRisk g n end = go (PQ.singleton 0 (0, 0)) M.empty
   where
-    end = maxXY g
+    (member, lookup) = extendGrid g n
     go queue lowestRiskAt
       | null queue = Nothing
       | pos == end = Just risk'
+      | not (member pos) = go rest lowestRiskAt
       | otherwise =
         case M.lookup pos lowestRiskAt of
           Nothing -> go queue' lowestRiskAt'
@@ -24,20 +25,27 @@ lowestRisk g = go (PQ.singleton 0 (0, 0)) M.empty
               else go queue' lowestRiskAt'
       where
         ((risk, pos), rest) = PQ.deleteFindMin queue
-        risk' = if pos == (0, 0) then 0 else risk + fromIntegral (cellToInt (g M.! pos))
-        next = [n | n <- neighborsNoDiags pos, n `M.member` g]
+        risk' = if pos == (0, 0) then 0 else risk + lookup pos
+        next = filter member (neighborsNoDiags pos)
         queue' = foldl' (flip (PQ.insert risk')) rest next
         lowestRiskAt' = M.insertWith min pos risk lowestRiskAt
 
+extendGrid :: Grid DigitCell -> Int -> ((Int, Int) -> Bool, (Int, Int) -> Int)
+extendGrid g n = (member, lookup)
+  where
+    (w, h) = both (+ 1) (maxXY g)
+    lookup (x, y) =
+      let (xMod, x') = x `divMod` w
+          (yMod, y') = y `divMod` h
+       in fromInteger $ cellToInt (incrementMod9 (xMod + yMod) (g M.! (x', y')))
+    member (x, y) = x >= 0 && y >= 0 && x < w * n && y < h * n
+
 part1 :: Maybe Int
-part1 = lowestRisk (readGrid $(input 15))
+part1 =
+  let g = readGrid $(input 15)
+   in lowestRisk g 1 (99, 99)
 
 part2 :: Maybe Int
 part2 =
   let g = readGrid $(input 15)
-      gPlus = [incrementMod9 i <$> g | i <- [0 .. 8]]
-   in lowestRisk . joinGrids . M.fromList $
-        [ ((x, y), gPlus !! (x + y))
-          | x <- [0 .. 4],
-            y <- [0 .. 4]
-        ]
+   in lowestRisk g 5 (499, 499)
