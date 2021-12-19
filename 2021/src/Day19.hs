e74813484ee88f9eee244335fe4c55e6e60b1c71
Harry Askham <harryaskham@gmail.com>
2021-12-19 11:23:42 +0000

    Day 19 cleanup

--- 2021/src/Day19.hs
+++ 2021/src/Day19.hs
@@ -1,15 +1,14 @@
 module Day19 where
 
-import Control.Lens
+import Control.Lens (over)
 import Data.Foldable (foldl1)
 import Data.List qualified as L
 import Data.List.Extra (maximumOn)
 import Data.Map.Strict qualified as M
-import Data.Tuple.Extra (first3, second3, third3)
 import Helper.Coord (manhattan3)
 import Helper.TH (input)
 import Helper.Util (countMap, eol, fromV3, number, parseWith, perms3, permsV3, powerset, toTuple3, toV3)
-import Linear.V3
+import Linear.V3 (R1 (_x), R2 (_y), R3 (_z), V3 (..))
 import Safe (headMay)
 import Text.ParserCombinators.Parsec (GenParser, between, char, eof, many1, sepBy1, string)
 
@@ -30,7 +29,9 @@ reorient (Scanner i bs) = L.nub $ Scanner i <$> [t . p <$> bs | t <- transforms,
   where
     transforms =
       foldl1 (.)
-        <$> filter (not . null) (powerset [id, over _x negate, over _y negate, over _z negate])
+        <$> filter
+          (not . null)
+          (powerset [id, over _x negate, over _y negate, over _z negate])
 
 overlap :: Scanner -> Scanner -> Maybe (Scanner, V3 Int)
 overlap (Scanner i bs) (Scanner _ bs0)
@@ -40,15 +41,16 @@ overlap (Scanner i bs) (Scanner _ bs0)
     (maxDiff, diffCount) = maximumOn snd . M.toList $ countMap $ (-) <$> bs <*> bs0
 
 addOne :: [(Scanner, V3 Int)] -> [Scanner] -> ([(Scanner, V3 Int)], [Scanner])
-addOne scanners0 scanners =
-  case go scanners of
-    Nothing -> (scanners0, scanners)
-    Just (s, pos) -> ((s, pos) : scanners0, filter ((/= scannerID s) . scannerID) scanners)
+addOne ss0 ss =
+  case firstMatch ss of
+    Nothing -> (ss0, ss)
+    Just (s, pos) -> ((s, pos) : ss0, filter ((/= scannerID s) . scannerID) ss)
   where
-    go [] = Nothing
-    go (scanner : scanners) =
-      headMay (catMaybes [overlap s s0 | s <- reorient scanner, (s0, _) <- scanners0])
-        <|> go scanners
+    firstMatch [] = Nothing
+    firstMatch (s : ss) =
+      headMay
+        (catMaybes [overlap s' s0 | s' <- reorient s, (s0, _) <- ss0])
+        <|> firstMatch ss
 
 addAll :: [(Scanner, V3 Int)] -> [Scanner] -> [(Scanner, V3 Int)]
 addAll scanners0 [] = scanners0
@@ -67,5 +69,10 @@ part1 = length (allPoints (fst <$> normalizedScanners))
 
 part2 :: Int
 part2 =
-  let ps = snd <$> normalizedScanners
-   in L.maximum [manhattan3 (fromV3 a) (fromV3 b) | a <- ps, b <- ps, a /= b]
+  L.maximum
+    [ manhattan3 (fromV3 a) (fromV3 b)
+      | let ps = snd <$> normalizedScanners,
+        a <- ps,
+        b <- ps,
+        a /= b
+    ]
