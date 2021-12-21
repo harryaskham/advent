cf24e210ac5a4ecef36e14882c6c20a05dcde823
Harry Askham <harryaskham@gmail.com>
2021-12-21 11:05:04 +0000

    Lens based refactor

--- 2021/src/Day21.hs
+++ 2021/src/Day21.hs
@@ -1,55 +1,46 @@
 module Day21 (part1, part2) where
 
+import Control.Lens (makeLenses, over, view, (^.))
 import Control.Monad.Memo (Memo, MonadMemo (memo), startEvalMemo)
 
 data Game = Game
-  { p1Pos :: Int,
-    p2Pos :: Int,
-    p1Score :: Int,
-    p2Score :: Int,
-    p1Turn :: Bool
+  { _p1Pos :: Int,
+    _p2Pos :: Int,
+    _p1Score :: Int,
+    _p2Score :: Int,
+    _p1Turn :: Bool
   }
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord)
+
+makeLenses ''Game
 
 mkGame :: Int -> Int -> Game
 mkGame p1Pos p2Pos = Game (p1Pos - 1) (p2Pos - 1) 0 0 True
 
-run :: [Int] -> Int -> Game -> Int
-run rolls numRolls game@Game {..}
-  | p1Score >= 1000 = p2Score * numRolls
-  | p2Score >= 1000 = p1Score * numRolls
-  | p1Turn =
-    run rolls' numRolls' $
-      game {p1Pos = p1Pos', p1Score = p1Score', p1Turn = p1Turn'}
-  | otherwise =
-    run rolls' numRolls' $
-      game {p2Pos = p2Pos', p2Score = p2Score', p1Turn = p1Turn'}
+roll :: Game -> Int -> Game
+roll game n
+  | game ^. p1Turn = go p1Pos p1Pos p1Score
+  | otherwise = go p2Pos p2Pos p2Score
   where
-    rollSum = sum $ take 3 rolls
-    rolls' = drop 3 rolls
-    p1Turn' = not p1Turn
-    p1Pos' = (p1Pos + rollSum) `rem` 10
-    p2Pos' = (p2Pos + rollSum) `rem` 10
-    p1Score' = p1Score + p1Pos' + 1
-    p2Score' = p2Score + p2Pos' + 1
-    numRolls' = numRolls + 3
+    go pPos pPos' pScore =
+      game
+        & over pPos ((`rem` 10) . (+ n))
+        & (\g -> over pScore (+ (view pPos' g + 1)) g)
+        & over p1Turn not
+
+run :: [Int] -> Int -> Game -> Int
+run rolls numRolls game
+  | game ^. p1Score >= 1000 = numRolls * game ^. p2Score
+  | game ^. p2Score >= 1000 = numRolls * game ^. p1Score
+  | otherwise = run (drop 3 rolls) (numRolls + 3) $ roll game (sum $ take 3 rolls)
 
 runQuantum :: Game -> Memo Game (Sum Int, Sum Int) (Sum Int, Sum Int)
-runQuantum game@Game {..}
-  | p1Score >= 21 = return (Sum 1, Sum 0)
-  | p2Score >= 21 = return (Sum 0, Sum 1)
-  | otherwise = mconcat <$> traverse (memo runQuantum) (roll <$> rolls)
-  where
-    rolls = [r1 + r2 + r3 | r1 <- [1 .. 3], r2 <- [1 .. 3], r3 <- [1 .. 3]]
-    p1Turn' = not p1Turn
-    roll n
-      | p1Turn = game {p1Pos = p1Pos', p1Score = p1Score', p1Turn = p1Turn'}
-      | otherwise = game {p2Pos = p2Pos', p2Score = p2Score', p1Turn = p1Turn'}
-      where
-        p1Pos' = (p1Pos + n) `rem` 10
-        p2Pos' = (p2Pos + n) `rem` 10
-        p1Score' = p1Score + p1Pos' + 1
-        p2Score' = p2Score + p2Pos' + 1
+runQuantum game
+  | game ^. p1Score >= 21 = return (Sum 1, Sum 0)
+  | game ^. p2Score >= 21 = return (Sum 0, Sum 1)
+  | otherwise =
+    let rolls = [r1 + r2 + r3 | let rs = [1 .. 3], r1 <- rs, r2 <- rs, r3 <- rs]
+     in mconcat <$> traverse (memo runQuantum) (roll game <$> rolls)
 
 part1 :: Int
 part1 = run (cycle [1 .. 100]) 0 (mkGame 9 3)
