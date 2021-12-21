f4744cfef2fb44d3a6cdeb4d6f304b1dbc1ec40e
Harry Askham <harryaskham@gmail.com>
2021-12-21 09:36:58 +0000

    Partial 21

--- 2021/src/Day21.hs
+++ 2021/src/Day21.hs
@@ -1,5 +1,6 @@
 module Day21 (part1, part2) where
 
+import Control.Monad.Memo
 import Data.Array qualified as A
 import Data.Bimap (Bimap)
 import Data.Bimap qualified as BM
@@ -20,51 +21,30 @@ import Helper.Tracers
 import Helper.Util
 import Text.ParserCombinators.Parsec
 
-data Roll = Classical Int | Quantum (Int, Int, Int)
-
 data Game = Game
   { p1Pos :: Int,
     p2Pos :: Int,
     p1Score :: Int,
     p2Score :: Int,
-    p1Turn :: Bool,
-    rolls :: [Roll],
-    numRolls :: Int
+    p1Turn :: Bool
   }
+  deriving (Eq, Ord, Show)
 
-mkGame :: Int -> Int -> [Roll] -> Game
-mkGame p1Pos p2Pos rolls = Game (p1Pos - 1) (p2Pos - 1) 0 0 True rolls 0
+mkGame :: Int -> Int -> Game
+mkGame p1Pos p2Pos = Game (p1Pos - 1) (p2Pos - 1) 0 0 True
 
-inc100 :: Roll -> Roll
-inc100 (Classical 100) = Classical 1
-inc100 (Classical x) = Classical (x + 1)
-inc100 _ = error "Can't increment a quantum die"
-
-run :: Game -> Int
-run game@Game {..}
+run :: [Int] -> Int -> Game -> Int
+run rolls numRolls game@Game {..}
   | p1Score >= 1000 = p2Score * numRolls
   | p2Score >= 1000 = p1Score * numRolls
   | p1Turn =
-    run $
-      game
-        { p1Pos = p1Pos',
-          p1Score = p1Score',
-          p1Turn = p1Turn',
-          numRolls = numRolls',
-          rolls = rolls'
-        }
+    run rolls' numRolls' $
+      game {p1Pos = p1Pos', p1Score = p1Score', p1Turn = p1Turn'}
   | otherwise =
-    run $
-      game
-        { p2Pos = p2Pos',
-          p2Score = p2Score',
-          p1Turn = p1Turn',
-          numRolls = numRolls',
-          rolls = rolls'
-        }
+    run rolls' numRolls' $
+      game {p2Pos = p2Pos', p2Score = p2Score', p1Turn = p1Turn'}
   where
-    [(Classical a), (Classical b), (Classical c)] = take 3 rolls
-    rollSum = a + b + c
+    rollSum = sum $ take 3 rolls
     rolls' = drop 3 rolls
     p1Turn' = not p1Turn
     p1Pos' = (p1Pos + rollSum) `rem` 10
@@ -74,9 +54,34 @@ run game@Game {..}
     numRolls' = numRolls + 3
 
 part1 :: Int
-part1 = run $ mkGame 9 3 (iterate inc100 (Classical 1))
+part1 = run (iterate inc100 1) 0 (mkGame 9 3)
+  where
+    inc100 100 = 1
+    inc100 x = x + 1
 
-runQuantum :: Game -> (Int, Int)
-runQuantum game@Game {..} = undefined
+runQuantum :: Game -> Memo Game (Sum Int, Sum Int) (Sum Int, Sum Int)
+runQuantum game@Game {..}
+  | p1Score >= 21 = return (Sum 1, Sum 0)
+  | p2Score >= 21 = return (Sum 0, Sum 1)
+  | otherwise =
+    traceShow game $ do
+      o1 <- memo runQuantum (roll 1)
+      o2 <- memo runQuantum (roll 2)
+      o3 <- memo runQuantum (roll 3)
+      return $ o1 <> o2 <> o3
+  where
+    --mconcat <$> traverse (memo runQuantum) (roll <$> [1, 2, 3])
 
-part2 = uncurry max . runQuantum $ mkGame 9 3 (repeat (Quantum (1, 2, 3)))
+    p1Turn' = not p1Turn
+    roll n
+      | p1Turn = game {p1Pos = p1Pos', p1Score = p1Score', p1Turn = p1Turn'}
+      | otherwise = game {p2Pos = p2Pos', p2Score = p2Score', p1Turn = p1Turn'}
+      where
+        p1Pos' = (p1Pos + n) `rem` 10
+        p2Pos' = (p2Pos + n) `rem` 10
+        p1Score' = p1Score + p1Pos' + 1
+        p2Score' = p2Score + p2Pos' + 1
+
+part2 :: Sum Int
+--part2 = uncurry max . startEvalMemo . runQuantum $ mkGame 9 3
+part2 = uncurry max . startEvalMemo . runQuantum $ mkGame 4 8
