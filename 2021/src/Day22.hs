1ad11df52edfc7c5d5c6a4afb8a94df7f3a2eea1
Harry Askham <harryaskham@gmail.com>
2021-12-22 19:09:28 +0000

    Cleaner 22

--- 2021/src/Day22.hs
+++ 2021/src/Day22.hs
@@ -6,37 +6,21 @@ import Text.ParserCombinators.Parsec (GenParser, char, string, try)
 
 type Range2 = (Int, Int)
 
-data CuboidState = On | Off deriving (Eq, Ord, Show)
+data CuboidState = On | Off deriving (Eq)
 
 type Cuboid = (Range2, Range2, Range2)
 
-data Instruction = Instruction
-  { instructionType :: CuboidState,
-    instructionCuboid :: Cuboid
-  }
-  deriving (Eq, Ord, Show)
+data Instruction = Instruction CuboidState Cuboid
 
 instruction :: GenParser Char () Instruction
 instruction = do
-  onOff <- try (string "on") <|> try (string "off")
-  string " x="
-  x <- range <* char ','
-  string "y="
-  y <- range <* char ','
-  string "z="
-  z <- range
-  let mkI =
-        case onOff of
-          "on" -> Instruction On
-          "off" -> Instruction Off
+  mkI <- try (string "on" >> return (Instruction On)) <|> try (string "off" >> return (Instruction Off))
+  x <- string " x=" *> range <* char ','
+  y <- string "y=" *> range <* char ','
+  z <- string "z=" *> range
   return $ mkI (x, y, z)
   where
-    range :: GenParser Char () Range2
-    range = do
-      a <- number
-      string ".."
-      b <- number
-      return (a, b + 1)
+    range = (,) <$> (number <* string "..") <*> ((+ 1) <$> number)
 
 runInstruction :: Instruction -> (Int, Int, Int) -> CuboidState -> CuboidState
 runInstruction (Instruction t ((x0, x1), (y0, y1), (z0, z1))) (x, y, z) c
@@ -52,8 +36,8 @@ part1 =
       [((x, y, z), Off) | x <- [-50 .. 50], y <- [-50 .. 50], z <- [-50 .. 50]]
     & count ((== On) . snd)
 
-runI :: [(Cuboid, Int)] -> Instruction -> [(Cuboid, Int)]
-runI seen (Instruction t c@((ax0, ax1), (ay0, ay1), (az0, az1))) =
+accumulate :: [(Cuboid, Int)] -> Instruction -> [(Cuboid, Int)]
+accumulate seen (Instruction t c@((ax0, ax1), (ay0, ay1), (az0, az1))) =
   seen
     ++ ( case t of
            On -> [(c, 1)]
@@ -72,14 +56,13 @@ runI seen (Instruction t c@((ax0, ax1), (ay0, ay1), (az0, az1))) =
            z0 <= z1
        ]
 
+volume :: Cuboid -> Int
+volume ((x0, x1), (y0, y1), (z0, z1)) =
+  abs (x1 - x0) * abs (y1 - y0) * abs (z1 - z0)
+
 part2 :: Int
 part2 =
-  const 1227345351869476 $ -- TODO: Why the difference now?
-    $(input 22)
-      & parseLinesWith instruction
-      & foldl' runI []
-      & foldl'
-        ( \s (((x0, x1), (y0, y1), (z0, z1)), sgn) ->
-            s + sgn * ((x1 - x0 + 1) * (y1 - y0 + 1) * (z1 - z0 + 1))
-        )
-        0
+  $(input 22)
+    & parseLinesWith instruction
+    & foldl' accumulate []
+    & foldl' (\s (c, sgn) -> s + sgn * volume c) 0
