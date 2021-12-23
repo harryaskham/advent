f9e88ef665f1df3ec5bf0036e15e5ff29cdd9876
Harry Askham <harryaskham@gmail.com>
2021-12-23 23:20:52 +0000

    Moved to set but broke part1

--- 2021/src/Day23.hs
+++ 2021/src/Day23.hs
@@ -8,6 +8,7 @@ module Day23 where
 import Data.Array qualified as A
 import Data.Bimap (Bimap)
 import Data.Bimap qualified as BM
+import Data.Foldable (foldl1)
 import Data.List ((!!))
 import Data.List qualified as L
 import Data.Map.Strict qualified as M
@@ -76,20 +77,21 @@ instance GridCell Cell where
 validHallwayDestinations :: Set Coord2
 validHallwayDestinations = S.fromList $ (,1) <$> [1, 2, 4, 6, 8, 9, 10, 11]
 
--- TODO: memo this
-allowedDestinations :: Grid Cell -> Amphipod -> Coord2 -> Map Amphipod [Coord2] -> [(Coord2, Int)]
+type APos = Map Amphipod (Set Coord2)
+
+allowedDestinations :: Grid Cell -> Amphipod -> Coord2 -> APos -> [(Coord2, Int)]
 allowedDestinations g a origin@(ox, oy) aPos = go (SQ.singleton (origin, 0)) S.empty []
   where
-    allAPos = S.fromList (concat (M.elems aPos)) -- faster
-    otherAPos = S.fromList ([p | (a', ps) <- M.toList aPos, a /= a', p <- ps]) -- faster
+    allAPos = foldl1 S.union (M.elems aPos)
+    otherAPos = foldl1 S.union [ps | (a', ps) <- M.toList aPos, a /= a']
     hallWayOrigin = oy == 1
-    validRoomDestinations = S.fromList $ let ds = destinations a in if any (`S.member` otherAPos) ds then [] else ds
+    validRoomDestinations = let others = otherAPos `S.intersection` (destinationMap M.! a) in if null others then destinationMap M.! a S.\\ allAPos else S.empty
     validDestinations = validHallwayDestinations `S.union` validRoomDestinations
     go SQ.Empty _ destinations = destinations
     go ((p, cost) SQ.:<| rest) seen destinations
       | p `S.member` seen = go rest seen destinations
       | hallWayOrigin && p `S.member` validRoomDestinations = go queue seen' ((p, cost) : destinations)
-      | (not hallWayOrigin) && p `S.member` validDestinations = go queue seen' ((p, cost) : destinations)
+      | not hallWayOrigin && p `S.member` validDestinations = go queue seen' ((p, cost) : destinations)
       | otherwise = go queue seen' destinations
       where
         seen' = S.insert p seen
@@ -122,8 +124,10 @@ solve g = go (PQ.singleton 0 (positions g, 0)) S.empty
   where
     go queue seen
       | PQ.null queue = Nothing
-      | aPos `S.member` seen = go rest seen
       | organized aPos = Just pathCost
+      | aPos `S.member` seen =
+        traceShow ("cache hit") $
+          go rest seen
       | otherwise =
         traceShow ("pathCost", pathCost, "pq cost", cost, "q size", PQ.size queue, "seen size", S.size seen) $
           -- traceTextLn (prettyA aPos)
@@ -131,31 +135,38 @@ solve g = go (PQ.singleton 0 (positions g, 0)) S.empty
       where
         ((cost, (aPos, pathCost)), rest) = PQ.deleteFindMin queue
         seen' = S.insert aPos seen
-        move p d a aPos = M.adjust (L.delete p . (d :)) a aPos
+        move p d a aPos = M.adjust (S.delete p . S.insert d) a aPos
         states =
           [ (aPos', pathCost + dist)
             | (a, ps) <- M.toList aPos,
-              p <- ps,
+              p <- S.toList ps,
               (d, dist) <- allowedDestinations g a p aPos,
               let aPos' = move p d a aPos
           ]
         minDistanceToDest (x, y) a
           | x == dx && y > 1 = 0
           | x == dx && y == 1 = 1
-          | otherwise = (y - 1) + abs (x - dx) + 1
+          | otherwise = y - 1 + abs (x - dx) + 1
           where
-            ((dx, _) : _) = destinations a
-        h aPos = sum [energy a * minDistanceToDest p a | a <- enumerate, p <- aPos M.! a]
+            dx = fst . L.head . S.toList $ destinationMap M.! a
+        minDistanceToDest' (x, y) a = abs (dx - x)
+          where
+            dx = fst . L.head . S.toList $ destinationMap M.! a
+        --h aPos = sum [energy a * minDistanceToDest p a | a <- enumerate, p <- S.toList (aPos M.! a)]
+        h aPos = sum [energy a * minDistanceToDest' p a | a <- enumerate, p <- S.toList (aPos M.! a)]
         queue' = foldl' (flip (PQ.insert (pathCost + h aPos))) rest states
 
-destinations :: Amphipod -> [Coord2]
-destinations Amber = (3,) <$> [2 .. 5]
-destinations Bronze = (5,) <$> [2 .. 5]
-destinations Copper = (7,) <$> [2 .. 5]
-destinations Desert = (9,) <$> [2 .. 5]
+destinationMap :: Map Amphipod (Set Coord2)
+destinationMap =
+  M.fromList
+    [ (Amber, S.fromList $ (3,) <$> [2 .. 5]),
+      (Bronze, S.fromList $ (5,) <$> [2 .. 5]),
+      (Copper, S.fromList $ (7,) <$> [2 .. 5]),
+      (Desert, S.fromList $ (9,) <$> [2 .. 5])
+    ]
 
-positions :: Grid Cell -> Map Amphipod [Coord2]
-positions g = M.fromList [(a, find (Full a) g) | a <- enumerate]
+positions :: Grid Cell -> APos
+positions g = M.fromList [(a, S.fromList $ find (Full a) g) | a <- enumerate]
 
 energy :: Amphipod -> Int
 energy Amber = 1
@@ -163,11 +174,8 @@ energy Bronze = 10
 energy Copper = 100
 energy Desert = 1000
 
-organized :: Map Amphipod [Coord2] -> Bool
-organized aPos =
-  all
-    (\a -> (aPos M.! a) `L.intersect` destinations a == (aPos M.! a))
-    enumerate
+organized :: APos -> Bool
+organized aPos = all (\a -> aPos M.! a == destinationMap M.! a) enumerate
 
 exx =
   [s|
@@ -254,6 +262,6 @@ part1 =
     & solve
 
 part2 =
-  (readGrid maze2 :: Grid Cell)
+  (readGrid exx2 :: Grid Cell)
     & fillDef None
     & solve
