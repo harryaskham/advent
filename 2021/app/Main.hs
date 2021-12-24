01ee4f85448ab4f54fafe9bb9c0c72cee149d291
Harry Askham <harryaskham@gmail.com>
2021-12-24 14:47:29 +0000

    cleanup

--- 2021/app/Main.hs
+++ 2021/app/Main.hs
@@ -32,9 +32,10 @@ import Test.BenchPress (benchMany)
 main :: IO ()
 --main = benchAllDays
 --main = benchOne (Day20.part1, Day20.part2)
---main = benchEachPart
+main = benchEachPart
+
 --main = print Day23.part2
-main = print Day24.part1
+--main = print Day24.part1
 
 benchOne :: Show a => a -> IO ()
 benchOne a = benchMany 1 [("One Thing", putTextLn (show a))]
