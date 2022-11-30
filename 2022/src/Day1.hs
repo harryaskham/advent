b3861b7115c6f6f5ecdeaa13aa8f44f6c87f42b1
Harry Askham <harryaskham@gmail.com>
2022-11-29 23:59:57 -0600

    [no commit message]

--- 2022/src/Day1.hs
+++ 2022/src/Day1.hs
@@ -1,9 +1,9 @@
 module Day1 (part1, part2) where
 
 import Data.List (maximum)
-import Helper.TH
-import Helper.Util
-import Text.ParserCombinators.Parsec
+import Helper.TH (input)
+import Helper.Util (eol, number, parseWith, (<$$>))
+import Text.ParserCombinators.Parsec (Parser, many1, sepBy)
 
 elves :: Parser [Integer]
 elves = sum <$$> many1 (number <* eol) `sepBy` eol
