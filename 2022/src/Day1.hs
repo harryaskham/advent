5b6d1c5627448019e96e33542ecdd17deea84cb6
Harry Askham <harryaskham@gmail.com>
2022-11-29 22:34:14 -0600

    [no commit message]

--- 2022/src/Day1.hs
+++ 2022/src/Day1.hs
@@ -5,11 +5,11 @@ import Helper.TH
 import Helper.Util
 import Text.ParserCombinators.Parsec
 
-elves :: Parser [[Integer]]
-elves = many1 (number <* eol) `sepBy` eol
+elves :: Parser [Integer]
+elves = sum <$$> many1 (number <* eol) `sepBy` eol
 
 part1 :: Integer
-part1 = $(input 1) & parseWith elves & fmap sum & maximum
+part1 = $(input 1) & parseWith elves & maximum
 
 part2 :: Integer
-part2 = $(input 1) & parseWith elves & fmap sum & sortOn Down & take 3 & sum
+part2 = $(input 1) & parseWith elves & sortOn Down & take 3 & sum
