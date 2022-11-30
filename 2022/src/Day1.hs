e0eb35941226362809b17218f0dc83a2a1923aa7
Harry Askham <harryaskham@gmail.com>
2022-11-29 22:18:18 -0600

    day1

--- 2022/src/Day1.hs
+++ 2022/src/Day1.hs
@@ -1,34 +1,15 @@
 module Day1 (part1, part2) where
 
-import Data.Array qualified as A
-import Data.Bimap (Bimap)
-import Data.Bimap qualified as BM
-import Data.Map.Strict qualified as M
-import Data.Mod
-import Data.PQueue.Prio.Min qualified as PQ
-import Data.Sequence qualified as SQ
-import Data.Set qualified as S
-import Data.Text qualified as T
-import Data.Text.Read
-import Data.Vector qualified as V
-import Helper.Coord
-import Helper.Grid
+import Data.List (maximum)
 import Helper.TH
-import Helper.Tracers
 import Helper.Util
 import Text.ParserCombinators.Parsec
 
-elf :: Parser [Integer]
-elf = many1 (number <* eol) <* eol
-
 elves :: Parser [[Integer]]
-elves = many1 elf <* eol
+elves = many1 (number <* eol) `sepBy` eol
 
-part1 :: Text
-part1 =
-  $(input 1)
-    & parseWith elves
-    & show
+part1 :: Integer
+part1 = $(input 1) & parseWith elves & fmap sum & maximum
 
-part2 :: Text
-part2 = "Part 2"
+part2 :: Integer
+part2 = $(input 1) & parseWith elves & fmap sum & sortOn Down & take 3 & sum
