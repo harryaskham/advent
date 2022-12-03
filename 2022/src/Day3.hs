89779236ddf9968c34d628d5e2597ec543fc24d1
Harry Askham <harryaskham@gmail.com>
2022-12-02 23:21:42 -0600

    day 3

--- 2022/src/Day3.hs
+++ 2022/src/Day3.hs
@@ -1,50 +1,47 @@
 module Day3 (part1, part2) where
 
-import Data.Array qualified as A
-import Data.Bimap (Bimap)
-import Data.Bimap qualified as BM
-import Data.Map.Strict qualified as M
-import Data.Mod
-import Data.PQueue.Prio.Min qualified as PQ
-import Data.Sequence qualified as SQ
+import Data.Char (isAsciiLower)
+import Data.List (foldl1)
+import Data.List.Split (chunk, chunksOf)
 import Data.Set qualified as S
 import Data.Text qualified as T
-import Data.Text.Read
-import Data.Vector qualified as V
-import Helper.Coord
-import Helper.Grid
-import Helper.TH
-import Helper.Tracers
-import Helper.Util
-import Text.ParserCombinators.Parsec
+import Helper.TH (input)
+import Helper.Util (toTuple2)
+import Relude.Unsafe qualified as U
 
--- parser :: Parser [Int]
--- parser = many1 (number <* eol) <* eof
+priority :: Char -> Int
+priority a
+  | isAsciiLower a = ord a - 96
+  | otherwise = ord a - 38
 
--- line :: Parser Int
--- line = number
+duplicate1 :: Text -> Char
+duplicate1 s =
+  U.head
+    . S.toList
+    . uncurry S.intersection
+    . toTuple2
+    . fmap S.fromList
+    . chunksOf (T.length s `div` 2)
+    $ T.unpack s
 
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
-
--- instance GridCell Cell where
---   charMap =
---     BM.fromList
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
-
-part1 :: Text
+part1 :: Int
 part1 =
   $(input 3)
-    -- & readAs (signed decimal)
-    -- & parseWith parser
-    -- & parseLinesWith line
-    -- & lines
-    -- & readGrid
-    & (<> "Part 1")
+    & lines
+    & fmap (priority . duplicate1)
+    & sum
 
-part2 :: Text
-part2 = "Part 2"
+duplicate2 :: [Text] -> Char
+duplicate2 =
+  U.head
+    . S.toList
+    . foldl1 S.intersection
+    . fmap (S.fromList . T.unpack)
+
+part2 :: Int
+part2 =
+  $(input 3)
+    & lines
+    & chunksOf 3
+    & fmap (priority . duplicate2)
+    & sum
--- 2022/test/TestSuite.hs
+++ 2022/test/TestSuite.hs
@@ -35,8 +35,8 @@ main = do
   assertEqual "Day 2 (Part 1)" Day2.part1 15523
   assertEqual "Day 2 (Part 2)" Day2.part2 15702
 
-  assertEqual "Day 3 (Part 1)" Day3.part1 "Part 1"
-  assertEqual "Day 3 (Part 2)" Day3.part2 "Part 2"
+  assertEqual "Day 3 (Part 1)" Day3.part1 7428
+  assertEqual "Day 3 (Part 2)" Day3.part2 2650
 
   assertEqual "Day 4 (Part 1)" Day4.part1 "Part 1"
   assertEqual "Day 4 (Part 2)" Day4.part2 "Part 2"
