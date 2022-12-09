1e0f6893f9396187d9382ff9c7e1a6543814c282
Harry Askham <harryaskham@gmail.com>
2022-12-09 09:44:25 +0000

    Day9

--- 2022/src/Day9.hs
+++ 2022/src/Day9.hs
@@ -1,50 +1,41 @@
 module Day9 (part1, part2) where
 
-import Data.Array qualified as A
-import Data.Bimap (Bimap)
-import Data.Bimap qualified as BM
-import Data.Map.Strict qualified as M
-import Data.Mod
-import Data.PQueue.Prio.Min qualified as PQ
-import Data.Sequence qualified as SQ
-import Data.Set qualified as S
-import Data.Text qualified as T
-import Data.Text.Read
-import Data.Vector qualified as V
-import Helper.Coord
-import Helper.Grid
-import Helper.TH
-import Helper.Tracers
-import Helper.Util
-import Text.ParserCombinators.Parsec
+import Data.List (elem, foldl', last, length, nub, replicate)
+import Helper.Coord (Coord2, Dir2, move, neighbors, udlrToDir2)
+import Helper.TH (input)
+import Helper.Util (eol, number, parseWith)
+import Text.ParserCombinators.Parsec (Parser, anyChar, eof, many1, spaces)
+import Prelude hiding (elem, last)
 
--- parser :: Parser [Int]
--- parser = many1 (number <* eol) <* eof
+parser :: Parser [(Dir2, Int)]
+parser = many1 ((,) <$> (udlrToDir2 <$> anyChar <* spaces) <*> number <* eol) <* eof
 
--- line :: Parser Int
--- line = number
+moveRope :: ([Coord2], [Coord2]) -> (Dir2, Int) -> ([Coord2], [Coord2])
+moveRope (rope, path) (_, 0) = (rope, path)
+moveRope ([], path) _ = ([], path)
+moveRope (h : ts, path) (d, n) = moveRope (rope', last rope' : path) (d, n - 1)
+  where
+    rope' = moveTail d (move d 1 h : ts)
+    moveTail d (a@(x, y) : b@(x', y') : rest) =
+      if b `elem` a : neighbors a
+        then a : b : rest
+        else
+          let clip = max (-1) . min 1
+              b' = (x' + clip (x - x'), y' + clip (y - y'))
+           in a : moveTail d (b' : rest)
+    moveTail _ a = a
 
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
-
--- instance GridCell Cell where
---   charMap =
---     BM.fromList
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
-
-part1 :: Text
-part1 =
+solve :: Int -> Int
+solve n =
   $(input 9)
-    -- & readAs (signed decimal)
-    -- & parseWith parser
-    -- & parseLinesWith line
-    -- & lines
-    -- & readGrid
-    & (<> "Part 1")
+    & parseWith parser
+    & foldl' moveRope (replicate n (0, 0), [])
+    & snd
+    & nub
+    & length
 
-part2 :: Text
-part2 = "Part 2"
+part1 :: Int
+part1 = solve 2
+
+part2 :: Int
+part2 = solve 10
