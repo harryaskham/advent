8b2bbe37af0207a567d6003c1ee2bee452670789
Harry Askham <harryaskham@gmail.com>
2022-12-10 09:14:51 +0000

    Day10

--- 2022/src/Day10.hs
+++ 2022/src/Day10.hs
@@ -1,50 +1,59 @@
 module Day10 (part1, part2) where
 
-import Data.Array qualified as A
-import Data.Bimap (Bimap)
-import Data.Bimap qualified as BM
 import Data.Map.Strict qualified as M
-import Data.Mod
-import Data.PQueue.Prio.Min qualified as PQ
-import Data.Sequence qualified as SQ
-import Data.Set qualified as S
-import Data.Text qualified as T
-import Data.Text.Read
-import Data.Vector qualified as V
-import Helper.Coord
-import Helper.Grid
-import Helper.TH
-import Helper.Tracers
-import Helper.Util
-import Text.ParserCombinators.Parsec
+import Helper.Coord (Coord2)
+import Helper.Grid (SimpleWall (Wall), fromCoords, pretty)
+import Helper.TH (input)
+import Helper.Util (eol, number, parseWith)
+import Text.ParserCombinators.Parsec (Parser, eof, many1, string, (<|>))
+import Prelude hiding ((<|>))
 
--- parser :: Parser [Int]
--- parser = many1 (number <* eol) <* eof
+data Instruction = Noop | Addx Int deriving (Show)
 
--- line :: Parser Int
--- line = number
+data Machine = Machine {register :: Int, cycles :: Int, cycleRegisters :: Map Int Int}
 
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
+parser :: Parser [Instruction]
+parser = many1 (instruction <* eol) <* eof
+  where
+    instruction = noop <|> addx
+    noop = string "noop" >> return Noop
+    addx = Addx <$> (string "addx " *> number)
 
--- instance GridCell Cell where
---   charMap =
---     BM.fromList
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
+runInstruction :: Machine -> Instruction -> Machine
+runInstruction m Noop =
+  Machine
+    (register m)
+    (cycles m + 1)
+    (M.insert (cycles m) (register m) (cycleRegisters m))
+runInstruction m (Addx x) =
+  Machine
+    (register m + x)
+    (cycles m + 2)
+    ((M.insert (cycles m) (register m) . M.insert (cycles m + 1) (register m)) (cycleRegisters m))
 
-part1 :: Text
-part1 =
+run :: [(Int, Int)]
+run =
   $(input 10)
-    -- & readAs (signed decimal)
-    -- & parseWith parser
-    -- & parseLinesWith line
-    -- & lines
-    -- & readGrid
-    & (<> "Part 1")
+    & parseWith parser
+    & foldl' runInstruction (Machine 1 1 M.empty)
+    & cycleRegisters
+    & M.toList
+
+draw :: (Int, Int) -> Maybe Coord2
+draw (c, x) =
+  let (cx, cy) = ((c - 1) `mod` 40, (c - 1) `div` 40)
+   in if cx `elem` [x - 1, x, x + 1] then Just (cx, cy) else Nothing
+
+part1 :: Int
+part1 =
+  run
+    & filter (\(c, _) -> c `elem` [20, 60, 100, 140, 180, 220])
+    & fmap (uncurry (*))
+    & sum
 
 part2 :: Text
-part2 = "Part 2"
+part2 =
+  run
+    & mapMaybe draw
+    & fromCoords Wall
+    & pretty
--- 2022/test/TestSuite.hs
+++ 2022/test/TestSuite.hs
@@ -56,8 +56,8 @@ main = do
   assertEqual "Day 9 (Part 1)" Day9.part1 6494
   assertEqual "Day 9 (Part 2)" Day9.part2 2691
 
-  assertEqual "Day 10 (Part 1)" Day10.part1 "Part 1"
-  assertEqual "Day 10 (Part 2)" Day10.part2 "Part 2"
+  assertEqual "Day 10 (Part 1)" Day10.part1 15680
+  assertEqual "Day 10 (Part 2)" Day10.part2 "#### #### ###  #### #  #  ##  #  # ### \n   # #    #  # #    #  # #  # #  # #  #\n  #  ###  ###  ###  #### #    #  # #  #\n #   #    #  # #    #  # # ## #  # ### \n#    #    #  # #    #  # #  # #  # #   \n#### #    ###  #    #  #  ###  ##  #   "
 
   assertEqual "Day 11 (Part 1)" Day11.part1 "Part 1"
   assertEqual "Day 11 (Part 2)" Day11.part2 "Part 2"
