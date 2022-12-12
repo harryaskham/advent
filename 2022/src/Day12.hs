876200228f1856599da8ea86f762ea8eacb42966
Harry Askham <harryaskham@gmail.com>
2022-12-12 11:41:21 +0000

    Day12

--- 2022/src/Day12.hs
+++ 2022/src/Day12.hs
@@ -1,7 +1,6 @@
 module Day12 (part1, part2) where
 
 import Data.Bimap qualified as BM
-import Data.List (minimum)
 import Data.Map.Strict qualified as M
 import Data.Sequence qualified as SQ
 import Data.Set qualified as S
@@ -9,42 +8,28 @@ import Helper.Coord (neighborsNoDiags)
 import Helper.Grid (Grid, GridCell (charMap), findOne, readGrid)
 import Helper.TH (input)
 
-data Cell = Start Char | End Char | Cell Char deriving (Eq, Ord)
+data Cell = Start {_c :: Char} | End {_c :: Char} | Cell {_c :: Char} deriving (Eq, Ord)
 
 instance GridCell Cell where
   charMap = BM.fromList $ [(Start 'a', 'S'), (End 'z', 'E')] ++ [(Cell c, c) | c <- ['a' .. 'z']]
-
-height :: Cell -> Int
-height (Start c) = ord c
-height (End c) = ord c
-height (Cell c) = ord c
 
 floodUntil :: Grid Cell -> (Cell -> Bool) -> Maybe Int
 floodUntil g p = go (SQ.singleton (findOne (End 'z') g, 0)) S.empty
   where
     go SQ.Empty _ = Nothing
-    go ((currentPos, n) SQ.:<| rest) seen
-      | currentPos `S.member` seen = go rest seen
-      | p (g M.! currentPos) = Just n
-      | otherwise = go queue' seen'
+    go ((currentPos, steps) SQ.:<| queue) seen
+      | currentPos `S.member` seen = go queue seen
+      | p (g M.! currentPos) = Just steps
+      | otherwise = go (queue SQ.>< SQ.fromList ns) (S.insert currentPos seen)
       where
-        current = M.lookup currentPos g
         ns =
-          [ ((x, y), n + 1)
-            | (x, y) <- neighborsNoDiags currentPos,
-              let c = M.lookup (x, y) g,
-              (height <$> c) >= (subtract 1 . height <$> current),
-              (x, y) `S.notMember` seen
+          [ (n, steps + 1)
+            | n <- neighborsNoDiags currentPos,
+              (ord . _c <$> M.lookup n g) >= (subtract 1 . ord . _c <$> M.lookup currentPos g)
           ]
-        queue' = rest SQ.>< SQ.fromList ns
-        seen' = S.insert currentPos seen
 
 part1 :: Maybe Int
-part1 =
-  let g = $(input 12) & readGrid
-   in floodUntil g (== Start 'a')
+part1 = floodUntil (readGrid $(input 12)) (== Start 'a')
 
 part2 :: Maybe Int
-part2 =
-  let g = $(input 12) & readGrid
-   in floodUntil g ((== height (Cell 'a')) . height)
+part2 = floodUntil (readGrid $(input 12)) (== Cell 'a')
