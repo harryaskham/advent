e46f236e3fffefe728d3c8b13fb2255ced279bd1
Harry Askham <harryaskham@gmail.com>
2022-12-15 10:24:40 +0000

    Day15

--- 2022/src/Day15.hs
+++ 2022/src/Day15.hs
@@ -24,10 +24,15 @@ search sensors = go (PQ.singleton (h start) start) S.empty
     sees (sx, sy, bx, by) (x, y) = manhattan (sx, sy) (x, y) <= manhattan (sx, sy) (bx, by)
     go queue seen
       | score == 0 = x * 4000000 + y
+      | (x, y) `S.member` seen = go rest seen
       | otherwise = go queue' (S.insert (x, y) seen)
       where
         ((score, (x, y)), rest) = PQ.deleteFindMin queue
-        queue' = foldl' (\q n -> PQ.insert (h n) n q) rest (neighbors (x, y))
+        queue' =
+          foldl'
+            (\q n -> PQ.insert (h n) n q)
+            rest
+            ([n | n@(nx, ny) <- neighbors (x, y), nx >= 0, ny >= 0, nx <= 4000000, ny <= 4000000])
 
 part1 :: Int
 part1 =
