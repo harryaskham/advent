e7676d63f4d06f9a360dab4e868b6c476ac03b3c
Harry Askham <harryaskham@gmail.com>
2022-12-16 07:38:25 +0000

    partial pt1

--- 2022/src/Day16.hs
+++ 2022/src/Day16.hs
@@ -15,36 +15,39 @@ import Helper.Coord
 import Helper.Grid
 import Helper.TH
 import Helper.Tracers
-import Helper.Util
+import Helper.Util hiding (count)
 import Text.ParserCombinators.Parsec
+import Prelude hiding ((<|>))
 
--- parser :: Parser [Int]
--- parser = many1 (number <* eol) <* eof
+parser :: Parser (Map String (Int, [String]))
+parser = M.fromList <$> many1 (line <* eol) <* eof
+  where
+    line = do
+      vid <- string "Valve " *> count 2 anyChar
+      rate <- string " has flow rate=" *> number
+      ns <- (try (string "; tunnels lead to valves ") <|> (try (string "; tunnel leads to valve "))) *> count 2 anyChar `sepBy` string ", "
+      return (vid, (rate, ns))
 
--- line :: Parser Int
--- line = number
+mostPressure :: Map String (Int, [String]) -> Int
+mostPressure g = go (SQ.singleton ("AA", S.empty, 0, 0, 0)) S.empty 0
+  where
+    go SQ.Empty _ best = best
+    go ((_, _, _, total, 30) SQ.:<| rest) seen best = go rest seen (max total best)
+    go (st@(current, open, flow, total, t) SQ.:<| queue) seen best
+      | st `S.member` seen = go queue seen best
+      | otherwise = traceShow (current, S.size open, flow, total, t) $ go queue' (S.insert st seen) best
+      where
+        (currentFlow, ns) = g M.! current
+        openCurrent = (current, S.insert current open, flow + currentFlow, total + flow, t + 1)
+        moves = [(n, open, flow, total + flow, t + 1) | n <- ns]
+        next = if current `S.member` open then moves else openCurrent : moves
+        queue' = queue SQ.>< SQ.fromList next
 
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
-
--- instance GridCell Cell where
---   charMap =
---     BM.fromList
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
-
-part1 :: Text
+part1 :: Int
 part1 =
   $(input 16)
-    -- & readAs (signed decimal)
-    -- & parseWith parser
-    -- & parseLinesWith line
-    -- & lines
-    -- & readGrid
-    & (<> "Part 1")
+    & parseWith parser
+    & mostPressure
 
 part2 :: Text
 part2 = "Part 2"
