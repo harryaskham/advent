a21ba5a94f2c8ef707f8c81f9edb9a20ce0161e5
Harry Askham <harryaskham@gmail.com>
2022-12-16 16:59:01 +0000

    cleanup 16

--- 2022/src/Day16.hs
+++ 2022/src/Day16.hs
@@ -18,8 +18,8 @@ parser = M.fromList <$> many1 (line <* eol) <* eof
       ns <- (try (string "; tunnels lead to valves ") <|> try (string "; tunnel leads to valve ")) *> count 2 anyChar `sepBy` string ", "
       return (vid, (rate, ns))
 
-mostPressure :: Map String (Int, [String]) -> Int
-mostPressure g = go (SQ.singleton ("AA", S.empty, 0, 0, 0)) M.empty 0
+mostPressure' :: Map String (Int, [String]) -> Int
+mostPressure' g = go (SQ.singleton ("AA", S.empty, 0, 0, 0)) M.empty 0
   where
     go SQ.Empty _ best = best
     go ((_, _, _, total, 30) SQ.:<| rest) cache best = go rest cache (max total best)
@@ -52,25 +52,34 @@ shortestPaths g = M.fromList ([(n, startFrom n) | n <- "AA" : S.toList nodesWith
             next = [(node, n + 1, seen') | node <- currentNs]
             queue' = queue SQ.>< SQ.fromList next
 
-mostPressure2 :: Map String (Int, [String]) -> Int
-mostPressure2 g = maximum [score g $ M.unionWith min pA pB | pA <- bestPaths, pB <- bestPaths]
+score :: Map String (Int, [String]) -> Map String Int -> Int
+score g open = sum [f * (26 - t) | (n, t) <- M.toList open, let (f, _) = g M.! n]
+
+singlePaths :: Int -> Map String (Int, [String]) -> [Map String Int]
+singlePaths limit g = S.toList $ go (SQ.singleton ("AA", 0, M.empty)) S.empty
   where
-    score g open = sum [f * (26 - t) | (n, t) <- M.toList open, let (f, _) = g M.! n]
-    singlePaths = S.toList $ go (SQ.singleton ("AA", 0, M.empty)) S.empty
-    bestPaths = take 250 $ sortOn (Down . score g) singlePaths
     paths = shortestPaths g
     go SQ.Empty opens = opens
     go ((current, t, open) SQ.:<| queue) opens
       | M.size open == M.size paths - 1 = go queue (S.insert open opens)
-      | t >= 26 = go queue (S.insert open opens)
+      | t >= limit = go queue (S.insert open opens)
       | otherwise = go queue' opens
       where
         ns = M.toList (paths M.! current)
         next = [(n, t + d + 1, M.insert n (t + d + 1) open) | (n, d) <- ns, not (n `M.member` open)]
         queue' = queue SQ.>< SQ.fromList next
 
+-- TODO: Not working?
+mostPressure :: Map String (Int, [String]) -> Int
+mostPressure g = maximum [score g p | p <- singlePaths 30 g]
+
+mostPressurePaired :: Map String (Int, [String]) -> Int
+mostPressurePaired g =
+  let bestPaths = take 250 $ sortOn (Down . score g) (singlePaths 26 g)
+   in maximum [score g $ M.unionWith min pA pB | pA <- bestPaths, pB <- bestPaths]
+
 part1 :: Int
-part1 = $(input 16) & parseWith parser & mostPressure
+part1 = $(input 16) & parseWith parser & mostPressure'
 
 part2 :: Int
-part2 = $(input 16) & parseWith parser & mostPressure2
+part2 = $(input 16) & parseWith parser & mostPressurePaired
