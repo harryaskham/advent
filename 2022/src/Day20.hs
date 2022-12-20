236fbb2cad50d8d964263f0d333122af0736a96e
Harry Askham <harryaskham@gmail.com>
2022-12-20 10:10:02 +0000

    pt1

--- 2022/src/Day20.hs
+++ 2022/src/Day20.hs
@@ -3,6 +3,7 @@ module Day20 (part1, part2) where
 import Data.Array qualified as A
 import Data.Bimap (Bimap)
 import Data.Bimap qualified as BM
+import Data.List ((!!))
 import Data.Map.Strict qualified as M
 import Data.Mod
 import Data.PQueue.Prio.Min qualified as PQ
@@ -10,6 +11,8 @@ import Data.Sequence qualified as SQ
 import Data.Set qualified as S
 import Data.Text qualified as T
 import Data.Text.Read
+import Data.Tuple.Extra (fst3, snd3, thd3)
+import Data.Vector (Vector)
 import Data.Vector qualified as V
 import Helper.Coord
 import Helper.Grid
@@ -18,33 +21,68 @@ import Helper.Tracers
 import Helper.Util
 import Text.ParserCombinators.Parsec
 
--- parser :: Parser [Int]
--- parser = many1 (number <* eol) <* eof
+type Ring = Vector (Int, Int, Int)
 
--- line :: Parser Int
--- line = number
+mkRing :: [Int] -> Ring
+mkRing xs = V.fromList $ (\(i, x) -> (x, (i - 1) `mod` length xs, (i + 1) `mod` length xs)) <$> zip [0 ..] xs
 
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
+mix :: Ring -> Ring
+mix r = foldl' mixOne r [0 .. length r - 1]
 
--- instance GridCell Cell where
---   charMap =
---     BM.fromList
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
+-- We can follow ptrs to construct the ptr ring
+-- e.g. follow terminates after 5000
+-- then use mod to figure out where we'd be in the ring after our actual amount of steps
+-- then make the swaps
 
-part1 :: Text
+mixOne :: Ring -> Int -> Ring
+mixOne r i
+  | n == 0 = r
+  | otherwise = traceShow i $ r''
+  where
+    (n, prevI, nextI) = r V.! i
+    (prevN, prevPrevI, _) = r V.! prevI
+    (nextN, _, nextNextI) = r V.! nextI
+    r' = r V.// [(prevI, (prevN, prevPrevI, nextI)), (nextI, (nextN, prevI, nextNextI))]
+    followFwd 0 prevPtr nextPtr = (prevPtr, nextPtr)
+    followFwd j _ nextPtr = let (_, _, nextPtr') = r' V.! nextPtr in followFwd (j - 1) nextPtr nextPtr'
+    followBwd 0 prevPtr nextPtr = (prevPtr, nextPtr)
+    followBwd j prevPtr _ = let (_, prevPtr', _) = r' V.! prevPtr in followBwd (j - 1) prevPtr' prevPtr
+    follow = if n < 0 then followBwd else followFwd
+    (prevI', nextI') = follow (abs n) prevI nextI
+    (prevN', prevPrevI', _) = r' V.! prevI'
+    (nextN', _, nextNextI') = r' V.! nextI'
+    r'' = r' V.// [(i, (n, prevI', nextI')), (prevI', (prevN', prevPrevI', i)), (nextI', (nextN', i, nextNextI'))]
+
+ringToList :: Ring -> [Int]
+ringToList r = go 0 zeroI
+  where
+    (Just zeroI) = V.findIndex ((== 0) . fst3) r
+    go j i
+      | j == length r = []
+      | otherwise =
+        let (n, _, nextI) = r V.! i
+         in n : go (j + 1) nextI
+
+part1 :: Int
 part1 =
   $(input 20)
-    -- & readAs (signed decimal)
-    -- & parseWith parser
-    -- & parseLinesWith line
-    -- & lines
-    -- & readGrid
-    & (<> "Part 1")
+    & readAs (signed decimal)
+    & mkRing
+    & mix
+    & ringToList
+    & cycle
+    & (\xs -> (xs !!) <$> [1000, 2000, 3000])
+    & sum
 
-part2 :: Text
-part2 = "Part 2"
+part2 :: Int
+part2 =
+  $(input 20)
+    & readAs (signed decimal)
+    & fmap (* 811589153)
+    & mkRing
+    & iterate mix
+    & (!! 10)
+    & ringToList
+    & cycle
+    & (\xs -> (xs !!) <$> [1000, 2000, 3000])
+    & sum
