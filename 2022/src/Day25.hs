842497511f6c846d833ff56c5b34f8af0d05efaf
Harry Askham <harryaskham@gmail.com>
2022-12-25 10:00:36 +0000

    Day25

--- 2022/src/Day25.hs
+++ 2022/src/Day25.hs
@@ -1,50 +1,40 @@
-module Day25 (part1, part2) where
+module Day25 (part1, part2, snafuToInt, intToSnafu) where
 
-import Data.Array qualified as A
-import Data.Bimap (Bimap)
-import Data.Bimap qualified as BM
-import Data.Map.Strict qualified as M
-import Data.Mod
-import Data.PQueue.Prio.Min qualified as PQ
-import Data.Sequence qualified as SQ
-import Data.Set qualified as S
 import Data.Text qualified as T
-import Data.Text.Read
-import Data.Vector qualified as V
-import Helper.Coord
-import Helper.Grid
-import Helper.TH
-import Helper.Tracers
-import Helper.Util
-import Text.ParserCombinators.Parsec
+import Helper.TH (input)
+import Relude.Unsafe qualified as U
 
--- parser :: Parser [Int]
--- parser = many1 (number <* eol) <* eof
+snafuToInt :: String -> Int
+snafuToInt s = go (reverse s) 0 0
+  where
+    go [] _ a = a
+    go (c : cs) n a = go cs (n + 1) (a + snafuVal c * 5 ^ n)
+    snafuVal '0' = 0
+    snafuVal '1' = 1
+    snafuVal '2' = 2
+    snafuVal '-' = -1
+    snafuVal '=' = -2
 
--- line :: Parser Int
--- line = number
+intToSnafu :: Int -> String
+intToSnafu a = make a powers ""
+  where
+    n = U.head [length s | s <- scanl' (++) "" (repeat "2"), let a' = snafuToInt s, a' >= a]
+    powers = reverse . take n $ scanl1 (*) (1 : repeat 5)
+    make _ [] s = reverse s
+    make x (p : ps) s
+      | x < 0 && p + 2 * sum ps < abs x = make (x + 2 * p) ps ('=' : s)
+      | x < 0 && 2 * sum ps < abs x = make (x + 1 * p) ps ('-' : s)
+      | x > 0 && p + 2 * sum ps < x = make (x - 2 * p) ps ('2' : s)
+      | x > 0 && 2 * sum ps < x = make (x - 1 * p) ps ('1' : s)
+      | otherwise = make x ps ('0' : s)
 
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
-
--- instance GridCell Cell where
---   charMap =
---     BM.fromList
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
-
-part1 :: Text
+part1 :: String
 part1 =
   $(input 25)
-    -- & readAs (signed decimal)
-    -- & parseWith parser
-    -- & parseLinesWith line
-    -- & lines
-    -- & readGrid
-    & (<> "Part 1")
+    & lines
+    & fmap (snafuToInt . T.unpack)
+    & sum
+    & intToSnafu
 
 part2 :: Text
-part2 = "Part 2"
+part2 = "Merry Christmas!"
--- 2022/test/TestSuite.hs
+++ 2022/test/TestSuite.hs
@@ -101,5 +101,5 @@ main = do
   assertEqual "Day 24 (Part 1)" Day24.part1 (Just 288)
   assertEqual "Day 24 (Part 2)" Day24.part2 (Just 861)
 
-  assertEqual "Day 25 (Part 1)" Day25.part1 "Part 1"
-  assertEqual "Day 25 (Part 2)" Day25.part2 "Part 2"
+  assertEqual "Day 25 (Part 1)" Day25.part1 "2=0-2-1-0=20-01-2-20"
+  assertEqual "Day 25 (Part 2)" Day25.part2 "Merry Christmas!"
