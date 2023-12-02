e9f959049ae3d28048165d6f50e7c9ba9bdce4e7
Harry Askham <harryaskham@gmail.com>
2023-12-02 14:47:15 +0000

    [no commit message]

--- 2023/src/Day2.hs
+++ 2023/src/Day2.hs
@@ -1,40 +1,15 @@
 module Day2 (part1, part2) where
 
-import Data.Array qualified as A
-import Data.Bimap (Bimap)
-import Data.Bimap qualified as BM
-import Data.Map.Strict qualified as M
-import Data.Mod
-import Data.PQueue.Prio.Min qualified as PQ
-import Data.Sequence qualified as SQ
-import Data.Set qualified as S
 import Data.Text qualified as T
-import Data.Text.Read
-import Data.Vector qualified as V
-import Helper.Coord
-import Helper.Grid
-import Helper.TH
-import Helper.Tracers
-import Helper.Util
+import Helper.TH (input)
+import Helper.Util (eol, number, parseWith)
 import Text.ParserCombinators.Parsec
-
--- parser :: Parser [Int]
--- parser = many1 (number <* eol) <* eof
-
--- line :: Parser Int
--- line = number
-
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
-
--- instance GridCell Cell where
---   charMap =
---     BM.fromList
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
+  ( Parser,
+    choice,
+    eof,
+    sepBy1,
+    string,
+  )
 
 data Color = Red Int | Blue Int | Green Int
   deriving (Show)
@@ -43,7 +18,6 @@ data Game = Game
   { index :: Int,
     cubes :: [[Color]]
   }
-  deriving (Show)
 
 color :: Parser Color
 color = do
@@ -74,14 +48,6 @@ possibleColor (Blue i) = i <= 14
 possibleGame :: Game -> Bool
 possibleGame (Game _ colors) = all (all possibleColor) colors
 
-part1 :: Int
-part1 =
-  $(input 2)
-    & parseWith games
-    & filter possibleGame
-    & fmap index
-    & sum
-
 power :: [Color] -> Int
 power colors = go colors 0 0 0
   where
@@ -89,6 +55,14 @@ power colors = go colors 0 0 0
     go ((Red i) : cs) r g b = go cs (max i r) g b
     go ((Green i) : cs) r g b = go cs r (max i g) b
     go ((Blue i) : cs) r g b = go cs r g (max i b)
+
+part1 :: Int
+part1 =
+  $(input 2)
+    & parseWith games
+    & filter possibleGame
+    & fmap index
+    & sum
 
 part2 :: Int
 part2 =
