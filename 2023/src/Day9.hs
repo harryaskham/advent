7d14ecdcc3bda7cdc1e34e1e4a2a8b36a0ef6dde
Harry Askham <harryaskham@gmail.com>
2023-12-09 17:58:28 +0000

    [no commit message]

--- 2023/src/Day9.hs
+++ 2023/src/Day9.hs
@@ -1,24 +1,16 @@
 module Day9 (part1, part2) where
 
-import Control.Arrow ((***))
 import Data.List (foldr1)
 import Helper.TH (input)
 import Helper.Util (eol, number, parseWith)
 import Relude.Unsafe qualified as U
 import Text.ParserCombinators.Parsec (Parser, eof, many1, sepBy, string)
 
-differences :: [Int] -> [[Int]]
-differences = pure &&& enumFromTo 2 . length >>> uncurry (foldl' (\(ds : dss) _ -> (uncurry (-) <$> zip (drop 1 ds) ds) : ds : dss))
-
-solve :: ([Int] -> Int) -> (Int -> Int -> Int) -> Int
-solve get pm =
-  $(input 9)
-    & parseWith (many1 ((number `sepBy` string " ") <* eol) <* eof)
-    & fmap (differences >>> (fmap get >>> ((foldr1 pm . U.tail) &&& U.head) >>> uncurry (flip pm)))
-    & sum
+solve :: ([Int] -> Int) -> (Int -> Int -> Int) -> String -> Int
+solve get pm = parseWith (many1 ((number `sepBy` string " ") <* eol) <* eof) >>> fmap (pure &&& enumFromTo 2 . length >>> uncurry (foldl' (\(ds : dss) _ -> (uncurry (-) <$> zip (drop 1 ds) ds) : ds : dss)) >>> (fmap get >>> ((foldr1 pm . U.tail) &&& U.head) >>> uncurry (flip pm))) >>> sum
 
 part1 :: Int
-part1 = solve U.last (+)
+part1 = solve U.last (+) $(input 9)
 
 part2 :: Int
-part2 = solve U.head (-)
+part2 = solve U.head (-) $(input 9)
