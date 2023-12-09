419c47158d81746527d8437f15562501691f7226
Harry Askham <harryaskham@gmail.com>
2023-12-09 17:30:53 +0000

    [no commit message]

--- 2023/src/Day9.hs
+++ 2023/src/Day9.hs
@@ -1,48 +1,20 @@
 module Day9 (part1, part2) where
 
-import Data.Array qualified as A
-import Data.Bimap (Bimap)
-import Data.Bimap qualified as BM
-import Data.List (foldl1, foldr1)
-import Data.Map.Strict qualified as M
-import Data.Mod
-import Data.PQueue.Prio.Min qualified as PQ
-import Data.Sequence qualified as SQ
-import Data.Set qualified as S
-import Data.Text qualified as T
-import Data.Text.Read
-import Data.Vector qualified as V
-import Helper.Coord
-import Helper.Grid
-import Helper.TH
-import Helper.Tracers
-import Helper.Util
+import Control.Arrow ((***))
+import Data.List (foldr1)
+import Helper.TH (input)
+import Helper.Util (eol, number, parseWith)
 import Relude.Unsafe qualified as U
-import Text.ParserCombinators.Parsec
-
-parser :: Parser [[Int]]
-parser = many1 ((number `sepBy` string " ") <* eol) <* eof
-
-difference :: [Int] -> [Int]
-difference [] = []
-difference [_] = []
-difference (x : x' : xs) = (x' - x) : difference (x' : xs)
+import Text.ParserCombinators.Parsec (Parser, eof, many1, sepBy, string)
 
 differences :: [Int] -> [[Int]]
-differences = go []
-  where
-    go ds xs
-      | all (== 0) xs = ds
-      | otherwise = go (xs : ds) (difference xs)
-
-interpolate :: ([Int] -> Int) -> (Int -> Int -> Int) -> [[Int]] -> Int
-interpolate get pm dss = get (U.last dss) `pm` foldl1 (flip pm) (get <$> U.init dss)
+differences = pure &&& enumFromTo 2 . length >>> uncurry (foldl' (\(ds : dss) _ -> (uncurry (-) <$> zip (drop 1 ds) ds) : ds : dss))
 
 solve :: ([Int] -> Int) -> (Int -> Int -> Int) -> Int
 solve get pm =
   $(input 9)
-    & parseWith parser
-    & fmap (interpolate get pm . differences)
+    & parseWith (many1 ((number `sepBy` string " ") <* eol) <* eof)
+    & fmap (differences >>> (fmap get >>> ((foldr1 pm . U.tail) &&& U.head) >>> uncurry (flip pm)))
     & sum
 
 part1 :: Int
