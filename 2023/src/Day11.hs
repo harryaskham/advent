2b022af153c357f46f77414b62dc8ae46dac33f1
Harry Askham <harryaskham@gmail.com>
2023-12-11 11:45:34 +0000

    [no commit message]

--- 2023/src/Day11.hs
+++ 2023/src/Day11.hs
@@ -1,6 +1,7 @@
 module Day11 (part1, part2) where
 
 import Data.Bimap qualified as BM
+import Data.Foldable.Unicode ((∈))
 import Data.Map.Strict qualified as M
 import Data.Set qualified as S
 import Helper.Grid (DotHash (..), Grid, gridFind, maxXY, readGrid)
@@ -10,7 +11,7 @@ import Helper.Util (both, fsts2, range, snds2, triPairs)
 distanceSum :: Int -> Grid DotHash -> Int
 distanceSum sc g =
   let es cons (a, b) = S.fromList [i | i <- [0 .. a], all (== Dot) [g M.! cons i j | j <- [0 .. b]]]
-      di (ai, bi) es = sum [bool 1 sc (i `S.member` es) | i <- range ai bi]
+      di (a, b) e = sum [bool 1 sc (i ∈ e) | i <- range a b]
       d ab = let m = maxXY g in di (fsts2 ab) (es (,) m) + di (snds2 ab) (es (flip (,)) (swap m)) - 2
    in sum $ d <$> triPairs (gridFind Hash g)
 
