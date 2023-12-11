fbb52e387867e44b0aeb3ff1b61acf9de68cb54c
Harry Askham <harryaskham@gmail.com>
2023-12-11 09:45:43 +0000

    [no commit message]

--- 2023/src/Day11.hs
+++ 2023/src/Day11.hs
@@ -1,55 +1,18 @@
 module Day11 (part1, part2) where
 
-import Data.Array qualified as A
-import Data.Bimap (Bimap)
 import Data.Bimap qualified as BM
 import Data.Map.Strict qualified as M
-import Data.Mod
-import Data.PQueue.Prio.Min qualified as PQ
-import Data.Sequence qualified as SQ
 import Data.Set qualified as S
-import Data.Text qualified as T
-import Data.Text.Read
-import Data.Vector qualified as V
-import Helper.Coord
-import Helper.Grid
-import Helper.TH
-import Helper.Tracers
-import Helper.Util
+import Helper.Coord (Coord2)
+import Helper.Grid (Grid, GridCell (charMap), find, maxXY, readGrid)
+import Helper.TH (input)
 import Relude.Unsafe qualified as U
-import Text.ParserCombinators.Parsec
 import Prelude hiding (find)
 
 data Cell = None | Star deriving (Eq, Ord)
 
 instance GridCell Cell where
   charMap = BM.fromList [(None, '.'), (Star, '#')]
-
--- pairwiseDistances' :: Grid Cell -> Map Coord2 (Map Coord2 Int)
--- pairwiseDistances' g =
---   let stars = find Star g
---       (maxX, maxY) = maxXY g
---       emptyX = S.fromList [x | x <- [0 .. maxX], all (== None) [g M.! (x, y) | y <- [0 .. maxY]]]
---       emptyY = S.fromList [y | y <- [0 .. maxY], all (== None) [g M.! (x, y) | x <- [0 .. maxX]]]
---       go seen t ((c@(x, y), (lx, ly), d) SQ.:<| q)
---         | c == t = d
---         | c `S.member` seen || c `M.notMember` g = go seen t q
---         | otherwise = go (S.insert c seen) t (q SQ.>< SQ.fromList ((,c,d + delta) <$> neighborsNoDiags c))
---         where
---           delta = case (abs (x - lx), abs (y - ly), x `S.member` emptyX, y `S.member` emptyY) of
---             (1, 0, True, _) ->
---             (0, 1, _, True) -> 2
---             _ -> 1
---    in M.fromList
---         <$> M.fromListWith
---           (<>)
---           ( mconcat
---               [ [(a, [(b, d)]), (b, [(a, d)])]
---                 | (i, a) <- zip [1 ..] stars,
---                   b <- drop i stars,
---                   let d = go S.empty b (SQ.singleton (a, a, 0))
---               ]
---           )
 
 pairwiseDistances :: Int -> Grid Cell -> Map Coord2 (Map Coord2 Int)
 pairwiseDistances scale g =
@@ -72,22 +35,16 @@ pairwiseDistances scale g =
               ]
           )
 
-part1 :: Int
-part1 =
+solve :: Int -> Int
+solve scale =
   $(input 11)
-    -- \$(exampleInput 11)
     & readGrid
-    & pairwiseDistances 2
-    -- & traceShowId
+    & pairwiseDistances scale
     & (\d -> [d M.! a M.! b | let stars = M.keys d, (i, a) <- zip [1 ..] stars, b <- drop i stars])
     & sum
 
+part1 :: Int
+part1 = solve 2
+
 part2 :: Int
-part2 =
-  $(input 11)
-    -- \$(exampleInput 11)
-    & readGrid
-    & pairwiseDistances 1000000
-    -- & traceShowId
-    & (\d -> [d M.! a M.! b | let stars = M.keys d, (i, a) <- zip [1 ..] stars, b <- drop i stars])
-    & sum
+part2 = solve 1000000
--- 2023/test/TestSuite.hs
+++ 2023/test/TestSuite.hs
@@ -59,8 +59,8 @@ main = do
   assertEqual "Day 10 (Part 1)" Day10.part1 6823
   assertEqual "Day 10 (Part 2)" Day10.part2 415
 
-  assertEqual "Day 11 (Part 1)" Day11.part1 "Part 1"
-  assertEqual "Day 11 (Part 2)" Day11.part2 "Part 2"
+  assertEqual "Day 11 (Part 1)" Day11.part1 9370588
+  assertEqual "Day 11 (Part 2)" Day11.part2 746207878188
 
   assertEqual "Day 12 (Part 1)" Day12.part1 "Part 1"
   assertEqual "Day 12 (Part 2)" Day12.part2 "Part 2"
