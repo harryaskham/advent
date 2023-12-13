fd6ee28f46b23a3ac66fe5c2ad6827d8aee14514
Harry Askham <harryaskham@gmail.com>
2023-12-13 11:57:56 +0000

    [no commit message]

--- 2023/src/Day13.hs
+++ 2023/src/Day13.hs
@@ -3,7 +3,6 @@ module Day13 (part1, part2) where
 reflects :: Grid DotHash -> [Either Int Int]
 reflects g =
   let column g =
-        --    traceTextLn (pretty g) $
         [ i
           | let (maxX, _) = maxXY g,
             i <- [1 .. maxX],
@@ -14,15 +13,12 @@ reflects g =
                    in mapKeys (first (subtract xO)) g',
             let (l, r) = bimap (crop (i - w) i) (crop 0 w) . second (mapKeys (first (subtract i))) $ partitionWithKey (\k _ -> k < (i, 0)) g,
             let (a, b) = second (v0 . variants) . toTuple2 . sortOn mapSize $ [l, r],
-            -- traceTextLn (pretty a) $
-            --   traceTextLn (pretty b) $
             (mkSet . unMap $ a) ⊆ (mkSet . unMap $ b)
-            -- traceTextLn (pretty . mkMap . unSet $ l) $ traceTextLn (pretty . mkMap . unSet $ r) $ l ⊆ r || l ⊇ r
         ]
    in (Left <$> column g) <> (Right <$> (column (r90 $ variants g)))
 
-reflects' :: Grid DotHash -> [Either Int Int]
-reflects' g =
+reflectsSmudged :: Grid DotHash -> [Either Int Int]
+reflectsSmudged g =
   let smudge = bool Dot Hash . (== Dot)
       previous = uhead (reflects g)
       gs = [insert c (smudge (g |! c)) g | c <- keys g]
@@ -30,7 +26,7 @@ reflects' g =
 
 solve :: (Grid DotHash -> [Either Int Int]) -> Int
 solve f =
-  ($(input 13) :: Text)
+  $(input 13)
     & unpack
     & splitOn "\n\n"
     & fmap pack
@@ -46,4 +42,4 @@ part1 :: Int
 part1 = solve reflects
 
 part2 :: Int
-part2 = solve reflects'
+part2 = solve reflectsSmudged
