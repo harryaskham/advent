75207aac7c399d4453fbee8e126b6335cbd3b6ad
Harry Askham <harryaskham@gmail.com>
2023-12-16 14:16:42 +0000

    [no commit message]

--- 2023/src/Day15.hs
+++ 2023/src/Day15.hs
@@ -1,17 +1,17 @@
 module Day15 (part1, part2) where
 
 hash :: String -> Int
-hash = foldl' (\n c -> ((n + ord c) * 17) `mod` 256) 0
+hash = foldl' (\n c -> (n + ord c) * 17 `mod` 256) 0
 
 part1 :: Int
 part1 = $(input 15) |- csv & fmap hash & sum
 
 part2 :: Int
 part2 =
-  let op = (,) <$> many1 alphaNum <*> (((,) <$> char '=' <*> number) <|> ((,) <$> char '-' <*> pure 0))
+  let op = (,) <$> many1 alphaNum <*> ((,) <$> char '=' <*> number <|> (,) <$> char '-' <*> pure 0)
       run m o = m |~ (hash (fst o), runOp o)
-      runOp (l, ('=', f)) (m, s) = bool ((m |. (l, f)), (s |> l)) ((m |. (l, f)), s) (l ∈ m)
-      runOp (l, ('-', _)) b@(m, s) = bool b ((m |/ l), (s >/< l)) (l ∈ m)
+      runOp (l, ('=', f)) (m, s) = bool (m |. (l, f), s |> l) (m |. (l, f), s) (l ∈ m)
+      runOp (l, ('-', _)) b@(m, s) = bool b (m |/ l, s >/< l) (l ∈ m)
       power (i, (m, s)) = sum [(i + 1) * slot * (m |! l) | (slot, l) <- zip [1 ..] (unSeq s)]
-      boxes = mkMap [(i, ((mkMap []), (mkSeq []))) | i <- [0 .. 255]]
+      boxes = mkMap [(i, (mkMap [], mkSeq [])) | i <- [0 .. 255]]
    in $(input 15) |- (op `sepBy` char ',') & (foldl' run boxes >>> unMap >>> fmap power >>> sum)
