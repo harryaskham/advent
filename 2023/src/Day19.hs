2d729aa7b5f55fa08cdf27a6c58313bdb1bf6772
Harry Askham <harryaskham@gmail.com>
2023-12-19 10:57:25 +0000

    [no commit message]

--- 2023/src/Day19.hs
+++ 2023/src/Day19.hs
@@ -1,25 +1,8 @@
 module Day19 (part1, part2) where
 
--- parser :: Parser [Int]
--- parser = many1 (number <* eol) <* eof
-
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
-
--- instance GridCell Cell where
---   charMap =
---     mkBimap
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
-
 part1 :: Text
 part1 =
   $(input 19)
-    -- \$(grid input 19)
-    -- \|- parser
     & (<> "Part 1")
 
 part2 :: Text
