b021a73ae908752c6473620318b43a0d346f2fc9
Harry Askham <harryaskham@gmail.com>
2023-12-21 16:08:21 +0000

    [no commit message]

--- 2023/src/Day21.hs
+++ 2023/src/Day21.hs
@@ -8,7 +8,6 @@ walk n g' = size (go n (mkSet [start]))
     go 0 cs = cs
     go n cs = go (n - 1) (mkSet [c' | c <- unSet cs, c' <- neighborsNoDiags c, g |? c' == Just '.'])
 
-
 part1 :: Int
 part1 = walk 64 $(grid input 21)
 
@@ -22,9 +21,15 @@ part2 = do
         | n `mod` 131 == 65 = size cs : next
         | otherwise = next
         where
-          next = go (n + 1) (mkSet [(x, y)
-                                    | c <- unSet cs,
-                                      (x, y) <- neighborsNoDiags c,
-                                      (mod x 131, mod y 131) ∈ spaces])
-      poly n = let [a,b,c] = go 0 cs in a + n * (b - a + (n - 1) * (c + a - 2*b) `div` 2)
+          next =
+            go
+              (n + 1)
+              ( mkSet
+                  [ (x, y)
+                    | c <- unSet cs,
+                      (x, y) <- neighborsNoDiags c,
+                      (mod x 131, mod y 131) ∈ spaces
+                  ]
+              )
+      poly n = let [a, b, c] = go 0 cs in a + n * (b - a + (n - 1) * (c + a - 2 * b) `div` 2)
    in poly (26501365 `div` 131)
