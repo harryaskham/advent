5628516fcc656a8b35c0d1e6dd6723fe4c0af3b4
Harry Askham <harryaskham@gmail.com>
2023-12-21 09:08:02 +0000

    [no commit message]

--- 2023/src/Day21.hs
+++ 2023/src/Day21.hs
@@ -59,18 +59,70 @@ walk n g' = size (go n (mkSet [start]))
 --                 -- (counts |~ (c, (<-| n)))
 --                 localCounts'
 
-walk' :: Int -> Grid Char -> Int
-walk' n' g' = size $ startEvalMemo (go (0, start))
+-- need a second thing which is when do we see each thing so we can get a steps-to
+
+stepsTo :: Grid Char -> (Int, Int) -> (Int, Int) -> Set Int
+stepsTo g start target = go (mkSeq [(0, start)]) (mkSet []) (mkSet [])
   where
-    (w, h) = both (+ 1) (maxXY g')
-    start = gridFindOne 'S' g'
-    g = g' |. (start, '.')
+    (w, h) = both (+ 1) (maxXY g)
     local (x, y) = (x `mod` w, y `mod` h)
     get g c = g |! local c
-    go (n, c@(x, y))
-      | x < 0 || y < 0 || x >= w || y >= h = memo go (n, local c)
-      | n == n' = return [c]
-      | otherwise = mconcat <$> sequence [memo go (n + 1, c') | c' <- neighborsNoDiags c, g `get` c' /= '#']
+    go Empty seen steps = steps
+    go ((n, c) :<| q) seen steps
+      | (n, c) ∈ seen = go q seen steps
+      | otherwise =
+          let seen' = ((n, c) |-> seen)
+              steps' = if c == target then n |-> steps else steps
+              q' =
+                q
+                  >< mkSeq
+                    [ (n + 1, local c')
+                      | g `get` c == '.',
+                        c' <- neighborsNoDiags c
+                    ]
+           in go q' seen' steps'
+
+walk' :: Int -> Grid Char -> IO Int
+walk' n' g' = do
+  seenRef <- newIORef (mkSet [] :: Set (Int, Coord2))
+  nctToCsRef <- newIORef (mkMap [] :: Map (Int, Coord2, Int) [(Int, Coord2)])
+  stepsToCacheRef <- newIORef (mkMap [] :: Map Coord2 (Set Int))
+  let (w, h) = both (+ 1) (maxXY g')
+      start = gridFindOne 'S' g'
+      g = g' |. (start, '.')
+      local (x, y) = (x `mod` w, y `mod` h)
+      get g c = g |! local c
+      go target n c@(x, y)
+        | x < 0 || y < 0 || x >= w || y >= h = do
+            print "teleporting"
+            stepsToCache <- readIORef stepsToCacheRef
+            let teleportTo = local c
+            allStepsToTeleport <-
+              if teleportTo ∈ stepsToCache
+                then return (stepsToCache |! teleportTo)
+                else do
+                  let steps = traceShowId $ stepsTo g start teleportTo
+                  modifyIORef stepsToCacheRef (|. (teleportTo, steps))
+                  return steps
+            mconcat <$> sequence [go (target - nSteps) 0 teleportTo | nSteps <- unSet allStepsToTeleport]
+        | n == target = return [(n, c)]
+        | otherwise = do
+            print (n, target, c)
+            -- seen <- readIORef seenRef
+            -- if (n, c) ∈ seen
+            -- if (n, c, t) ∈ seen
+            --   then return []
+            --   else do
+            --     modifyIORef seenRef (<-| (n, c))
+            --     mconcat <$> sequence [go target (n + 1) c' | c' <- neighborsNoDiags c, g `get` c' /= '#']
+            nctToCs <- readIORef nctToCsRef
+            case nctToCs |? (n, c, target) of
+              Nothing -> do
+                ncs <- mconcat <$> sequence [go target (n + 1) c' | c' <- neighborsNoDiags c, g `get` c' == '.']
+                modifyIORef nctToCsRef (|. ((n, c, target), ncs))
+                return ncs
+              Just cs -> return cs
+   in size . mkSet <$> go n' 0 start
 
 part1 :: Int
 part1 = walk 64 $(grid input 21)
@@ -79,11 +131,12 @@ part1 = walk 64 $(grid input 21)
 -- part2 = walk' 26501365 $(grid input 21)
 part2 :: IO ()
 part2 = do
-  print $ walk' 6 $(grid exampleInput 21)
-  print $ walk' 10 $(grid exampleInput 21)
-  print $ walk' 50 $(grid exampleInput 21)
-  print $ walk' 100 $(grid exampleInput 21)
-  print $ walk' 500 $(grid exampleInput 21)
+  print =<< walk' 6 $(grid exampleInput 21)
+  print =<< walk' 10 $(grid exampleInput 21)
+  print =<< walk' 50 $(grid exampleInput 21)
+  print =<< walk' 100 $(grid exampleInput 21)
+  print =<< walk' 500 $(grid exampleInput 21)
+  return ()
 
 -- walk' 1000 $(grid exampleInput 21)
 -- walk' 5000 $(grid exampleInput 21)
