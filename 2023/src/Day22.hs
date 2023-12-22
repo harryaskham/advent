91ecaa639dea3a70546a9ac72673e47b2ed81135
Harry Askham <harryaskham@gmail.com>
2023-12-22 12:35:32 +0000

    [no commit message]

--- 2023/src/Day22.hs
+++ 2023/src/Day22.hs
@@ -19,14 +19,12 @@ onFloor ((_, _, z), (_, _, z')) = min z z' == 1
 
 freefall :: [Brick] -> [Brick]
 freefall =
-  let fallOnce :: [Brick] -> [Brick]
-      fallOnce bricks' =
-        let go bricks i =
-              let brick = bricks |! i
-               in if onFloor brick || (brick `restingOn`) `any` bricks
-                    then bricks
-                    else bricks |. (i, both (move3 D3zN 1) brick)
-         in snd <$> unMap (foldl' go (mkMap (zip [0 ..] bricks')) [0 .. length bricks' - 1])
+  let go bricks i =
+        let brick = bricks |! i
+         in if onFloor brick || (brick `restingOn`) `any` bricks
+              then bricks
+              else bricks |. (i, both (move3 D3zN 1) brick)
+      fallOnce bricks' = snd <$> unMap (foldl' go (mkMap (zip [0 ..] bricks')) [0 .. length bricks' - 1])
    in iterateFix fallOnce
 
 structure :: [Brick] -> (Map Brick (Set Brick), Map Brick (Set Brick))
