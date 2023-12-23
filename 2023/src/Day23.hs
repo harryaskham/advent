f06ed7af611d62bdcbd70a3bd3dc63d483b63042
Harry Askham <harryaskham@gmail.com>
2023-12-23 19:00:23 +0000

    [no commit message]

--- 2023/src/Day23.hs
+++ 2023/src/Day23.hs
@@ -5,20 +5,19 @@ slopePaths ns stopping start end g =
   let go Empty paths = paths
       go ((c, s) :<| q) paths
         | c == end = go q (end |-> s : paths)
-        | c /= start && stopping c = go q paths
-        | c ∈ s = go q paths
+        | c /= start && stopping c || c ∈ s = go q paths
         | otherwise =
-            let q' =
-                  mkSeq
-                    [ (c', c |-> s)
-                      | c' <- move <$> ns (g |! c) <*> pure 1 <*> pure c,
-                        g |? c' ∈ (Just <$> (".<>^v" :: String))
-                    ]
-             in go (q >< q') paths
+          let q' =
+                mkSeq
+                  [ (c', c |-> s)
+                    | c' <- move <$> ns (g |! c) <*> pure 1 <*> pure c,
+                      g |? c' ∈ (Just <$> (".<>^v" :: String))
+                  ]
+           in go (q >< q') paths
    in go (mkSeq [(start, (∅))]) []
 
-findForks :: Grid Char -> Set Coord2
-findForks =
+forks :: Grid Char -> Set Coord2
+forks =
   mkSet
     . gridFind True
     . convolve
@@ -48,24 +47,21 @@ allPaths graph start end = fromMaybe (-1) $ go start (∅)
       | a == end = Just 0
       | a ∈ seen = Nothing
       | otherwise =
-          let ls =
-                [ (-1 + size p +) <$> go b (a |-> seen)
-                  | (b, ps) <- unMap (graph |! a),
-                    b ∉ seen,
-                    p <- ps
-                ]
-           in case catMaybes ls of
-                [] -> Nothing
-                ls -> Just (maximum ls)
+        case catMaybes
+          [ (-1 + size p +) <$> go b (a |-> seen)
+            | (b, ps) <- unMap (graph |! a),
+              b ∉ seen,
+              p <- ps
+          ] of
+          [] -> Nothing
+          ls -> Just (maximum ls)
 
 parts :: (Int, Int)
 parts =
   let g = $(grid input 23)
-      (maxX, maxY) = maxXY g
-      (start, end) = ((1, 0), (maxX - 1, maxY))
+      (start, end) = ((1, 0), first (subtract 1) (maxXY g))
       paths = slopePaths (\c -> bool [fromArrow2 c] enumerate (c == '.')) (const False) start end g
-      forks = findForks g
-      graph = mkGraph g (mkSet [start, end] ∪ forks)
+      graph = mkGraph g (mkSet [start, end] ∪ forks g)
    in (subtract 1 . maximum . fmap length $ paths, allPaths graph start end)
 
 part1 :: Int
