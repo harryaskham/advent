7703d08fe502cc392f95c615133f579b98211bb6
Harry Askham <harryaskham@gmail.com>
2023-12-24 12:49:53 +0000

    [no commit message]

--- 2023/src/Day24.hs
+++ 2023/src/Day24.hs
@@ -1,5 +1,7 @@
 module Day24 where
 
+import Math.LinearEquationSolver
+
 -- {-
 -- t = x'-x/vx-vx'
 -- x'-x/vx-vx' = y'-y/vy-vy'
@@ -65,9 +67,97 @@ intersections lowHigh stones =
       validIntersection lowHigh (x', y') (vx', vy') i
   ]
 
+txyzStone :: ((Rational, Rational, Rational), (Rational, Rational, Rational)) -> Rational -> (Rational, Rational, Rational)
+txyzStone ((x, y, z), (vx, vy, vz)) t = (x + vx * t, y + vy * t, z + vz * t)
+
+-- vx*x + vy*y + vz*z + c = t
+-- c =
+
+-- z = dz/dy y + dz/dx x + c
+
+-- x(t) = xi + vx*t
+
+-- dz / dt = d / dt (dz / dy) y + (dz)
+
+-- we have (x,y,z) as function of t
+
+toLinear ((x, y, z), (vx, vy, vz)) =
+  ([vx, vy, vz, 1], -x * vx - y * vy - z * vz)
+
+-- [[vx / vz, vy / vz, ], [-x * vx - y * vy - z * vz]]
+-- [ [[vx], [x]],
+-- [[vy], [y]],
+-- [[vz], [z]]
+-- ]
+
+-- smashTime :: Intersection -> Maybe Rational
+-- smashTime NoIntersection = Nothing
+-- smashTime IntersectAt = undefined
+
+-- each is a 4d line, and we are trying to find a 4d line intersecting all
+-- every pair of lines forms a plane
+-- every pair of planes traces out a volume...
+-- get the hypersurface
+-- ie need the plane between the lines, t1 = first line, t2 = second line
+-- the plane swept out by the line between endpoints
+-- 2 lines:
+-- z = vz/vy y + vz/vx x + c
+
+-- line between:
+-- z'' = my'' * y'' + mx'' * x'' + c''
+-- where
+--  my'' * y + mx'' * x + c'' = z => my'' = (z - mx'' * x - c'') / y
+--  my'' * y' + mx'' * x' + c'' = z' => my'' = (z' - m''*x' - c'') / y'
+-- so my''(y-y') + mx''(x-x') = z-z'
+--  my'' = ((z-z')-mx''(x-x')) / (y-y')
+-- my'' = (z-z')/(y-y') - (x-x')/(y-y') * mx''
+-- also
+-- 2*c'' = (z+z') - my''(y+y') - mx''(x+x')
+-- 2*c'' = (z+z') - mx''*((z-z')(y+y')/(y-y') - (x-x')(y+y')/(y-y'))  - (x+x')
+-- mx'' = 2*c'' - (z+z')  /  ((z-z')(y+y')/(y-y') - (x-x')(y+y')/(y-y'))  - (x+x')
+
+-- (z - mx'' * x - c'') / y = (z-z')/(y-y') - (x-x')/(y-y') * ( 2*c'' - (z+z')  /  ((z-z')(y+y')/(y-y') - (x-x')(y+y')/(y-y'))  - (x+x') )
+
+-- so plane between:
+-- z'' = (y''*(z-z')/(y-y') - y''*((x-x')/(y-y') + 1) * mx'' * x'' + c''
+
+-- actual system is the line between endpoints
+-- hence the first pass gets us all the lines between all endpoints
+-- next pass
+
+solve (e, c) (e', c') =
+  traceShow ((e, c), (e', c')) $
+    case unsafePerformIO $ solveRationalLinearEqsAll Z3 2 [e, e'] [c, c'] of
+      [] -> Nothing
+      [[x, y, z, t], [x', y', z', t']] ->
+        traceShow ("solutions!", (x, y, z, t), (x', y', z', t')) $
+          let dt = t' - t
+              dt' = if dt == 0 then 1 else dt
+              dxdt = ((x' - x) / dt')
+              dydt = ((y' - y) / dt')
+              dzdt = ((z' - z) / dt')
+           in Just ([dxdt, dydt, dzdt, dt], -dxdt * x - dydt * y - dzdt * z)
+
+intersectReduce :: [([Rational], Rational)] -> [([Rational], Rational)]
+intersectReduce =
+  iterateFix
+    ( \surfaces ->
+        catMaybes
+          [ traceShowId $ solve a b
+            | (a, b) <- triPairs surfaces
+          ]
+    )
+
+plane ((x, y, z), (vx, vy, vz)) ((x', y', z'), (vx', vy', vz')) =
+  let (mx, my) = (vz / vx, vz / vy)
+      (mx', my') = (vz' / vx', vz' / vy')
+      c = z - my * y + mx * x
+      c' = z' - my' * y' + mx' * x'
+   in undefined
+
 stones :: [((Rational, Rational, Rational), (Rational, Rational, Rational))]
 stones =
-  $(input 24)
+  $(exampleInput 24)
     |- ( let c3 = toTuple3 <$> count 3 (number <* optional (char ',' >> many (char ' ')))
           in many1 ((,) <$> (c3 <* (string " @" >> many (char ' '))) <*> c3 <* eol) <* eof
        )
@@ -78,21 +168,13 @@ stones =
           )
       )
 
--- smashTime :: Intersection -> Maybe Rational
--- smashTime NoIntersection = Nothing
--- smashTime IntersectAt = undefined
-
--- each is a 4d line, and we are trying to find a 4d line intersecting all
-
-
 part1 :: Int
 part1 =
   stones
     & intersections (200000000000000 % 1, 400000000000000 % 1)
     & length
 
-part2 :: Int
 part2 =
   stones
-    & intersections (-1000000000000000000000000 % 1, 1000000000000000000000000 % 1)
-    & length
+    & fmap toLinear
+    & intersectReduce
