ba81153ebbe207ee09cda002453452b3dcc7bd3d
Harry Askham <harryaskham@gmail.com>
2024-01-02 18:55:18 +0000

    wo

--- adlude/src/Helper/Unary.hs
+++ adlude/src/Helper/Unary.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wno-missing-kind-signatures #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
 
 module Helper.Unary where
 
@@ -9,18 +10,24 @@ import Data.Foldable qualified as F
 λ :: ()
 λ = ()
 
--- Lift a value to a unary context.
-ɾ :: a -> Unary a
-ɾ = const
-
 -- Type for enabling unary prefix ops that take a placeholder as first argument.
 type Unary a = () -> a
 
+ɾ :: a -> Unary a
+ɾ a _ = a
+
 -- Partially applied u which resolves to a
 class IsUnary u a where
+  -- Resolve the thunk
   unary :: u -> a
+
+  -- Postfix forcing
   (⁻) :: u -> a
   (⁻) = unary
+
+  -- Prefix forcing
+  ƛ :: u -> a
+  ƛ = unary
 
 instance IsUnary (Unary a) a where
   unary f = f λ
@@ -34,33 +41,57 @@ instance {-# OVERLAPPABLE #-} (u ~ Unary a, IsUnary u a, Eq a) => Eq u where
 instance {-# OVERLAPPABLE #-} (u ~ Unary a, IsUnary u a, Ord a) => Ord u where
   a <= b = (unary a :: a) <= (unary b :: a)
 
-mkUnary1 :: IsUnary u a => (a -> b) -> Unary (u -> b)
-mkUnary1 f _ = f . unary
+instance {-# OVERLAPPABLE #-} (u ~ Unary a, IsUnary u a, Semigroup a) => Semigroup u where
+  a <> b = ɾ ((unary a :: a) <> (unary b :: a))
 
-mkUnary2 :: (IsUnary u a, IsUnary v b) => (a -> b -> c) -> Unary (u -> v -> c)
-mkUnary2 f _ u v = f (unary u) (unary v)
+instance {-# OVERLAPPABLE #-} (u ~ Unary a, IsUnary u a, Monoid a) => Monoid u where
+  mempty = ɾ (mempty :: a)
+
+mkUnary1 :: (IsUnary u a) => (a -> b) -> Unary (u -> b)
+mkUnary1 f _ u = f (unary u)
+
+mkUnary2 :: (IsUnary u a, IsUnary v b) => (a -> b -> c) -> u -> v -> Unary c
+mkUnary2 f u v _ = f (unary u) (unary v)
 
 (¬) :: IsUnary u Bool => Unary (u -> Bool)
 (¬) = mkUnary1 not
 
 infixr 3 ∧
 
-(∧) :: IsUnary u Bool => Unary (u -> u -> Bool)
+(∧) :: IsUnary u Bool => u -> u -> Unary Bool
 (∧) = mkUnary2 (&&)
 
 infixr 2 ∨
 
-(∨) :: IsUnary u Bool => Unary (u -> u -> Bool)
+(∨) :: IsUnary u Bool => u -> u -> Unary Bool
 (∨) = mkUnary2 (||)
 
-(⋀) :: Foldable t => Unary (t Bool -> Bool)
-_ ⋀ bs = and bs
+(⋀) :: (Foldable t, Functor t, IsUnary u Bool) => Unary (t u -> Bool)
+(⋀) _ = and . fmap unary
 
-(⋁) :: Foldable t => Unary (t Bool -> Bool)
-_ ⋁ bs = or bs
+(⋁) :: (Foldable t, Functor t, IsUnary u Bool) => Unary (t u -> Bool)
+(⋁) _ = or . fmap unary
 
-(∑) :: (Foldable f, Num a) => Unary (f a -> a)
-(∑) = const $ F.foldl' (+) 0
+(∑) :: (Foldable t, Functor t, Num a, IsUnary u a) => Unary (t u -> a)
+(∑) _ = F.foldl' (+) 0 . fmap unary
 
-(∏) :: (Foldable f, Num a) => Unary (f a -> a)
-(∏) = const $ F.foldl' (*) 1
+(∏) :: (Foldable t, Functor t, Num a, IsUnary u a) => Unary (t u -> a)
+(∏) _ = F.foldl' (*) 1 . fmap unary
+
+testUnary :: Bool
+testUnary =
+  let a :: Unary Int
+      a = (∏ [2 .. 4 :: Int])
+      b :: Bool
+      b = a == ɾ 24
+      c :: Unary Int
+      c = (∑ [1 .. 10 :: Int])
+      d :: Bool
+      d = c > ɾ 100
+      e :: Unary Bool
+      e = (¬ d)
+      f :: Unary Bool
+      f = ɾ b ∨ e
+      g :: Unary Bool
+      g = (⋀ [ɾ b, e, f])
+   in ƛ g
