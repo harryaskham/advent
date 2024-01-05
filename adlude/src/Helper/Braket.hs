745954d7f2c7afbbc5f37e5ded6bb8c0eb94e64a
Harry Askham <harryaskham@gmail.com>
2024-01-05 18:20:23 +0000

    [no commit message]

--- adlude/src/Helper/Braket.hs
+++ adlude/src/Helper/Braket.hs
@@ -1,4 +1,17 @@
+{-# OPTIONS_GHC -Wno-missing-kind-signatures #-}
+
 module Helper.Braket where
+
+import Helper.Alias
+
+data Braket a b = Braket a b
+
+newtype Bra' a = Bra' a
+
+newtype Ket a = Ket a
+
+-- To enable using partially applied bra-kets as bras
+type Bra a = () -> BraketClose -> Braket a ()
 
 data BraketDivider = Ⲓ
 
@@ -7,16 +20,45 @@ data BraketClose = BraketClose
 ᐳ :: BraketClose
 ᐳ = BraketClose
 
-ᕽ :: BraketClose
-ᕽ = BraketClose
-
-class Braket t a b where
+class Braketed t a b where
   く :: a -> BraketDivider -> b -> BraketClose -> t a b
   unBraket :: t a b -> (a, b)
+  unBra :: t a b -> a
+  unBra = fst . unBraket
+  unKet :: t a b -> b
+  unKet = snd . unBraket
 
-instance Braket (,) a b where
-  く a Ⲓ b _ = (a, b)
+instance Braketed Braket a b where
+  く a Ⲓ b ᐳ = Braket a b
+  unBraket (Braket a b) = (a, b)
+
+instance Braketed (,) a b where
+  く a Ⲓ b ᐳ = (a, b)
   unBraket = id
 
-a :: (Int, Int)
+class BraketedBra t a where
+  bra :: t -> Bra' a
+
+instance BraketedBra (Bra' a) a where
+  bra = id
+
+instance (Braketed t a b) => BraketedBra (b -> BraketClose -> t a b) a where
+  bra b = Bra' . fst . unBraket $ b (⊥) ᐳ
+
+class BraketedKet t a where
+  ket :: t -> Ket a
+
+ߊ :: a -> BraketClose -> Ket a
+ߊ a ᐳ = Ket a
+
+instance BraketedKet (Ket a) a where
+  ket = id
+
+a :: Braket Int Int
 a = く 1 Ⲓ 2 ᐳ
+
+b :: Bra Int
+b = く 1 Ⲓ
+
+c :: Ket Int
+c = ߊ 2 ᐳ
