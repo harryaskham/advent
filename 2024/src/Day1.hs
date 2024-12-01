1eb723efc8373a7092878df0f27f141f28aeaa26
Harry Askham <harryaskham@gmail.com>
2024-12-01 17:49:42 +0000

    unaryops

--- 2024/src/Day1.hs
+++ 2024/src/Day1.hs
@@ -4,10 +4,7 @@ lists :: ([Int], [Int])
 lists = $(input 1) |-.. twoOf (spaceTabs `surrounding` number @Int) & unzip
 
 part1 :: Int
-part1 = sum $ sort `both` lists &+> (abs .) . (-)
+part1 = ⵉ (sort `both` lists &+> (abs .) . (-))
 
 part2 :: Int
-part2 =
-  second countMap lists
-    &<@> (bicomp . ((*) &&& ((? 0) .<. flip (|?))))
-    & sum
+part2 = ⵉ (countMap `second` lists &<@> ((*) &.& (flip (|?) .>. (? 0))))
