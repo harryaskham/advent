213b461c797f44043d0bca8fd2c75d145c16b632
Harry Askham <harryaskham@gmail.com>
2024-12-01 10:27:37 +0000

    2024 tmpl

---
+++ 2024/src/Day2.hs
@@ -0,0 +1,32 @@
+module Day2 (part1, part2) where
+
+-- parser :: Parser [Int]
+-- parser = many1 (number <* eol) <* eof
+
+-- line :: Parser Int
+-- line = number
+
+-- data Cell
+--   = Empty
+--   | Wall
+--   deriving (Eq, Ord)
+
+-- instance GridCell Cell where
+--   charMap =
+--     BM.fromList
+--       [ (Empty, ' '),
+--         (Wall, '#')
+--       ]
+
+part1 :: Text
+part1 =
+  $(input 2)
+    -- & readAs (signed decimal)
+    -- & parseWith parser
+    -- & parseLinesWith line
+    -- & lines
+    -- & readGrid
+    & (<> "Part 1")
+
+part2 :: Text
+part2 = "Part 2"
