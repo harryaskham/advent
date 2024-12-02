d14e5b7030241f522b54d6d0ad7cf85779c3e9b0
Harry Askham <harryaskham@gmail.com>
2024-12-02 13:56:46 +0000

    Day2

--- 2024/src/Day2.hs
+++ 2024/src/Day2.hs
@@ -1,22 +1,21 @@
 module Day2 (part1, part2) where
 
-import Text.Parsec.Combinator
-
-safe :: 𝔹 -> Parser (Sum ℤ')
-safe skip = go skip []
+safe :: 𝔹 -> [ℤ'] -> Parser (Sum ℤ')
+safe skip ls =
+  trying
+    [ safe skip . (: ls) =<< level ls,
+      guard skip >> try (level [] >> safe False ls),
+      succeed (Sum 1)
+    ]
   where
-    go skip levels =
-      try (go skip . (: levels) =<< level levels)
-        <|> (guard skip >> try (level [] >> go False levels))
-        <|> (lookAhead eof $> Sum 1)
     level [] = wordOf $ number @ℤ'
-    level ls = do
-      l <- level []
-      let (d : ds) = diffs (l : ls)
-      l <$ guard (all ((`elem` [1, 2, 3]) . (* sgn d)) (d : ds))
+    level ls =
+      level [] >>= \l -> do
+        let (d : ds) = diffs (l : ls)
+        l <$ guard (all ((`elem` [1, 2, 3]) . (* sgn d)) (d : ds))
 
 part1 :: ℤ'
-part1 = $(input 2) |-<> safe False
+part1 = $(input 2) |-<> safe False []
 
 part2 :: ℤ'
-part2 = $(input 2) |-<> safe True
+part2 = $(input 2) |-<> safe True []
