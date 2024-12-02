f49c1fb0159aecc27eb031fa8666598719f42ba4
Harry Askham <harryaskham@gmail.com>
2024-12-02 18:49:51 +0000

    subscripts

--- 2024/src/Day2.hs
+++ 2024/src/Day2.hs
@@ -1,6 +1,6 @@
 module Day2 (part1, part2) where
 
-safe :: 𝔹 -> [ℤ'] -> Parser (Σ ℤ')
+safe :: 𝔹 -> [ℤ₆₄] -> Parser (Σ ℤ₆₄)
 safe skip ls =
   trying
     [ safe skip . (: ls) =<< level ls,
@@ -8,14 +8,14 @@ safe skip ls =
       succeed (Σ 1)
     ]
   where
-    level [] = wordOf $ number @ℤ'
+    level [] = wordOf $ number @ℤ₆₄
     level ls =
       level [] >>= \l -> do
         let (d : ds) = diffs (l : ls)
         l <$ guard (all ((`elem` [1, 2, 3]) . (* sgn d)) (d : ds))
 
-part1 :: ℤ'
+part1 :: ℤ₆₄
 part1 = $(input 2) |-<> safe False []
 
-part2 :: ℤ'
+part2 :: ℤ₆₄
 part2 = $(input 2) |-<> safe True []
