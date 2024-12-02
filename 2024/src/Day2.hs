d24c1b49c5cc8d625137b4c7f0b790858b611150
Harry Askham <harryaskham@gmail.com>
2024-12-02 19:36:42 +0000

    good

--- 2024/src/Day2.hs
+++ 2024/src/Day2.hs
@@ -3,16 +3,14 @@ module Day2 (part1, part2) where
 safe :: 𝔹 -> [ℤ₆₄] -> Parser (Σ ℤ₆₄)
 safe skip ls =
   trying
-    [ safe skip . (: ls) =<< level ls,
-      guard skip >> try (level [] >> safe False ls),
+    [ do
+        ls' <- wordOf number <&> (: ls)
+        let (d : ds) = diffs ls'
+        guard (null ls || all ((`elem` [1, 2, 3]) . (* sgn d)) (d : ds))
+        safe skip ls',
+      guard skip >> anyWord >> safe False ls,
       succeed (Σ 1)
     ]
-  where
-    level [] = wordOf $ number @ℤ₆₄
-    level ls =
-      level [] >>= \l -> do
-        let (d : ds) = diffs (l : ls)
-        l <$ guard (all ((`elem` [1, 2, 3]) . (* sgn d)) (d : ds))
 
 part1 :: ℤ₆₄
 part1 = $(input 2) |-<> safe False []
