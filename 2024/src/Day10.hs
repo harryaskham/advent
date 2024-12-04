0ee232e44dad4f95ae760d465093bbe20b0009ef
Harry Askham <harryaskham@gmail.com>
2024-12-04 08:24:08 +0000

    tmpl

--- 2024/src/Day10.hs
+++ 2024/src/Day10.hs
@@ -1,23 +1,5 @@
 module Day10 (part1, part2) where
 
--- parser :: Parser [Int]
--- parser = many1 (number <* eol) <* eof
-
--- line :: Parser Int
--- line = number
-
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
-
--- instance GridCell Cell where
---   charMap =
---     BM.fromList
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
-
 part1 :: Text
 part1 =
   $(input 10)
