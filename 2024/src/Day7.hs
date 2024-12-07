eb5ec8d6a229985641cb8d036ea14f2b6bec19cd
Harry Askham <harryaskham@gmail.com>
2024-12-07 16:57:42 +0000

    day7

--- 2024/src/Day7.hs
+++ 2024/src/Day7.hs
@@ -1,18 +1,16 @@
 module Day7 (part1, part2) where
 
-sat :: [ℤ -> ℤ -> ℤ] -> Parser (Σ ℤ)
-sat ops = do
-  t <- (number @ℤ) <* string ": "
-  xs <- many1 (wordOf (number @ℤ)) <* eof
-  let f s (x : xs) = mconcat [f (s `op` x) xs | op <- ops]
-      f s [] = [s]
-  let ts = f 0 xs
-  return $ if t `elem` ts then Σ t else Σ 0
+with :: [ℤ -> ℤ -> ℤ] -> Parser (Σ ℤ)
+with ops =
+  let f (x : xs) = (ops >>=) . ((f xs . ($ x)) .<. (&))
+      f [] = pure
+      g t = bool (Σ 0) (Σ t) . (t ∈) . ((⇄ f) 0)
+   in g <$@> snoc <$> numbers
 
 part1 :: Σ ℤ
-part1 = $(aoc 7) |-<..?!> sat [(+), (*)]
+part1 = $(aoc 7) |-<..?!> with [(+), (*)]
 
 part2 :: Σ ℤ
 part2 =
   let a || b = number -| show a <> show b
-   in $(aoc 7) |-<..?!> sat [(+), (*), (||)]
+   in $(aoc 7) |-<..?!> with [(+), (*), (||)]
