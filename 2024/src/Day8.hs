aab8e5cd945e9848876d86694ffba5bffbcbd18e
Harry Askham <harryaskham@gmail.com>
2024-12-08 14:31:20 +0000

    day8

--- 2024/src/Day8.hs
+++ 2024/src/Day8.hs
@@ -1,19 +1,22 @@
 module Day8 (part1, part2) where
 
 antinodes :: ℤ -> ℤ -> Map Char [ℤ × ℤ] -> [ℤ × ℤ]
-antinodes l n =
-  nub ∘ (>>= ((>>= (\(a, b) -> bool [b + (i, i) ⋅ (b - a) | i <- [l .. n]] [] (a ≡ b))) ∘ uncurry cartesian ∘ dup)) ∘ values ∘ (|/ '.')
+antinodes l u =
+  nub ∘ (>>= ((>>= (\(a, b) -> bool [b + (i, i) ⋅ (b - a) | i <- [l .. u]] [] (a ≡ b))) ∘ uncurry cartesian ∘ dup)) ∘ values ∘ (|/ '.')
 
-solve l n =
-  readGrid @Grid' @(ℤ × ℤ) @Char $(aoc 8)
-    & dup
-    & ((∋) &<$>& (antinodes l n ∘ co @(Grid' (ℤ × ℤ) Char) @(Map Char [ℤ × ℤ])))
+solve l u g =
+  dup g
+    & ((∋) &<$>& (antinodes l u ∘ co @(Grid' (ℤ × ℤ) Char) @(Map Char [ℤ × ℤ])))
     & filter (≡ True)
     & size
 
 part1 :: ℤ
-part1 = solve 1 1
+part1 =
+  readGrid @Grid' @(ℤ × ℤ) @Char $(aoc 8)
+    & solve 1 1
 
 -- 946 too low
 part2 :: ℤ
-part2 = stabilize (solve 0) [100 ..]
+part2 =
+  readGrid @Grid' @(ℤ × ℤ) @Char $(aoc 8)
+    & (\g -> solve 0 (max $@ maxXY g))
