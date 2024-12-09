2ba3a9258fc67ef0cfb50777025832a91bfb9a59
Harry Askham <harryaskham@gmail.com>
2024-12-09 23:13:14 +0000

    [no commit message]

--- 2024/src/Day9.hs
+++ 2024/src/Day9.hs
@@ -7,27 +7,30 @@ defragment by =
       (ends, files, _) =
         foldl'
           ( \(ends, files, block) (i, (free, occ)) ->
-              ( if free > 0 then (block, block + free - 1) : ends else ends,
+              ( if free > 0 then ends |. (block, block + free - 1) else ends,
                 (i, (block + free, block + free + occ - 1)) : files,
                 block + free + occ
               )
           )
-          ([], [], 0)
+          (ø, ø, 0)
           (zip [0 ..] freeOcc)
       (_, checksum) =
         foldl'
           ( \(ends, score) (i, r@(fa, fb)) ->
               let blocks = fb - fa + 1
-                  go [] = (r, [])
-                  go ((a, b) : rest)
-                    | a > fa = (r, (a, b) : rest)
-                    | a + blocks - 1 ≡ b = ((a, b), sort (r : rest))
-                    | a + blocks - 1 < b = ((a, a + blocks - 1), sort (r : (a + blocks, b) : rest))
-                    | otherwise = second ((a, b) :) (go rest)
+                  go q
+                    | nullQ q = (r, ø)
+                    | otherwise =
+                        let ((a, b), q') = (q <!)
+                         in if
+                              | a > fa -> (r, q' |. (a, b))
+                              | a + blocks - 1 ≡ b -> ((a, b), q' |. r)
+                              | a + blocks - 1 < b -> ((a, a + blocks - 1), q' |. r |. (a + blocks, b))
+                              | otherwise -> second (|. (a, b)) (go q')
                   (r', ends') = go ends
                in (ends', score + scoreRange i r')
           )
-          (reverse ends, 0)
+          (ends, 0)
           (by files)
    in checksum
 
