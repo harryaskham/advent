56c56202ba82b8f846ff02e560a44ed16d983a58
Harry Askham <harryaskham@gmail.com>
2024-12-10 02:04:58 +0000

    [no commit message]

--- 2024/src/Day9.hs
+++ 2024/src/Day9.hs
@@ -2,37 +2,35 @@ module Day9 (part1, part2) where
 
 defragment :: ([(ℤ, (ℤ, ℤ))] -> [(ℤ, (ℤ, ℤ))]) -> ℤ
 defragment by =
-  let freeOcc = ('0' : $(aoc 9)) |- many (twoOf (fromIntegral ∘ digitToInt <$> digit))
-      scoreRange i (a, b) = i ⋅ (triangular b - triangular (a - 1))
+  let freeFull = ('0' : $(aoc 9)) |- many (twoOf (fromIntegral ∘ digitToInt <$> digit))
       (ends, files, _) =
         foldl'
-          ( \(ends, files, block) (i, (free, occ)) ->
-              ( if free > 0 then ends |. (block, block + free - 1) else ends,
-                (i, (block + free, block + free + occ - 1)) : files,
-                block + free + occ
+          ( \(ends, files, start) (i, (free, full)) ->
+              ( free > 0 ??? ends $ ends |. (start, start + free - 1),
+                (i, (start + free, start + free + full - 1)) : files,
+                start + free + full
               )
           )
           (ø, ø, 0)
-          (zip [0 ..] freeOcc)
-      (_, checksum) =
+          (freeFull ..#)
+      place r@(fa, fb) ends
+        | nullQ ends = (ø, r)
+        | a > fa = (ends' |. (a, b), r)
+        | a + blocks - 1 ≡ b = (ends' |. r, (a, b))
+        | a + blocks - 1 < b = (ends' |. r |. (a + blocks, b), (a, a + blocks - 1))
+        | otherwise = first (|. (a, b)) (place r ends')
+        where
+          ((a, b), ends') = (ends <!)
+          blocks = fb - fa + 1
+   in snd $
         foldl'
-          ( \(ends, score) (i, r@(fa, fb)) ->
-              let blocks = fb - fa + 1
-                  go q
-                    | nullQ q = (r, ø)
-                    | otherwise =
-                        let ((a, b), q') = (q <!)
-                         in if
-                              | a > fa -> (r, q' |. (a, b))
-                              | a + blocks - 1 ≡ b -> ((a, b), q' |. r)
-                              | a + blocks - 1 < b -> ((a, a + blocks - 1), q' |. r |. (a + blocks, b))
-                              | otherwise -> second (|. (a, b)) (go q')
-                  (r', ends') = go ends
-               in (ends', score + scoreRange i r')
+          ( \(ends, score) (i, r) ->
+              second
+                ((+ score) ∘ (⋅ i) ∘ ((-) $@) ∘ (triangular <:>) ∘ second (subtract 1) ∘ swap)
+                (place r ends)
           )
           (ends, 0)
           (by files)
-   in checksum
 
 part1 :: ℤ
 part1 = defragment $ \files -> [(i, (block, block)) | (i, (a, b)) <- files, block <- [a .. b]]
