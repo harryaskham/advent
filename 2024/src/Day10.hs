7cefc30ccb9e16f0280e10d5b1a5cf4192cdc7af
Harry Askham <harryaskham@gmail.com>
2024-12-10 11:56:25 +0000

    [no commit message]

--- 2024/src/Day10.hs
+++ 2024/src/Day10.hs
@@ -2,21 +2,16 @@ module Day10 (part1, part2) where
 
 trails :: 𝔹 -> ℤ² ℕ₁₀ -> (ℤ × ℤ) -> [ℤ × ℤ]
 trails skip g c =
-  let go (c :<| cs) seen nines
+  let uphill n = g |! n ≡ g |! c + 1
+      go (c :<| cs) seen nines
         | skip ∧ c ∈ seen = go cs seen nines
         | g |! c ≡ 9 = go cs (c |-> seen) (c : nines)
-        | otherwise =
-            let ns = [n | n <- neighs @4 c g, g |! n ≡ g |! c + 1]
-             in go (cs >< ns) (c |-> seen) nines
+        | otherwise = go (cs >< (neighbors @4 c g |-?-> uphill)) (c |-> seen) nines
       go _ _ nines = nines
    in go (mk₁ c) ø ø
 
 hike :: ([ℤ × ℤ] -> [ℤ × ℤ]) -> 𝔹 -> Σ ℤ
-hike f skip =
-  ( readGrid $(aoc 10)
-      ⥢ (((Σ ∘ size ∘ f) ∘<∘ trails skip) &<$>& (|?> (0 :: ℕ₁₀)))
-      <>!
-  )
+hike f skip = (readGrid $(aoc 10) ⥢ (((Σ ∘ size ∘ f) ∘<∘ trails skip) &<$>& (|?> (0 :: ℕ₁₀))) <>!)
 
 part1 :: Σ ℤ
 part1 = hike nub True
