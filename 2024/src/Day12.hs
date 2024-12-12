17cb30afad88fa70a8fccee12084a10f50069e64
Harry Askham <harryaskham@gmail.com>
2024-12-12 18:01:30 +0000

    [no commit message]

--- 2024/src/Day12.hs
+++ 2024/src/Day12.hs
@@ -1,33 +1,26 @@
 module Day12 (part1, part2) where
 
 region :: ℤ² Char -> (ℤ × ℤ) -> (Set (ℤ × ℤ), ℤ, ℤ, ℤ)
-region g c = go (mk₁ c) ø 0 0 ø
-  where
-    go Empty r area perim segments = (r, area, perim, sides segments)
-    go (c :<| cs) r area perim segments
-      | c ∈ r = go cs r area perim segments
-      | otherwise =
-          let ns = [n | n <- neighs @4 c g, g |! n ≡ g |! c]
-              segments' = [(n - c, [c]) | n <- vicinity @4 c, g |? n /= g |? c]
-           in go (cs >< ns) (c |-> r) (area + 1) (perim + 4 - size ns) (segments >< segments')
-    sides segments = size $ mconcat $ measure <$@> (unMap (mkWith (<>) segments))
-    measure orientation cs =
-      case abs orientation of
-        (0, 1) -> groupBy (\(x, y) (x', y') -> y ≡ y' ∧ diff x x' ≡ 1) (sortOn swap cs)
-        (1, 0) -> groupBy (\(x, y) (x', y') -> x ≡ x' ∧ diff y y' ≡ 1) (sort cs)
+region g c =
+  let go r Empty segs area perim = (r, area, perim, sides segs)
+      go r (((∈ r) -> True) :<| cs) segs area perim = go r cs segs area perim
+      go r (c :<| cs) segs area perim =
+        let ns = [n | n <- neighs @4 c g, g |! n ≡ g |! c]
+            segs' = [(n - c, [c]) | n <- vicinity @4 c, g |? n /= g |? c]
+         in go (c |-> r) (cs >< ns) (segs >< segs') (area + 1) (perim + 4 - size ns)
+      sides segs = size ∘ mconcat $ measure <$@> unMap (mkWith (<>) segs)
+      measure orientation cs =
+        case abs orientation of
+          (0, 1) -> groupBy (\(x, y) (x', y') -> y ≡ y' ∧ diff x x' ≡ 1) (sortOn swap cs)
+          (1, 0) -> groupBy (\(x, y) (x', y') -> x ≡ x' ∧ diff y y' ≡ 1) (sort cs)
+   in go ø (mk₁ c) ø 0 0
 
 prices :: ℤ² Char -> (ℤ, ℤ)
-prices g = go (mk $ coords g) 0 0
-  where
-    go cs perimPrice sidePrice
-      | null cs = (perimPrice, sidePrice)
-      | otherwise =
-          let c = arbitrary cs
-              (r, area, perim, sides) = region g c
-           in go (cs \\ r) (perimPrice + area ⋅ perim) (sidePrice + area ⋅ sides)
+prices g =
+  let go (null -> True) ps = ps
+      go cs ps =
+        let (r, area, perim, sides) = region g (arbitrary cs)
+         in go (cs \\ r) (bimap (+ area ⋅ perim) (+ area ⋅ sides) ps)
+   in go (mk $ coords g) (0, 0)
 
-part1 :: ℤ
-part1 = $(aoc 12) & readGrid & prices & fst
-
-part2 :: ℤ
-part2 = $(aoc 12) & readGrid & prices & snd
+(part1, part2) :: (ℤ, ℤ) = prices (readGrid $(aoc 12))
