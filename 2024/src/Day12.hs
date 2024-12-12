f33b69d534f396737ba35dc9219d79d34666a811
Harry Askham <harryaskham@gmail.com>
2024-12-12 17:04:56 +0000

    [no commit message]

--- 2024/src/Day12.hs
+++ 2024/src/Day12.hs
@@ -1,38 +1,20 @@
 module Day12 (part1, part2) where
 
-import Data.List (groupBy)
-
 region :: ℤ² Char -> (ℤ × ℤ) -> (Set (ℤ × ℤ), ℤ, ℤ, ℤ)
 region g c = go (mk₁ c) ø 0 0 ø
   where
-    go Empty r area perim segments =
-      ( r,
-        area,
-        perim,
-        sides segments
-      )
+    go Empty r area perim segments = (r, area, perim, sides segments)
     go (c :<| cs) r area perim segments
       | c ∈ r = go cs r area perim segments
       | otherwise =
-          -- traceShow (c, area, perim, segments) $
           let ns = [n | n <- neighs @4 c g, g |! n ≡ g |! c]
               segments' = [(n - c, [c]) | n <- vicinity @4 c, g |? n /= g |? c]
            in go (cs >< ns) (c |-> r) (area + 1) (perim + 4 - size ns) (segments >< segments')
     sides segments = size $ mconcat $ measure <$@> (unMap (mkWith (<>) segments))
     measure orientation cs =
       case abs orientation of
-        (0, 1) -> grpBy (\(x, y) (x', y') -> y ≡ y' ∧ diff x x' ≡ 1) (sortOn swap cs)
-        (1, 0) -> grpBy (\(x, y) (x', y') -> x ≡ x' ∧ diff y y' ≡ 1) (sort cs)
-
-grp :: (a -> a -> Bool) -> [a] -> [a] -> [[a]]
-grp _ [] [] = []
-grp _ cur [] = [cur]
-grp f [] (a : as) = grp f [a] as
-grp f cur@(cura : _) (a : as)
-  | f cura a = grp f (a : cur) as
-  | otherwise = (a : cur) : (grp f [a] as)
-
-grpBy f = grp f []
+        (0, 1) -> groupBy (\(x, y) (x', y') -> y ≡ y' ∧ diff x x' ≡ 1) (sortOn swap cs)
+        (1, 0) -> groupBy (\(x, y) (x', y') -> x ≡ x' ∧ diff y y' ≡ 1) (sort cs)
 
 prices :: ℤ² Char -> (ℤ, ℤ)
 prices g = go (mk $ coords g) 0 0
