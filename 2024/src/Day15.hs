c4a82607b16886abbda20601d803e925bf4ae6be
Harry Askham <harryaskham@gmail.com>
2024-12-15 19:28:25 +0000

    [no commit message]

--- 2024/src/Day15.hs
+++ 2024/src/Day15.hs
@@ -10,64 +10,48 @@ gps (x, y) = Σ (100 ⋅ y + x)
 
 step :: ℤ² -> [Dir²] -> G ℤ² C -> G ℤ² C
 step _ [] g = g
-step robot (dir : dirs) g =
-  -- traceShow g $
-  -- pauseId $
-  -- traceAnim 1 g $
-  case go robot robot dir g of
-    Nothing -> step robot dirs g
-    Just (g', robot') -> step robot' dirs g'
+step robot (dir : dirs) g
+  | g |! robot' ≡ W = step robot dirs g
+  | g |! robot' ≡ S = step robot' dirs g
+  | otherwise =
+      case push [robot] of
+        Just moves -> step robot' dirs (foldl' (&) g moves)
+        Nothing -> step robot dirs g
   where
-    go s robot dir g =
-      -- traceShow ("one move:", s, robot, dir) $
-      let s' = move @ℤ dir 1 s
-          push cs
-            | otherwise =
-                let cs' = move @ℤ dir 1 <$> cs
-                    mov c = (\g -> g ||. (c, S) ||. (move @ℤ dir 1 c, g |! c))
-                    movs cs = (\g -> foldl' (\g c -> mov c g) g cs)
-                    affected = (g |!) <$> cs
-                    expandedAffected =
-                      nub $
-                        mconcat $
-                          [ case (g |! c, dir) of
-                              (OL, DirRight) -> [c]
-                              (OR, DirRight) -> [c]
-                              (OR, DirLeft) -> [c]
-                              (OL, DirLeft) -> [c]
-                              (OL, DirUp) -> [c, (x + 1, y)]
-                              (OL, DirDown) -> [c, (x + 1, y)]
-                              (OR, DirUp) -> [c, (x - 1, y)]
-                              (OR, DirDown) -> [c, (x - 1, y)]
-                              (S, _) -> []
-                              (W, _) -> []
-                            | c@(x, y) <- cs'
-                          ]
-                 in -- traceShow ("pushing", cs, "to", cs', "affecting", expandedAffected, (g |!) <$> expandedAffected) $
-                    if
-                      | any (≡ W) (((g |!) <$> (move @ℤ dir 1 <$> expandedAffected))) -> Nothing
-                      | all (≡ S) (((g |!) <$> (move @ℤ dir 1 <$> expandedAffected))) -> Just $ [movs expandedAffected] <> [movs cs]
-                      | otherwise -> (<>) <$> push expandedAffected <*> Just ([movs cs])
-       in case (g |! s', push [s]) of
-            (W, _) -> Nothing
-            (_, Nothing) -> Nothing
-            (_, Just moves) -> Just $ (foldl' (&) g moves, s')
+    robot' = move @ℤ dir 1 robot
+    push cs =
+      let cs' = move @ℤ dir 1 <$> cs
+          movs = flip $ foldl' (\g c -> g ||. (c, S) ||. (move @ℤ dir 1 c, g |! c))
+          affected =
+            nub $
+              mconcat $
+                [ case (g |! c, dir) of
+                    (O, _) -> [c]
+                    (_, DirUp) -> [c, (x + 1, y)]
+                    (_, DirDown) -> [c, (x - 1, y)]
+                    (_, DirRight) -> [c]
+                    (_, DirLeft) -> [c]
+                  | c@(x, y) <- cs'
+                ]
+          affected' = (g |!) ∘ move @ℤ dir 1 <$> affected
+       in if
+            | any (≡ W) affected' -> Nothing
+            | all (≡ S) affected' -> Just $ [movs affected] <> [movs cs]
+            | otherwise -> (<>) <$> push affected <*> Just [movs cs]
 
--- $> part1
-
-wide :: G ℤ² C -> G ℤ² C
-wide g =
+widen :: G ℤ² C -> G ℤ² C
+widen g =
   mkGrid
-    ( [ [((2 ⋅ x, y), l), ((2 ⋅ x + 1, y), r)]
-        | ((x, y), v) <- unGrid g,
-          let (l, r) = case v of
-                R -> (R, S)
-                O -> (OL, OR)
-                W -> (W, W)
-                S -> (S, S)
-      ]
-        <>!
-    )
+    [ c
+      | ((x, y), v) <- unGrid g,
+        let (l, r) = wide v,
+        c <- [((2 ⋅ x, y), l), ((2 ⋅ x + 1, y), r)]
+    ]
+  where
+    wide R = (R, S)
+    wide O = (OL, OR)
+    wide W = (W, W)
+    wide S = (S, S)
 
 sim :: (G ℤ² C -> G ℤ² C) -> C -> Σ ℤ
 sim f o =
@@ -80,7 +64,7 @@ sim f o =
    in (step (g |!> R) dirs g |?> o <&> gps <>!)
 
 part1 :: Σ ℤ
-part1 = pure 0 -- sim id O
+part1 = sim id O
 
 part2 :: Σ ℤ
-part2 = sim wide OL
+part2 = sim widen OL
