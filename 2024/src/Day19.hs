da3ab79e3979e292d7fc42f73696620841edd01b
Harry Askham <harryaskham@gmail.com>
2024-12-19 17:36:59 +0000

    [rapid commit; squash later]

--- 2024/src/Day19.hs
+++ 2024/src/Day19.hs
@@ -1,6 +1,5 @@
 module Day19 (part1, part2) where
 
 (part1, part2) :: (Σ ℤ, Σ ℤ) =
-  let go ts "" = pure (Σ 1)
-      go ts p = go ts .=<<. (stripPrefix <$> splitOn ", " ts <*> [p] <>?)
+  let go t p = p ≡ ø ??? go t .=<<. (stripPrefix <$> splitOn ", " t <*> [p] <>?) $ (Σ 1 #)
    in ((⥢ ((±) ⇱)) ∘ run ∘<∘ go &<$> snoc (flines $(aoc 19)) <>!)
