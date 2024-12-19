93cdd803814afe80a075d6f3ee3fb01def90a094
Harry Askham <harryaskham@gmail.com>
2024-12-19 13:28:53 +0000

    [rapid commit; squash later]

--- 2024/src/Day19.hs
+++ 2024/src/Day19.hs
@@ -1,7 +1,7 @@
 module Day19 (part1, part2) where
 
 (part1, part2) :: (Σ ℤ, Σ ℤ) =
-  let (ts : _ : ps) = lines $(aocx 19)
-      go "" = return (Σ 1, Σ 1)
-      go p = go .=<<. (stripPrefix <$> splitOn ", " ts <*> pure p <>?)
-   in (first signum ∘ run ∘ (go :: Text .->. (Σ ℤ, Σ ℤ)) <$> ps <>!)
+  let (ts : _ : ps) = lines $(aoc 19)
+      go "" = pure (Σ 1)
+      go p = go .=<<. (stripPrefix <$> splitOn ", " ts <*> [p] <>?)
+   in ((⥢ first signum) ∘ run ∘ (go :: Text .->. Σ ℤ) <$> ps <>!)
