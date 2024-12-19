3791ecc7ab2357ea276536dee5e3e46bca4a783c
Harry Askham <harryaskham@gmail.com>
2024-12-19 13:04:32 +0000

    [rapid commit; squash later]

--- 2024/src/Day19.hs
+++ 2024/src/Day19.hs
@@ -1,13 +1,7 @@
 module Day19 (part1, part2) where
 
 (part1, part2) :: (Σ ℤ, Σ ℤ) =
-  (((⥢ (first signum)) ∘ run ∘ go <$> ps) <>!)
-  where
-    (ts' : _ : ps) = lines $(aoc 19)
-    ts = splitOn ", " ts'
-    go :: Text .->. Σ ℤ
-    go "" = return (Σ 1)
-    go p =
-      case catMaybes (stripPrefix <$> ts <*> pure p) of
-        [] -> return (Σ 0)
-        ps -> ([memo go p | p <- ps] <>!)
+  let (ts : _ : ps) = lines $(aoc 19)
+      go "" = return (Σ 1)
+      go p = go .=<<. (stripPrefix <$> splitOn ", " ts <*> pure p <>?)
+   in ((⥢ (first signum)) ∘ run ∘ (go :: Text .->. Σ ℤ) <$> ps <>!)
