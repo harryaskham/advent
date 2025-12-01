d2c1709f0fc4586b75f35f9d3596a568c2d168b6
Harry Askham <harryaskham@gmail.com>
2025-12-01 08:37:32 +0000

    day1

--- 2025/test/Spec.hs
+++ 2025/test/Spec.hs
@@ -30,38 +30,8 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  let parts =
-        mkVec
-          [ (Day1.part1, Day1.part2),
-            (Day2.part1, Day2.part2),
-            (Day3.part1, Day3.part2),
-            (Day4.part1, Day4.part2),
-            (Day5.part1, Day5.part2),
-            (Day6.part1, Day6.part2),
-            (Day7.part1, Day7.part2),
-            (Day8.part1, Day8.part2),
-            (Day9.part1, Day9.part2),
-            (Day10.part1, Day10.part2),
-            (Day11.part1, Day11.part2),
-            (Day12.part1, Day12.part2),
-            (Day13.part1, Day13.part2),
-            (Day14.part1, Day14.part2),
-            (Day15.part1, Day15.part2),
-            (Day16.part1, Day16.part2),
-            (Day17.part1, Day17.part2),
-            (Day18.part1, Day18.part2),
-            (Day19.part1, Day19.part2),
-            (Day20.part1, Day20.part2),
-            (Day21.part1, Day21.part2),
-            (Day22.part1, Day22.part2),
-            (Day23.part1, Day23.part2),
-            (Day24.part1, Day24.part2),
-            (Day25.part1, Day25.part2)
-          ]
-  forM_ [1 .. 25] $ \day ->
-    describe ("Day " <> show day) do
-      let part = (parts V.! (day - 1))
-      it "computes Part 1" do
-        fst part `shouldBe` "Part 1"
-      it "computes Part 2" do
-        snd part `shouldBe` "Part 2"
+  describe ("Day 1") do
+    it "computes Part 1" do
+      Day1.part1 `shouldBe` 1129
+    it "computes Part 2" do
+      Day1.part2 `shouldBe` 6638
