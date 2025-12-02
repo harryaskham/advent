009ce66316146d12868127c5875fa95279e4b036
Harry Askham <harryaskham@gmail.com>
2025-12-02 20:41:18 +0000

    [rapid commit; squash later]

--- 2025/src/Day2.hs
+++ 2025/src/Day2.hs
@@ -1,17 +1,17 @@
 module Day2 (part1, part2) where
 
-invalid :: ℤ -> ℤ -> Bool
-invalid i s =
-  let l = (floor (logBase 10 (fromInteger i)) + 1)
-      t = l `div` s
+invalid :: ℤ -> ℤ -> ℤ -> Bool
+invalid i l s =
+  let t = l `div` s
       r = i `div` (10 ^ (l - t))
       o = sum [r * 10 ^ j | j <- [0, t .. l - 1]]
    in t * s == l && i == o
 
 aRange :: [ℤ] -> Parser (Σ ℤ)
-aRange r = do
+aRange s = do
   (a, b) <- (,) <$> (number <* char '-') <*> number
-  pure ([Σ i | i <- [a .. b], any (invalid i) r] <>!)
+  let o = [Σ i | i <- [a .. b], let l = floor (logBase 10 (fromInteger i)) + 1, any (invalid i l) s]
+  pure (o <>!)
 
 part1 :: Σ ℤ
 part1 = $(aoc 2) |-<> csvOf (aRange [2])
