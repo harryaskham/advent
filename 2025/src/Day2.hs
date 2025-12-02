71183b1c2f3a190fa994a707eb5064d2b2aaa847
Harry Askham <harryaskham@gmail.com>
2025-12-02 20:38:17 +0000

    [rapid commit; squash later]

--- 2025/src/Day2.hs
+++ 2025/src/Day2.hs
@@ -1,24 +1,20 @@
 module Day2 (part1, part2) where
 
-invalid :: Integer -> Integer -> Bool
+invalid :: ℤ -> ℤ -> Bool
 invalid i s =
-  let n = (floor (logBase 10 (fromInteger i)) + 1)
-      t = n `div` s
-      e = 10 ^ t
-      r = i `div` (10 ^ (n - t))
-      o = sum [r * 10 ^ j | j <- [0, t .. n - 1]]
-   in if t == 0 || t >= n || n `mod` t /= 0
-        then False
-        -- else traceShow (i, s, t, n, e, r, o) $ i == o
-        else i == o
+  let l = (floor (logBase 10 (fromInteger i)) + 1)
+      t = l `div` s
+      r = i `div` (10 ^ (l - t))
+      o = sum [r * 10 ^ j | j <- [0, t .. l - 1]]
+   in t * s == l && i == o
 
-oneRange :: [Integer] -> Parser (Σ Integer)
-oneRange r = do
+aRange :: [ℤ] -> Parser (Σ ℤ)
+aRange r = do
   (a, b) <- (,) <$> (number <* char '-') <*> number
-  return (mconcat [Σ i | i <- [a .. b], any (invalid i) r])
+  pure ([Σ i | i <- [a .. b], any (invalid i) r] <>!)
 
-part1 :: Σ Integer
-part1 = $(aoc 2) |-<> oneRange [2] `sepBy` char ','
+part1 :: Σ ℤ
+part1 = $(aoc 2) |-<> csvOf (aRange [2])
 
-part2 :: Σ Integer
-part2 = $(aoc 2) |-<> oneRange [1 .. 10] `sepBy` char ','
+part2 :: Σ ℤ
+part2 = $(aoc 2) |-<> csvOf (aRange [1 .. 10])
