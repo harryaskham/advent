7213612c0db85160396c944b7e8e24f58988b608
Harry Askham <harryaskham@gmail.com>
2025-12-03 16:45:02 +0000

    day3

--- 2025/src/Day3.hs
+++ 2025/src/Day3.hs
@@ -1,32 +1,28 @@
 module Day3 (part1, part2) where
 
--- parser :: Parser [Int]
--- parser = many1 (number <* eol) <* eof
+p :: Integer -> Vector ℕ₁₀ -> ℤ
+p e bs =
+  let n = size bs
+      go :: (ℤ, ℤ) .->. Maybe ℤ
+      go (i, e)
+        | e == 0 = pure (Just 0)
+        | i == n = pure Nothing
+        | otherwise = do
+            aM <- go .$. (i + 1, e)
+            bM <- go .$. (i + 1, e - 1)
+            case (aM, bM) of
+              (Nothing, Nothing) -> pure Nothing
+              (Just a, Nothing) -> pure . Just $ a
+              (Nothing, Just b') ->
+                let b = b' + (fromIntegral (bs !! i) * 10 ^ (e - 1))
+                 in pure . Just $ b
+              (Just a, Just b') ->
+                let b = b' + (fromIntegral (bs !! i) * 10 ^ (e - 1))
+                 in pure . Just $ max a b
+   in (? 0) . run $ go .$. (0, e)
 
--- line :: Parser Int
--- line = number
+part1 :: Integer
+part1 = $(aoc 3) |-<..> (Σ . p 2 . co <$> many nat₁₀)
 
--- data Cell
---   = Empty
---   | Wall
---   deriving (Eq, Ord)
-
--- instance GridCell Cell where
---   charMap =
---     BM.fromList
---       [ (Empty, ' '),
---         (Wall, '#')
---       ]
-
-part1 :: Text
-part1 =
-  $(input (3 :: Int))
-    -- & readAs (signed decimal)
-    -- & parseWith parser
-    -- & parseLinesWith line
-    -- & lines
-    -- & readGrid
-    & (<> "Part 1")
-
-part2 :: Text
-part2 = "Part 2"
+part2 :: Integer
+part2 = $(aoc 3) |-<..> (Σ . p 12 . co <$> many nat₁₀)
