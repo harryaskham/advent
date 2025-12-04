f918ecfec4375d09167a431bde7b95547c047563
Harry Askham <harryaskham@gmail.com>
2025-12-04 09:39:20 +0000

    Day4

--- 2025/src/Day4.hs
+++ 2025/src/Day4.hs
@@ -1,15 +1,15 @@
 module Day4 (part1, part2) where
 
+removable :: ".@" ▦ ℤ² -> [ℤ²]
+removable g = [c | c <- coords g, g |! c == (#"@" □), size [n | n <- neighs @8 @[] c g, g |! n == (#"@" □)] < 4]
+
 remove :: ℤ -> ".@" ▦ ℤ² -> ℤ
 remove 0 _ = 0
 remove n g =
-  let cs = [c | c <- coords g, g |! c == (#"@" □), size [n | n <- neighs @8 @[] c g, g |! n == (#"@" □)] < 4]
-   in case cs of
-        [] -> 0
-        cs -> size cs + remove (n - 1) (foldl' (\g c -> g |. (c, (#"." □))) g cs)
+  case removable g of
+    [] -> 0
+    cs -> size cs + remove (n - 1) (foldl' (\g c -> g |. (c, (#"." □))) g cs)
 
-(part1, part2) :: ℤ² =
-  let (g :: ".@" ▦ ℤ²) = $(grid 4)
-   in ( remove 1 g,
-        remove (-1) g
-      )
+g :: ".@" ▦ ℤ² = $(grid 4)
+
+(part1, part2) :: ℤ² = (remove 1 g, remove (-1) g)
--- 2025/test/Spec.hs
+++ 2025/test/Spec.hs
@@ -26,3 +26,30 @@ main = hspec do
   describe ("Day 3") do
     it "computes Part 1" (Day3.part1 `shouldBe` 17158)
     it "computes Part 2" (Day3.part2 `shouldBe` 170449335646486)
+  describe ("Day 4") do
+    it "computes Part 1" (Day4.part1 `shouldBe` 1397)
+    it "computes Part 2" (Day4.part2 `shouldBe` 8758)
+  describe ("Day 5") do
+    it "computes Part 1" (Day5.part1 `shouldBe` 0)
+    it "computes Part 2" (Day5.part2 `shouldBe` 0)
+  describe ("Day 6") do
+    it "computes Part 1" (Day6.part1 `shouldBe` 0)
+    it "computes Part 2" (Day6.part2 `shouldBe` 0)
+  describe ("Day 7") do
+    it "computes Part 1" (Day7.part1 `shouldBe` 0)
+    it "computes Part 2" (Day7.part2 `shouldBe` 0)
+  describe ("Day 8") do
+    it "computes Part 1" (Day8.part1 `shouldBe` 0)
+    it "computes Part 2" (Day8.part2 `shouldBe` 0)
+  describe ("Day 9") do
+    it "computes Part 1" (Day9.part1 `shouldBe` 0)
+    it "computes Part 2" (Day9.part2 `shouldBe` 0)
+  describe ("Day 10") do
+    it "computes Part 1" (Day10.part1 `shouldBe` 0)
+    it "computes Part 2" (Day10.part2 `shouldBe` 0)
+  describe ("Day 11") do
+    it "computes Part 1" (Day11.part1 `shouldBe` 0)
+    it "computes Part 2" (Day11.part2 `shouldBe` 0)
+  describe ("Day 12") do
+    it "computes Part 1" (Day12.part1 `shouldBe` 0)
+    it "computes Part 2" (Day12.part2 `shouldBe` 0)
