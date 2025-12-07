2b3d32a35cab5cabb267b470579e5e3607623cee
Harry Askham <harryaskham@gmail.com>
2025-12-07 14:22:09 +0000

    [rapid commit; squash later]

--- 2025/src/Day6.hs
+++ 2025/src/Day6.hs
@@ -2,6 +2,6 @@ module Day6 (part1, part2) where
 
 (ps, ops) :: [[ℕ]] × [ℕ -> ℕ -> ℕ] = (⊤) ⇱ ($(aoc 6) ⋯)
 
-psᵀ :: [[ℕ]] = ((splitOn "\n" $(aoc 6)) ⊤) & splitBy (all (== ' ')) & (filter isDigit <$$>) & ((⋯) <$$>)
+psᵀ :: [[ℕ]] = ((splitOn "\n" $(aoc 6)) ⊤) & splitBy (all (== ' ')) & ((isDigit <-?-|) <$$>) & ((⋯) <$$>)
 
 (part1, part2) :: (Σ ℕ, Σ ℕ) = both ((<>!) . ((Σ . (!>) . (Ŀ $@)) <$>) . (zip ops)) (ps, psᵀ)
