ab070eb6ddbf2129d911b6d060da826263a57177
Harry Askham <harryaskham@gmail.com>
2025-12-12 23:28:15 +0000

    [rapid commit; squash later]

--- 2025/src/Day12.hs
+++ 2025/src/Day12.hs
@@ -1,5 +1,29 @@
 module Day12 (part1, part2) where
 
-part1 :: ℤ = ($(aoc 12) ⋯)
+(ps, rs) :: [(ℤ, ".#" ▦ ℤ²)] × [(ℤ², [ℤ])] =
+  $(aoc 12) & (⊏|⊐) @(([(ℤ, ".#" ▦ ℤ²) ⯻ ":\n"] ≠ []) × ([(ℤ² ⯻ "x", [ℤ] ⯻ " ") ⯻ ": "] ≠ []))
 
-part2 :: ℤ = ($(aoc 12) ⋯)
+place :: ℤ² -> [".#" ▦ ℤ²] -> ".#" ▦ ℤ² -> [".#" ▦ ℤ²]
+place (w, h) gs p =
+  nub
+    [ ((Ł (\g c -> g |. (c, (#"#" □))) g ks) !>)
+    | g <- gs,
+      traceTextLn (pretty g) $ True,
+      x <- [0 .. w - 1],
+      y <- [0 .. h - 1],
+      v <- mapCoords (+ (x, y)) <$> variantsNub p,
+      let ks = v |?> (#"#" □),
+      let os = ks |-?-> ((≡ Just (#"." □)) ∘ (g |?)),
+      os ≡ ks
+    ]
+
+fit :: (ℤ², [ℤ]) -> Maybe (".#" ▦ ℤ²)
+fit ((w, h), ns) =
+  let gs = foldl' (place (w, h)) [mkGrid [((x, y), (#"." □)) | x <- [0 .. w - 1], y <- [0 .. h - 1]]] ((snd <$> ps) ⤊* ns)
+   in case gs of
+        [] -> Nothing
+        (g : _) -> Just g
+
+part1 :: ℤ = rs |?| (isJust ∘ fit)
+
+part2 :: ℤ = 0
